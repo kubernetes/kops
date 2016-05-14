505ef056d94ce9c1d26b1200dc7bcb200a0954c6
Justin Santa Barbara <justin@fathomdb.com>
2016-05-14 13:03:21 -0400

    Add upup command, with a subcommand to generate kubecfg

---
+++ upup/cmd/upup/kubecfg.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// kubecfgCmd represents the kubecfg command
+var kubecfgCmd = &cobra.Command{
+	Use:   "kubecfg",
+	Short: "Manage kubecfg files",
+	Long:  `Manage kubecfg files`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Usage: generate")
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(kubecfgCmd)
+}
