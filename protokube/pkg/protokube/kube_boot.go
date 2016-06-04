d5141af03fb2a36bac48236bcc85250ed7a3b029
Justin Santa Barbara <justin@fathomdb.com>
2016-06-04 15:58:34 -0400

    protokube: only try to mount the master disk on the master

--- protokube/pkg/protokube/kube_boot.go
+++ protokube/pkg/protokube/kube_boot.go
@@ -6,11 +6,13 @@ import (
 )
 
 type KubeBoot struct {
+	master bool
 	volumes Volumes
 }
 
-func NewKubeBoot(volumes Volumes) *KubeBoot {
+func NewKubeBoot(master bool, volumes Volumes) *KubeBoot {
 	k := &KubeBoot{
+		master: master,
 		volumes: volumes,
 	}
 	return k
@@ -20,11 +22,11 @@ func (k *KubeBoot) Bootstrap() error {
 	for {
 		done, err := k.tryBootstrap()
 		if err != nil {
-			glog.Warningf("error during attempt to acquire master volume (will sleep and retry): %v", err)
+			glog.Warningf("error during attempt to bootstrap (will sleep and retry): %v", err)
 		} else if done {
 			break
 		} else {
-			glog.Infof("unable to acquire master volume; will sleep and retry")
+			glog.Infof("unable to bootstrap; will sleep and retry")
 		}
 
 		time.Sleep(1 * time.Minute)
@@ -34,13 +36,20 @@ func (k *KubeBoot) Bootstrap() error {
 }
 
 func (k *KubeBoot) tryBootstrap() (bool, error) {
-	mountpoint, err := k.mountMasterVolume()
-	if err != nil {
-		return false, err
-	}
+	if k.master {
+		mountpoint, err := k.mountMasterVolume()
+		if err != nil {
+			return false, err
+		}
 
-	glog.Infof("mounted master on %s", mountpoint)
-	// TODO: Should we set up symlinks here?
+		if mountpoint == "" {
+			glog.Infof("unable to acquire master volume")
+			return false, nil
+		}
+
+		glog.Infof("mounted master on %s", mountpoint)
+		// TODO: Should we set up symlinks here?
+	}
 
 	return true, nil
 }
