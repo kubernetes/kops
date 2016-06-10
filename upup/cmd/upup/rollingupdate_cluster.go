9b32edfb8106a8a5e59fd15d0acbd23cc5115fdc
Justin Santa Barbara <justin@fathomdb.com>
2016-06-10 11:30:05 -0400

    upup: add experimental cluster rolling-update command
    Little more than a hack right now, but a good place to start.

---
+++ upup/cmd/upup/rollingupdate_cluster.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+
+	"bytes"
+	"github.com/golang/glog"
+	"github.com/spf13/cobra"
+	"k8s.io/kube-deploy/upup/pkg/fi/cloudup/awsup"
+	"k8s.io/kube-deploy/upup/pkg/kutil"
+	"os"
+	"text/tabwriter"
+)
+
+type RollingUpdateClusterCmd struct {
+	ClusterID string
+	Yes       bool
+	Region    string
+
+	cobraCommand *cobra.Command
+}
+
+var rollingupdateCluster = RollingUpdateClusterCmd{
+	cobraCommand: &cobra.Command{
+		Use:   "cluster",
+		Short: "rolling-update cluster",
+		Long:  `rolling-updates a k8s cluster.`,
+	},
+}
+
+func init() {
+	cmd := rollingupdateCluster.cobraCommand
+	rollingUpdateCommand.cobraCommand.AddCommand(cmd)
+
+	cmd.Flags().BoolVar(&rollingupdateCluster.Yes, "yes", false, "Rollingupdate without confirmation")
+
+	cmd.Flags().StringVar(&rollingupdateCluster.ClusterID, "cluster-id", "", "cluster id")
+	cmd.Flags().StringVar(&rollingupdateCluster.Region, "region", "", "region")
+
+	cmd.Run = func(cmd *cobra.Command, args []string) {
+		err := rollingupdateCluster.Run()
+		if err != nil {
+			glog.Exitf("%v", err)
+		}
+	}
+}
+
+func (c *RollingUpdateClusterCmd) Run() error {
+	if c.Region == "" {
+		return fmt.Errorf("--region is required")
+	}
+	if c.ClusterID == "" {
+		return fmt.Errorf("--cluster-id is required")
+	}
+
+	tags := map[string]string{"KubernetesCluster": c.ClusterID}
+	cloud, err := awsup.NewAWSCloud(c.Region, tags)
+	if err != nil {
+		return fmt.Errorf("error initializing AWS client: %v", err)
+	}
+
+	d := &kutil.RollingUpdateCluster{}
+
+	d.ClusterID = c.ClusterID
+	d.Region = c.Region
+	d.Cloud = cloud
+
+	nodesets, err := d.ListNodesets()
+	if err != nil {
+		return err
+	}
+
+	err = c.printNodesets(nodesets)
+	if err != nil {
+		return err
+	}
+
+	if !c.Yes {
+		return fmt.Errorf("Must specify --yes to rolling-update")
+	}
+
+	return d.RollingUpdateNodesets(nodesets)
+}
+
+func (c *RollingUpdateClusterCmd) printNodesets(nodesets map[string]*kutil.Nodeset) error {
+	w := new(tabwriter.Writer)
+	var b bytes.Buffer
+
+	// Format in tab-separated columns with a tab stop of 8.
+	w.Init(os.Stdout, 0, 8, 0, '\t', tabwriter.StripEscape)
+	for _, n := range nodesets {
+		b.WriteByte(tabwriter.Escape)
+		b.WriteString(n.Name)
+		b.WriteByte(tabwriter.Escape)
+		b.WriteByte('\t')
+		b.WriteByte(tabwriter.Escape)
+		b.WriteString(n.Status)
+		b.WriteByte(tabwriter.Escape)
+		b.WriteByte('\t')
+		b.WriteByte(tabwriter.Escape)
+		b.WriteString(fmt.Sprintf("%d", len(n.NeedUpdate)))
+		b.WriteByte(tabwriter.Escape)
+		b.WriteByte('\t')
+		b.WriteByte(tabwriter.Escape)
+		b.WriteString(fmt.Sprintf("%d", len(n.Ready)))
+		b.WriteByte(tabwriter.Escape)
+		b.WriteByte('\n')
+
+		_, err := w.Write(b.Bytes())
+		if err != nil {
+			return fmt.Errorf("error writing to output: %v", err)
+		}
+		b.Reset()
+	}
+
+	return w.Flush()
+}
---
+++ upup/pkg/kutil/rollingupdate_cluster.go
@@ -0,0 +1,138 @@
+package kutil
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/autoscaling"
+	"k8s.io/kube-deploy/upup/pkg/fi"
+	"k8s.io/kube-deploy/upup/pkg/fi/cloudup/awsup"
+	"github.com/golang/glog"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"fmt"
+	"time"
+	"sync"
+)
+
+// RollingUpdateCluster restarts cluster nodes
+type RollingUpdateCluster struct {
+	ClusterID string
+	Region    string
+	Cloud     fi.Cloud
+}
+
+func (c *RollingUpdateCluster) ListNodesets() (map[string]*Nodeset, error) {
+	cloud := c.Cloud.(*awsup.AWSCloud)
+
+	nodesets := make(map[string]*Nodeset)
+
+	tags := cloud.BuildTags(nil)
+
+	asgs, err := findAutoscalingGroups(cloud, tags)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, asg := range asgs {
+		nodeset := buildNodeset(asg)
+		nodesets[nodeset.Name] = nodeset
+	}
+
+	return nodesets, nil
+}
+
+func (c *RollingUpdateCluster) RollingUpdateNodesets(nodesets map[string]*Nodeset) error {
+	if len(nodesets) == 0 {
+		return nil
+	}
+
+	var wg sync.WaitGroup
+	var resultsMutex sync.Mutex
+	results := make(map[string]error)
+
+	for k, nodeset := range nodesets {
+		wg.Add(1)
+		go func(k string, nodeset *Nodeset) {
+			results[k] = fmt.Errorf("function panic")
+			defer wg.Done()
+			err := nodeset.RollingUpdate(c.Cloud)
+
+			resultsMutex.Lock()
+			defer resultsMutex.Unlock()
+			results[k] = err
+		}(k, nodeset)
+	}
+
+	wg.Wait()
+
+	for _, err := range results {
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+type Nodeset struct {
+	Name       string
+	Status     string
+	Ready      []*autoscaling.Instance
+	NeedUpdate []*autoscaling.Instance
+}
+
+func buildNodeset(g *autoscaling.Group) *Nodeset {
+	n := &Nodeset{
+		Name: aws.StringValue(g.AutoScalingGroupName),
+	}
+
+	findLaunchConfigurationName := aws.StringValue(g.LaunchConfigurationName)
+
+	for _, i := range g.Instances {
+		if findLaunchConfigurationName == aws.StringValue(i.LaunchConfigurationName) {
+			n.Ready = append(n.Ready, i)
+		} else {
+			n.NeedUpdate = append(n.NeedUpdate, i)
+		}
+	}
+
+	if len(n.NeedUpdate) == 0 {
+		n.Status = "Ready"
+	} else {
+		n.Status = "NeedsUpdate"
+	}
+
+	return n
+}
+
+func (n *Nodeset) RollingUpdate(cloud fi.Cloud) error {
+	c := cloud.(*awsup.AWSCloud)
+
+	for _, i := range n.NeedUpdate {
+		glog.Infof("Stopping instance %q in nodeset %q", *i.InstanceId, n.Name)
+
+		// TODO: Evacuate through k8s first?
+
+		// TODO: Temporarily increase size of ASG?
+		
+		// TODO: Remove from ASG first so status is immediately updated?
+
+		// TODO: Batch termination, like a rolling-update
+
+		request := &ec2.TerminateInstancesInput{
+			InstanceIds: []*string{i.InstanceId},
+		}
+		_, err := c.EC2.TerminateInstances(request)
+		if err != nil {
+			return fmt.Errorf("error deleting instance %q: %v", i.InstanceId, err)
+		}
+
+
+		// TODO: Wait for node to appear back in k8s
+		time.Sleep(time.Minute)
+	}
+
+	return nil
+}
+
+func (n *Nodeset) String() string {
+	return "nodeset:" + n.Name
+}
