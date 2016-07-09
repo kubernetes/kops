5c7913bd4a6cc7b6e07e218e7e5d1945bae83eaa
Justin Santa Barbara <justin@fathomdb.com>
2016-07-09 14:49:25 -0400

    Sort table output
    Currently sort order is locked to table order, but this could be changed
    if needed.
    
    Fix #80

--- cmd/kops/format.go
+++ cmd/kops/format.go
@@ -7,31 +7,123 @@ import (
 	"io"
 	"k8s.io/kops/upup/pkg/fi"
 	"reflect"
+	"sort"
 	"text/tabwriter"
 )
 
 // Table renders tables to stdout
 type Table struct {
-	columns []string
-	getters []reflect.Value
+	columns map[string]*TableColumn
 }
+
+type TableColumn struct {
+	Name   string
+	Getter reflect.Value
+}
+
+func (c *TableColumn) getFromValue(v reflect.Value) string {
+	var args []reflect.Value
+	args = append(args, v)
+	fvs := c.Getter.Call(args)
+	fv := fvs[0]
+
+	return fi.ValueAsString(fv)
+}
+
+type getterFunction func(interface{}) string
 
 // AddColumn registers an available column for formatting
 func (t *Table) AddColumn(name string, getter interface{}) {
 	getterVal := reflect.ValueOf(getter)
 
-	t.columns = append(t.columns, name)
-	t.getters = append(t.getters, getterVal)
+	column := &TableColumn{
+		Name:   name,
+		Getter: getterVal,
+	}
+	if t.columns == nil {
+		t.columns = make(map[string]*TableColumn)
+	}
+	t.columns[name] = column
+}
+
+type funcSorter struct {
+	len  int
+	less func(int, int) bool
+	swap func(int, int)
+}
+
+func (f *funcSorter) Len() int {
+	return f.len
+}
+func (f *funcSorter) Less(i, j int) bool {
+	return f.less(i, j)
+}
+func (f *funcSorter) Swap(i, j int) {
+	f.swap(i, j)
+}
+
+func SortByFunction(len int, swap func(int, int), less func(int, int) bool) {
+	sort.Sort(&funcSorter{len, less, swap})
+}
+
+func (t *Table) findColumns(columnNames ...string) ([]*TableColumn, error) {
+	columns := make([]*TableColumn, len(columnNames))
+	for i, columnName := range columnNames {
+		c := t.columns[columnName]
+		if c == nil {
+			return nil, fmt.Errorf("column not found: %v", columnName)
+		}
+		columns[i] = c
+	}
+	return columns, nil
 }
 
 // Render writes the items in a table, to out
-func (t *Table) Render(items interface{}, out io.Writer) error {
+func (t *Table) Render(items interface{}, out io.Writer, columnNames ...string) error {
 	itemsValue := reflect.ValueOf(items)
 	if itemsValue.Kind() != reflect.Slice {
 		glog.Fatal("unexpected kind for items: ", itemsValue.Kind())
 	}
 
-	length := itemsValue.Len()
+	columns, err := t.findColumns(columnNames...)
+	if err != nil {
+		return err
+	}
+
+	n := itemsValue.Len()
+
+	rows := make([][]string, n)
+	for i := 0; i < n; i++ {
+		row := make([]string, len(columns))
+		item := itemsValue.Index(i)
+		for j, column := range columns {
+			row[j] = column.getFromValue(item)
+		}
+		rows[i] = row
+	}
+
+	SortByFunction(n, func(i, j int) {
+		a := itemsValue.Index(i)
+		itemsValue.Index(i).Set(itemsValue.Index(j))
+		itemsValue.Index(j).Set(a)
+
+		row := rows[i]
+		rows[i] = rows[j]
+		rows[j] = row
+	}, func(i, j int) bool {
+		l := rows[i]
+		r := rows[j]
+
+		for k := 0; k < len(columns); k++ {
+			lV := l[k]
+			rV := r[k]
+
+			if lV != rV {
+				return lV < rV
+			}
+		}
+		return false
+	})
 
 	var b bytes.Buffer
 	w := new(tabwriter.Writer)
@@ -41,12 +133,12 @@ func (t *Table) Render(items interface{}, out io.Writer) error {
 
 	writeHeader := true
 	if writeHeader {
-		for i, c := range t.columns {
+		for i, c := range columns {
 			if i != 0 {
 				b.WriteByte('\t')
 			}
 			b.WriteByte(tabwriter.Escape)
-			b.WriteString(c)
+			b.WriteString(c.Name)
 			b.WriteByte(tabwriter.Escape)
 		}
 		b.WriteByte('\n')
@@ -58,24 +150,14 @@ func (t *Table) Render(items interface{}, out io.Writer) error {
 		b.Reset()
 	}
 
-	for i := 0; i < length; i++ {
-		item := itemsValue.Index(i)
-
-		for j := range t.columns {
-			if j != 0 {
+	for _, row := range rows {
+		for i, col := range row {
+			if i != 0 {
 				b.WriteByte('\t')
 			}
 
-			getter := t.getters[j]
-			var args []reflect.Value
-			args = append(args, item)
-			fvs := getter.Call(args)
-			fv := fvs[0]
-
-			s := fi.ValueAsString(fv)
-
 			b.WriteByte(tabwriter.Escape)
-			b.WriteString(s)
+			b.WriteString(col)
 			b.WriteByte(tabwriter.Escape)
 		}
 		b.WriteByte('\n')
