775c40e4b2ce36dcc1b0c94c09ad48b0e63aefae
Justin Santa Barbara <justin@fathomdb.com>
2016-07-12 22:15:20 -0400

    Find matching existing DNS hosted zone
    We choose the longest matching existing hosted zone as the default,
    rather than the previous heuristic which was overly simplistic.
    
    We also require the hosted zone to exist now; it doesn't seem to really
    cost us anything given that the user has to set up DNS delegation
    anyway.
    
    Fix #125

--- upup/pkg/fi/cloudup/awsup/aws_cloud.go
+++ upup/pkg/fi/cloudup/awsup/aws_cloud.go
@@ -464,3 +464,60 @@ func (c *AWSCloud) ValidateZones(zones []string) error {
 
 	return nil
 }
+
+func (c *AWSCloud) FindDNSHostedZone(clusterDNSName string) (string, error) {
+	glog.V(2).Infof("Querying for all route53 zones to find match for %q", clusterDNSName)
+
+	clusterDNSName = "." + strings.TrimSuffix(clusterDNSName, ".")
+
+	var zones []*route53.HostedZone
+	request := &route53.ListHostedZonesInput{}
+	err := c.Route53.ListHostedZonesPages(request, func(p *route53.ListHostedZonesOutput, lastPage bool) bool {
+		for _, zone := range p.HostedZones {
+			zoneName := aws.StringValue(zone.Name)
+			zoneName = "." + strings.TrimSuffix(zoneName, ".")
+
+			if strings.HasSuffix(clusterDNSName, zoneName) {
+				zones = append(zones, zone)
+			}
+		}
+		return true
+	})
+	if err != nil {
+		return "", fmt.Errorf("error querying for route53 zones: %v", err)
+	}
+
+	// Find the longest zones
+	maxLength := -1
+	maxLengthZones := []*route53.HostedZone{}
+	for _, z := range zones {
+		n := len(aws.StringValue(z.Name))
+		if n < maxLength {
+			continue
+		}
+
+		if n > maxLength {
+			maxLength = n
+			maxLengthZones = []*route53.HostedZone{}
+		}
+
+		maxLengthZones = append(maxLengthZones, z)
+	}
+
+	if len(maxLengthZones) == 0 {
+		// We make this an error because you have to set up DNS delegation anyway
+		tokens := strings.Split(clusterDNSName, ".")
+		suffix := strings.Join(tokens[len(tokens)-2:], ".")
+		//glog.Warningf("No matching hosted zones found; will created %q", suffix)
+		//return suffix, nil
+		return "", fmt.Errorf("No matching hosted zones found for %q; please create one (e.g. %q) first", clusterDNSName, suffix)
+	}
+
+	if len(maxLengthZones) == 1 {
+		id := aws.StringValue(maxLengthZones[0].Id)
+		id = strings.TrimPrefix(id, "/hostedzone/")
+		return id, nil
+	}
+
+	return "", fmt.Errorf("Found multiple hosted zones matching cluster %q; please specify the ID of the zone to use")
+}
