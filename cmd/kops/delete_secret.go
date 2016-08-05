c43bb2486b117c2254ff80fc907a5ce776a10edb
Justin Santa Barbara <justin@fathomdb.com>
2016-08-05 01:23:13 -0400

    More consistency in publickey CLI
    Also docs skeleton

--- cmd/kops/delete_secret.go
+++ cmd/kops/delete_secret.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"k8s.io/kops/upup/pkg/fi"
 )
 
 type DeleteSecretCmd struct {
@@ -28,16 +29,31 @@ func init() {
 }
 
 func (c *DeleteSecretCmd) Run(args []string) error {
-	if len(args) != 2 {
-		return fmt.Errorf("Syntax: <type> <name>")
+	if len(args) != 2 && len(args) != 3 {
+		return fmt.Errorf("Syntax: <type> <name> [<id>]")
 	}
 
 	secretType := args[0]
 	secretName := args[1]
 
+	secretID := ""
+	if len(args) == 3 {
+		secretID = args[2]
+	}
+
 	secrets, err := listSecrets(secretType, []string{secretName})
 	if err != nil {
 		return err
+	}
+
+	if secretID != "" {
+		var matches []*fi.KeystoreItem
+		for _, s := range secrets {
+			if s.Id == secretID {
+				matches = append(matches, s)
+			}
+		}
+		secrets = matches
 	}
 
 	if len(secrets) == 0 {
@@ -45,7 +61,8 @@ func (c *DeleteSecretCmd) Run(args []string) error {
 	}
 
 	if len(secrets) != 1 {
-		return fmt.Errorf("found multiple matching secrets")
+		// TODO: it would be friendly to print the matching keys
+		return fmt.Errorf("found multiple matching secrets; specify the id of the key")
 	}
 
 	keyStore, err := rootCommand.KeyStore()
