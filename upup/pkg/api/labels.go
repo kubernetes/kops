9c3105b323d00c8aec8728e7a796d216b238ddbd
Justin Santa Barbara <justin@fathomdb.com>
2016-08-17 02:14:48 -0400

    Make automatic upgrades optional
    We add a `UpdatePolicy` field to the cluster spec
    
    Also document how this feature was added.

--- upup/pkg/api/labels.go
+++ upup/pkg/api/labels.go
@@ -5,3 +5,6 @@ const AnnotationNameManagement = "kops.kubernetes.io/management"
 
 // AnnotationValueManagementImported is the annotation value that indicates a cluster was imported, typically as part of an upgrade
 const AnnotationValueManagementImported = "imported"
+
+// UpdatePolicyExternal is a value for ClusterSpec.UpdatePolicy indicating that upgrades are done externally, and we should disable automatic upgrades
+const UpdatePolicyExternal = "external"
