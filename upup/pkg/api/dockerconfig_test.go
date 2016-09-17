972cd000caf4fb86fb0dee1c6a058d395c7d78c9
Justin Santa Barbara <justin@fathomdb.com>
2016-09-17 14:48:15 -0400

    Split out docker config

---
+++ upup/pkg/api/dockerconfig_test.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"k8s.io/kubernetes/pkg/util/validation/field"
+	"testing"
+)
+
+func Test_Validate_DockerConfig_Storage(t *testing.T) {
+	for _, name := range []string{"aufs", "zfs", "overlay"} {
+		config := &DockerConfig{Storage: &name}
+		errs := ValidateDockerConfig(config, field.NewPath("docker"))
+		if len(errs) != 0 {
+			t.Fatalf("Unexpected errors validating DockerConfig %q", errs)
+		}
+	}
+
+	for _, name := range []string{"overlayfs", "", "au"} {
+		config := &DockerConfig{Storage: &name}
+		errs := ValidateDockerConfig(config, field.NewPath("docker"))
+		if len(errs) != 1 {
+			t.Fatalf("Expected errors validating DockerConfig %q", config)
+		}
+		if errs[0].Field != "docker.storage" || errs[0].Type != field.ErrorTypeNotSupported {
+			t.Fatalf("Not the expected error validating DockerConfig %q", errs)
+		}
+	}
+}
