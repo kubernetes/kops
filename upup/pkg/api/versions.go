13870c30c5891c588cd15adb801834a8bb3c9cf7
Justin Santa Barbara <justin@fathomdb.com>
2016-09-29 21:57:25 -0400

    Prevent KubernetesVersion 1.4 with classic networking

---
+++ upup/pkg/api/versions.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"fmt"
+	"github.com/blang/semver"
+	"github.com/golang/glog"
+	"strings"
+)
+
+func ParseKubernetesVersion(version string) (*semver.Version, error) {
+	sv, err := semver.ParseTolerant(version)
+	if err != nil {
+		glog.Warningf("error parsing kubernetes semver %q, falling back to string matching", version)
+
+		v := strings.Trim(version, "v")
+		if strings.HasPrefix(v, "1.3.") {
+			sv = semver.Version{Major: 1, Minor: 3}
+		} else if strings.HasPrefix(v, "1.4.") {
+			sv = semver.Version{Major: 1, Minor: 4}
+		} else if strings.HasPrefix(v, "1.5.") {
+			sv = semver.Version{Major: 1, Minor: 5}
+		} else if strings.Contains(v, "/v1.3.") {
+			sv = semver.Version{Major: 1, Minor: 3}
+		} else if strings.Contains(v, "/v1.4.") {
+			sv = semver.Version{Major: 1, Minor: 4}
+		} else if strings.Contains(v, "/v1.5.") {
+			sv = semver.Version{Major: 1, Minor: 5}
+		} else {
+			return nil, fmt.Errorf("unable to parse kubernetes version %q", version)
+		}
+	}
+
+	return &sv, nil
+}
