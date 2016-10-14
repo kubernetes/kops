7a4c748eb88c8203430dd190790f3a15efc426bb
chrislovecnm <clove@cnmconsulting.net>
2016-10-14 11:44:50 -0600

    go fmt and unit test fixed
    more test cleanup

--- upup/pkg/fi/cloudup/populatecluster_test.go
+++ upup/pkg/fi/cloudup/populatecluster_test.go
@@ -156,11 +156,11 @@ func TestPopulateCluster_CNI(t *testing.T) {
 	}
 
 	/*
-	c.Spec.Zones = []*api.ClusterZoneSpec{
-		{Name: "us-west-2a", CIDR: "172.20.2.0/27"},
-		{Name: "us-west-2b", CIDR: "172.20.2.32/27"},
-		{Name: "us-west-2c", CIDR: "172.20.2.64/27"},
-	}*/
+		c.Spec.Zones = []*api.ClusterZoneSpec{
+			{Name: "us-west-2a", CIDR: "172.20.2.0/27"},
+			{Name: "us-west-2b", CIDR: "172.20.2.32/27"},
+			{Name: "us-west-2c", CIDR: "172.20.2.64/27"},
+		}*/
 
 	full, err := build(c)
 	if err != nil {
