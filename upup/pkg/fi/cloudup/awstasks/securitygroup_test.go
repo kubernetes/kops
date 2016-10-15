e8816f064381666b72fc4411a0588ef6f4e6fbb3
Justin Santa Barbara <justin@fathomdb.com>
2016-10-15 10:28:31 -0400

    Remove security group rules that match our filter
    We configure a filter so that we only remove rules on port 22 & 443
    
    Fix #478

---
+++ upup/pkg/fi/cloudup/awstasks/securitygroup_test.go
@@ -0,0 +1,73 @@
+package awstasks
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"testing"
+)
+
+func TestParseRemovalRule(t *testing.T) {
+	testNotParse(t, "port 22")
+	testNotParse(t, "port22")
+	testNotParse(t, "port=a")
+	testNotParse(t, "port=22-23")
+
+	testParsesAsPort(t, "port=22", 22)
+	testParsesAsPort(t, "port=443", 443)
+}
+
+func testNotParse(t *testing.T, rule string) {
+	r, err := ParseRemovalRule(rule)
+	if err == nil {
+		t.Fatalf("expected failure to parse removal rule %q, got %v", rule, r)
+	}
+}
+
+func testParsesAsPort(t *testing.T, rule string, port int) {
+	r, err := ParseRemovalRule(rule)
+	if err != nil {
+		t.Fatalf("unexpected failure to parse rule %q: %v", rule, err)
+	}
+	portRemovalRule, ok := r.(*PortRemovalRule)
+	if !ok {
+		t.Fatalf("unexpected rule type for rule %q: %T", r, err)
+	}
+	if portRemovalRule.Port != port {
+		t.Fatalf("unexpected port for %q, expecting %d, got %q", rule, port, r)
+	}
+}
+
+func TestPortRemovalRule(t *testing.T) {
+	r := &PortRemovalRule{Port: 22}
+	testMatches(t, r, &ec2.IpPermission{FromPort: aws.Int64(22), ToPort: aws.Int64(22)})
+
+	testNotMatches(t, r, &ec2.IpPermission{FromPort: aws.Int64(0), ToPort: aws.Int64(0)})
+	testNotMatches(t, r, &ec2.IpPermission{FromPort: aws.Int64(23), ToPort: aws.Int64(23)})
+	testNotMatches(t, r, &ec2.IpPermission{FromPort: aws.Int64(20), ToPort: aws.Int64(22)})
+	testNotMatches(t, r, &ec2.IpPermission{FromPort: aws.Int64(22), ToPort: aws.Int64(23)})
+	testNotMatches(t, r, &ec2.IpPermission{ToPort: aws.Int64(22)})
+	testNotMatches(t, r, &ec2.IpPermission{FromPort: aws.Int64(22)})
+	testNotMatches(t, r, &ec2.IpPermission{})
+}
+
+func TestPortRemovalRule_Zero(t *testing.T) {
+	r := &PortRemovalRule{Port: 0}
+	testMatches(t, r, &ec2.IpPermission{FromPort: aws.Int64(0), ToPort: aws.Int64(0)})
+
+	testNotMatches(t, r, &ec2.IpPermission{FromPort: aws.Int64(0), ToPort: aws.Int64(20)})
+	testNotMatches(t, r, &ec2.IpPermission{ToPort: aws.Int64(0)})
+	testNotMatches(t, r, &ec2.IpPermission{FromPort: aws.Int64(0)})
+	testNotMatches(t, r, &ec2.IpPermission{})
+}
+
+func testMatches(t *testing.T, rule *PortRemovalRule, permission *ec2.IpPermission) {
+	if !rule.Matches(permission) {
+		t.Fatalf("rule %q failed to match permission %q", rule, permission)
+	}
+}
+
+func testNotMatches(t *testing.T, rule *PortRemovalRule, permission *ec2.IpPermission) {
+	if rule.Matches(permission) {
+		t.Fatalf("rule %q unexpectedly matched permission %q", rule, permission)
+	}
+}
