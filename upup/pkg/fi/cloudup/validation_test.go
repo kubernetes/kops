60410eb95ba6a739b30e97a8baa191311a3622ef
Justin Santa Barbara <justin@fathomdb.com>
2016-10-18 21:24:13 -0400

    Add unit test for imported cluster name
    Fix #684

--- upup/pkg/fi/cloudup/validation_test.go
+++ upup/pkg/fi/cloudup/validation_test.go
@@ -166,6 +166,17 @@ func Test_Validate_Kubenet_With_14(t *testing.T) {
 	expectNoErrorFromValidate(t, c)
 }
 
+func TestValidate_ClusterName_Import(t *testing.T) {
+	c := buildDefaultCluster(t)
+
+	// When we import a cluster, it likely won't have a valid name until we convert it
+	c.Annotations = make(map[string]string)
+	c.Annotations[api.AnnotationNameManagement] = api.AnnotationValueManagementImported
+	c.Name = "kubernetes"
+
+	expectNoErrorFromValidate(t, c)
+}
+
 func expectErrorFromValidate(t *testing.T, c *api.Cluster, message string) {
 	err := c.Validate(false)
 	if err == nil {
@@ -183,75 +194,3 @@ func expectNoErrorFromValidate(t *testing.T, c *api.Cluster) {
 		t.Fatalf("Unexpected error from Validate: %v", err)
 	}
 }
-
-//
-//import (
-//	"fmt"
-//	api "k8s.io/kops/pkg/apis/kops"
-//	"k8s.io/kops/upup/pkg/fi/vfs"
-//	k8sapi "k8s.io/kubernetes/pkg/api"
-//	"os"
-//	"path"
-//	"strings"
-//	"testing"
-//)
-//
-//func buildDefaultCreateCluster() *CreateClusterCmd {
-//	memfs := vfs.NewMemFSContext()
-//	memfs.MarkClusterReadable()
-//
-//	c := &CreateClusterCmd{}
-//
-//	// TODO: We should actually just specify the minimums here, and run in though the default logic
-//	c.Cluster = &api.Cluster{}
-//	c.Cluster.Name = "testcluster.mydomain.com"
-//	c.Cluster.Spec.Zones = []*api.ClusterZoneSpec{
-//		{Name: "us-mock-1a", CIDR: "172.20.1.0/24"},
-//		{Name: "us-mock-1b", CIDR: "172.20.2.0/24"},
-//		{Name: "us-mock-1c", CIDR: "172.20.3.0/24"},
-//		{Name: "us-mock-1d", CIDR: "172.20.4.0/24"},
-//	}
-//	c.InstanceGroups = append(c.InstanceGroups, buildNodeInstanceGroup("us-mock-1a"))
-//	c.InstanceGroups = append(c.InstanceGroups, buildMasterInstanceGroup("us-mock-1a"))
-//	c.SSHPublicKey = path.Join(os.Getenv("HOME"), ".ssh", "id_rsa.pub")
-//
-//	c.Cluster.Spec.Kubelet = &api.KubeletConfig{}
-//	c.Cluster.Spec.KubeControllerManager = &api.KubeControllerManagerConfig{}
-//	c.Cluster.Spec.KubeDNS = &api.KubeDNSConfig{}
-//	c.Cluster.Spec.KubeAPIServer = &api.KubeAPIServerConfig{}
-//	c.Cluster.Spec.KubeProxy = &api.KubeProxyConfig{}
-//	c.Cluster.Spec.Docker = &api.DockerConfig{}
-//
-//	c.Cluster.Spec.NetworkCIDR = "172.20.0.0/16"
-//
-//	c.Cluster.Spec.NonMasqueradeCIDR = "100.64.0.0/10"
-//	c.Cluster.Spec.Kubelet.NonMasqueradeCIDR = c.Cluster.Spec.NonMasqueradeCIDR
-//
-//	c.Cluster.Spec.ServiceClusterIPRange = "100.64.1.0/24"
-//	c.Cluster.Spec.KubeAPIServer.ServiceClusterIPRange = c.Cluster.Spec.ServiceClusterIPRange
-//
-//	c.Cluster.Spec.KubeDNS.ServerIP = "100.64.1.10"
-//	c.Cluster.Spec.Kubelet.ClusterDNS = c.Cluster.Spec.KubeDNS.ServerIP
-//
-//	c.Cluster.Spec.CloudProvider = "aws"
-//	c.Cluster.Spec.Kubelet.CloudProvider = c.Cluster.Spec.CloudProvider
-//	c.Cluster.Spec.KubeAPIServer.CloudProvider = c.Cluster.Spec.CloudProvider
-//	c.Cluster.Spec.KubeControllerManager.CloudProvider = c.Cluster.Spec.CloudProvider
-//
-//	c.Target = "dryrun"
-//
-//	basePath := vfs.NewMemFSPath(memfs, "test-statestore")
-//	c.ClusterRegistry = api.NewClusterRegistry(basePath)
-//
-//	return c
-//}
-//func expectErrorFromRun(t *testing.T, c *CreateClusterCmd, message string) {
-//	err := c.Run()
-//	if err == nil {
-//		t.Fatalf("Expected error from run")
-//	}
-//	actualMessage := fmt.Sprintf("%v", err)
-//	if !strings.Contains(actualMessage, message) {
-//		t.Fatalf("Expected error %q, got %q", message, actualMessage)
-//	}
-//}
