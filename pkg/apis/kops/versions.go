eff232dae5cc94e3c559e9b3c15f90fdc0cedf53
Zach Loafman <zml@google.com>
2016-11-07 11:22:59 -0800

    Quick fix: add v1.[67] to valid version checks

--- pkg/apis/kops/versions.go
+++ pkg/apis/kops/versions.go
@@ -18,9 +18,10 @@ package kops
 
 import (
 	"fmt"
+	"strings"
+
 	"github.com/blang/semver"
 	"github.com/golang/glog"
-	"strings"
 )
 
 func ParseKubernetesVersion(version string) (*semver.Version, error) {
@@ -35,12 +36,20 @@ func ParseKubernetesVersion(version string) (*semver.Version, error) {
 			sv = semver.Version{Major: 1, Minor: 4}
 		} else if strings.HasPrefix(v, "1.5.") {
 			sv = semver.Version{Major: 1, Minor: 5}
+		} else if strings.HasPrefix(v, "1.6.") {
+			sv = semver.Version{Major: 1, Minor: 6}
+		} else if strings.HasPrefix(v, "1.7.") {
+			sv = semver.Version{Major: 1, Minor: 7}
 		} else if strings.Contains(v, "/v1.3.") {
 			sv = semver.Version{Major: 1, Minor: 3}
 		} else if strings.Contains(v, "/v1.4.") {
 			sv = semver.Version{Major: 1, Minor: 4}
 		} else if strings.Contains(v, "/v1.5.") {
 			sv = semver.Version{Major: 1, Minor: 5}
+		} else if strings.Contains(v, "/v1.6.") {
+			sv = semver.Version{Major: 1, Minor: 6}
+		} else if strings.Contains(v, "/v1.7.") {
+			sv = semver.Version{Major: 1, Minor: 7}
 		} else {
 			return nil, fmt.Errorf("unable to parse kubernetes version %q", version)
 		}
