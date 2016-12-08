8715408e8ba4953cf314a0a2f47c035b7755b970
Justin Santa Barbara <justin@fathomdb.com>
2016-12-08 12:53:51 -0500

    dryrun output: include name instead of id:nil
    When we're creating objects, we tend not to have an id.  Rather than
    drop them entirely though, we can print their name (where we have one)

--- upup/pkg/fi/dryrun_target.go
+++ upup/pkg/fi/dryrun_target.go
@@ -343,7 +343,17 @@ func ValueAsString(value reflect.Value) string {
 			} else if compareWithID, ok := intf.(CompareWithID); ok {
 				id := compareWithID.CompareWithID()
 				if id == nil {
-					fmt.Fprintf(b, "id:<nil>")
+					// Uninformative, but we can often print the name instead
+					name := ""
+					hasName, ok := intf.(HasName)
+					if ok {
+						name = StringValue(hasName.GetName())
+					}
+					if name != "" {
+						fmt.Fprintf(b, "name:%s", name)
+					} else {
+						fmt.Fprintf(b, "id:<nil>")
+					}
 				} else {
 					fmt.Fprintf(b, "id:%s", *id)
 				}
