f08c451a065f08ba1b0fc027321344e89c3abffb
Kris Childress <kris@nivenly.com>
2016-12-11 14:07:57 -0700

    Fix nodeup_url - adding .s3

--- hack/dev-build.sh
+++ hack/dev-build.sh
@@ -75,7 +75,7 @@ cd $KOPS_DIRECTORY/..
 GIT_VER=git-$(git describe --always)
 [ -z "$GIT_VER" ] && echo "we do not have GIT_VER something is very wrong" && exit 1;
 
-NODEUP_URL="https://${NODEUP_BUCKET}.amazonaws.com/kops/${GIT_VER}/linux/amd64/nodeup"
+NODEUP_URL="https://${NODEUP_BUCKET}.s3.amazonaws.com/kops/${GIT_VER}/linux/amd64/nodeup"
 
 echo ==========
 echo "Starting build"
