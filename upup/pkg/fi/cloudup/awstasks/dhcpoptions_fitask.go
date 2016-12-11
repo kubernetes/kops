2e386c890a54802b503d2f685a97bc22a02572a9
Justin Santa Barbara <justin@fathomdb.com>
2016-12-11 10:40:34 -0500

    Updated codegen output

--- upup/pkg/fi/cloudup/awstasks/dhcpoptions_fitask.go
+++ upup/pkg/fi/cloudup/awstasks/dhcpoptions_fitask.go
@@ -29,6 +29,7 @@ import (
 // JSON marshalling boilerplate
 type realDHCPOptions DHCPOptions
 
+// UnmarshalJSON implements conversion to JSON, supporitng an alternate specification of the object as a string
 func (o *DHCPOptions) UnmarshalJSON(data []byte) error {
 	var jsonName string
 	if err := json.Unmarshal(data, &jsonName); err == nil {
@@ -46,14 +47,17 @@ func (o *DHCPOptions) UnmarshalJSON(data []byte) error {
 
 var _ fi.HasName = &DHCPOptions{}
 
-func (e *DHCPOptions) GetName() *string {
-	return e.Name
+// GetName returns the Name of the object, implementing fi.HasName
+func (o *DHCPOptions) GetName() *string {
+	return o.Name
 }
 
-func (e *DHCPOptions) SetName(name string) {
-	e.Name = &name
+// SetName sets the Name of the object, implementing fi.SetName
+func (o *DHCPOptions) SetName(name string) {
+	o.Name = &name
 }
 
-func (e *DHCPOptions) String() string {
-	return fi.TaskAsString(e)
+// String is the stringer function for the task, producing readable output using fi.TaskAsString
+func (o *DHCPOptions) String() string {
+	return fi.TaskAsString(o)
 }
--- upup/pkg/fi/cloudup/awstasks/internetgateway_fitask.go
+++ upup/pkg/fi/cloudup/awstasks/internetgateway_fitask.go
@@ -29,6 +29,7 @@ import (
 // JSON marshalling boilerplate
 type realInternetGateway InternetGateway
 
+// UnmarshalJSON implements conversion to JSON, supporitng an alternate specification of the object as a string
 func (o *InternetGateway) UnmarshalJSON(data []byte) error {
 	var jsonName string
 	if err := json.Unmarshal(data, &jsonName); err == nil {
@@ -46,14 +47,17 @@ func (o *InternetGateway) UnmarshalJSON(data []byte) error {
 
 var _ fi.HasName = &InternetGateway{}
 
-func (e *InternetGateway) GetName() *string {
-	return e.Name
+// GetName returns the Name of the object, implementing fi.HasName
+func (o *InternetGateway) GetName() *string {
+	return o.Name
 }
 
-func (e *InternetGateway) SetName(name string) {
-	e.Name = &name
+// SetName sets the Name of the object, implementing fi.SetName
+func (o *InternetGateway) SetName(name string) {
+	o.Name = &name
 }
 
-func (e *InternetGateway) String() string {
-	return fi.TaskAsString(e)
+// String is the stringer function for the task, producing readable output using fi.TaskAsString
+func (o *InternetGateway) String() string {
+	return fi.TaskAsString(o)
 }
