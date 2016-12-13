2aac4ea699bb0383958647ea593bcdd5844b05c0
Randall McPherson <rlmcpherson@gmail.com>
2016-12-13 17:55:47 -0500

    wip

--- cmd/kops/get.go
+++ cmd/kops/get.go
@@ -17,6 +17,11 @@ limitations under the License.
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/rlmcpherson/kops/upup/pkg/api"
 	"github.com/spf13/cobra"
 )
 
@@ -39,6 +44,7 @@ var getCmd = GetCmd{
 const (
 	OutputYaml  = "yaml"
 	OutputTable = "table"
+	OutputJSON  = "json"
 )
 
 func init() {
@@ -46,5 +52,37 @@ func init() {
 
 	rootCommand.AddCommand(cmd)
 
-	cmd.PersistentFlags().StringVarP(&getCmd.output, "output", "o", OutputTable, "output format.  One of: table, yaml")
+	cmd.PersistentFlags().StringVarP(&getCmd.output, "output", "o", OutputTable, "output format.  One of: table, yaml, json")
+}
+
+type marshalFunc func(v interface{}) ([]byte, error)
+
+func marshalToStdout(item interface{}, marshal marshalFunc) error {
+	b, err := marshal(item)
+	if err != nil {
+		return err
+	}
+	_, err = os.Stdout.Write(b)
+	if err != nil {
+		return fmt.Errorf("error writing to stdout: %v", err)
+	}
+	return nil
+}
+
+// v must be a pointer to a marshalable object
+func marshalYaml(v interface{}) ([]byte, error) {
+	y, err := api.ToYaml(v)
+	if err != nil {
+		return nil, fmt.Errorf("error marshaling yaml: %v", err)
+	}
+	return y, nil
+}
+
+// v must be a pointer to a marshalable object
+func marshalJSON(v interface{}) ([]byte, error) {
+	j, err := json.Marshal(v)
+	if err != nil {
+		return nil, fmt.Errorf("error marshaling json: %v", err)
+	}
+	return j, nil
 }
