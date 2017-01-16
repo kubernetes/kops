60bd9e7356e24bd8e51ed8e2de51e62107a204f4
chrislovecnm <clove@cnmconsulting.net>
2017-01-16 13:11:27 -0700

    updates using a pointer now

--- pkg/model/components/kubecontrollermanager_test.go
+++ pkg/model/components/kubecontrollermanager_test.go
@@ -1,10 +1,27 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package components
 
 import (
-	api "k8s.io/kops/pkg/apis/kops"
-	"k8s.io/kops/upup/pkg/fi"
 	"testing"
 	"time"
+
+	api "k8s.io/kops/pkg/apis/kops"
+	"k8s.io/kops/upup/pkg/fi"
 )
 
 type ClusterParams struct {
@@ -69,9 +86,8 @@ func Test_Build_KCM_Builder_Lower_Version(t *testing.T) {
 
 }
 
-
 func Test_Build_KCM_Builder_High_Enough_Version(t *testing.T) {
-	c := buildCluster(ClusterParams{KubernetesVersion:"1.4.8"})
+	c := buildCluster(ClusterParams{KubernetesVersion: "1.4.8"})
 
 	kcm := &KubeControllerManagerOptionsBuilder{
 		Context: &OptionsContext{
@@ -90,7 +106,4 @@ func Test_Build_KCM_Builder_High_Enough_Version(t *testing.T) {
 		t.Fatalf("k-c-m builder should be set to 1m - %s", spec.KubeControllerManager.AttachDetachReconcileSyncPeriod.Duration.String())
 	}
 
-
-
 }
-
