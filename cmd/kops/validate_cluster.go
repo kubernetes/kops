01497760c79fcbd0911e93da78294b0a57ce2059
chrislovecnm <clove@cnmconsulting.net>
2017-02-07 15:45:37 -0700

    Updates for pr review and backing out validate_cluster changes

--- cmd/kops/validate_cluster.go
+++ cmd/kops/validate_cluster.go
@@ -171,8 +171,7 @@ func RunValidateCluster(f *util.Factory, cmd *cobra.Command, args []string, out 
 	} else {
 		// do we need to print which instance group is not ready?
 		// nodes are going to be a pain
-		// FIXME: format still not making sense
-		// FIXME: not sending back how many are running with less than IG are running
+		fmt.Fprint(out, "\nValidation Failed\n")
 		fmt.Fprintf(out, "Master(s) Not Ready %d out of %d.\n", len(validationCluster.MastersNotReadyArray), validationCluster.MastersCount)
 		fmt.Fprintf(out, "Node(s) Not Ready   %d out of %d.\n", len(validationCluster.NodesNotReadyArray), validationCluster.NodesCount)
 		return fmt.Errorf("Your cluster %s is NOT ready.\n", cluster.ObjectMeta.Name)
