b506ca651c619f5cbdb3679bf6f16a62dc305706
Manuel de Brito Fontes <aledbf@gmail.com>
2017-02-10 09:01:03 -0300

    Map kubelet flag volume-plugin-dir

--- pkg/apis/kops/componentconfig.go
+++ pkg/apis/kops/componentconfig.go
@@ -311,6 +311,9 @@ type KubeletConfigSpec struct {
 	EvictionMaxPodGracePeriod int32 `json:"evictionMaxPodGracePeriod,omitempty" flag:"eviction-max-pod-grace-period" flag-empty:"0"`
 	// Comma-delimited list of minimum reclaims (e.g. imagefs.available=2Gi) that describes the minimum amount of resource the kubelet will reclaim when performing a pod eviction if that resource is under pressure.
 	EvictionMinimumReclaim string `json:"evictionMinimumReclaim,omitempty" flag:"eviction-minimum-reclaim"`
+
+	// The full path of the directory in which to search for additional third party volume plugins
+	VolumePluginDirectory string `json:"volumePluginDirectory,omitempty" flag:"volume-plugin-dir"`
 }
 
 type KubeProxyConfig struct {
