8c7d683698c5bdb1e9a337c59a1f914d089ed314
Justin Santa Barbara <justin@fathomdb.com>
2017-02-11 11:25:00 -0500

    Update calico manifest version

--- upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
+++ upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
@@ -192,7 +192,7 @@ func (b *BootstrapChannelBuilder) buildManifest() (*channelsapi.Addons, map[stri
 
 	if b.cluster.Spec.Networking.Calico != nil {
 		key := "networking.projectcalico.org"
-		version := "2.0"
+		version := "2.0.2"
 
 		// TODO: Create configuration object for cni providers (maybe create it but orphan it)?
 		location := key + "/v" + version + ".yaml"
