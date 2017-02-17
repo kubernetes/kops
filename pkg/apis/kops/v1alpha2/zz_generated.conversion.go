38b97e505cae2c80dc92441b97dfbc9f054987f3
Seth Pollack <spollack@beenverified.com>
2017-02-17 14:55:35 -0500

    change variable names

--- pkg/apis/kops/v1alpha2/zz_generated.conversion.go
+++ pkg/apis/kops/v1alpha2/zz_generated.conversion.go
@@ -1100,6 +1100,10 @@ func autoConvert_v1alpha2_KubeAPIServerConfig_To_kops_KubeAPIServerConfig(in *Ku
 	out.OIDCIssuerURL = in.OIDCIssuerURL
 	out.OIDCClientID = in.OIDCClientID
 	out.OIDCCAFile = in.OIDCCAFile
+	out.AuditLogPath = in.AuditLogPath
+	out.AuditLogMaxAge = in.AuditLogMaxAge
+	out.AuditLogMaxBackups = in.AuditLogMaxBackups
+	out.AuditLogMaxSize = in.AuditLogMaxSize
 	out.AuthorizationMode = in.AuthorizationMode
 	out.AuthorizationRBACSuperUser = in.AuthorizationRBACSuperUser
 	return nil
@@ -1137,6 +1141,10 @@ func autoConvert_kops_KubeAPIServerConfig_To_v1alpha2_KubeAPIServerConfig(in *ko
 	out.OIDCIssuerURL = in.OIDCIssuerURL
 	out.OIDCClientID = in.OIDCClientID
 	out.OIDCCAFile = in.OIDCCAFile
+	out.AuditLogPath = in.AuditLogPath
+	out.AuditLogMaxAge = in.AuditLogMaxAge
+	out.AuditLogMaxBackups = in.AuditLogMaxBackups
+	out.AuditLogMaxSize = in.AuditLogMaxSize
 	out.AuthorizationMode = in.AuthorizationMode
 	out.AuthorizationRBACSuperUser = in.AuthorizationRBACSuperUser
 	return nil
