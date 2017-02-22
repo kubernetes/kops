858293d9b99176ffcf3667dd2a3d73c463c0e8e7
Robin Percy <robin@trinimbus.com>
2017-02-22 12:20:31 -0800

    Now applying the tags to IGs at render time.
    - Previous method would have caused issues with the way tags are used
      for filtering resources.
    - Updated docs and comments to only refer to instance groups, rather
      than all AWS resources

--- pkg/apis/kops/v1alpha1/cluster.go
+++ pkg/apis/kops/v1alpha1/cluster.go
@@ -236,7 +236,7 @@ type ClusterSpec struct {
 	// API field controls how the API is exposed outside the cluster
 	API *AccessSpec `json:"api,omitempty"`
 
-	// Tags for AWS resources
+	// Tags for AWS instance groups
 	CloudLabels map[string]string `json:"cloudLabels,omitempty"`
 }
 
