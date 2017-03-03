75b77c8f0f738813745679820c822abc2bd8ce93
pdh <patrick.d.hull@gmail.com>
2017-03-03 15:14:18 -0800

    Allows additional Subject Alternate Names

--- pkg/apis/kops/v1alpha2/zz_generated.conversion.go
+++ pkg/apis/kops/v1alpha2/zz_generated.conversion.go
@@ -633,6 +633,7 @@ func autoConvert_v1alpha2_ClusterSpec_To_kops_ClusterSpec(in *ClusterSpec, out *
 	out.Project = in.Project
 	out.MasterPublicName = in.MasterPublicName
 	out.MasterInternalName = in.MasterInternalName
+	out.AdditionalSANs = in.AdditionalSANs
 	out.NetworkCIDR = in.NetworkCIDR
 	out.NetworkID = in.NetworkID
 	if in.Topology != nil {
@@ -883,6 +884,7 @@ func autoConvert_kops_ClusterSpec_To_v1alpha2_ClusterSpec(in *kops.ClusterSpec, 
 	out.Project = in.Project
 	out.MasterPublicName = in.MasterPublicName
 	out.MasterInternalName = in.MasterInternalName
+	out.AdditionalSANs = in.AdditionalSANs
 	out.NetworkCIDR = in.NetworkCIDR
 	out.NetworkID = in.NetworkID
 	if in.Topology != nil {
