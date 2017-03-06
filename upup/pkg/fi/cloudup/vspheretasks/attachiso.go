a2c48f761a3d4fddaac32f5a103276dd77a3d129
SandeepPissay <ssrinivas@vmware.com>
2017-03-06 13:16:46 -0800

    Fixed the formatting and added copyright headers to new files

--- upup/pkg/fi/cloudup/vspheretasks/attachiso.go
+++ upup/pkg/fi/cloudup/vspheretasks/attachiso.go
@@ -1,16 +1,32 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package vspheretasks
 
 import (
-	"k8s.io/kops/upup/pkg/fi"
 	"github.com/golang/glog"
+	"k8s.io/kops/upup/pkg/fi"
 	"k8s.io/kops/upup/pkg/fi/cloudup/vsphere"
 )
 
 // AttachISO represents the cloud-init ISO file attached to a VMware VM
 //go:generate fitask -type=AttachISO
 type AttachISO struct {
-	Name       *string
-	VM *VirtualMachine
+	Name *string
+	VM   *VirtualMachine
 }
 
 var _ fi.HasName = &AttachISO{}
@@ -44,4 +60,3 @@ func (_ *AttachISO) RenderVC(t *vsphere.VSphereAPITarget, a, e, changes *AttachI
 	glog.Info("AttachISO.RenderVC invoked!")
 	return nil
 }
-
