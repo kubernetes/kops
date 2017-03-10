716349bf225351d663d4b290d7046d1016d4df4d
Miao Luo <miaol@vmware.com>
2017-03-10 11:06:41 -0800

    Fix the type of vSphere fields in CloudConfig and update auto-generated conversion files.
    To make sure the vSphere fields in CloudConfiguration can be successfully serialized
    and output into config file.

--- pkg/apis/kops/v1alpha2/componentconfig.go
+++ pkg/apis/kops/v1alpha2/componentconfig.go
@@ -299,7 +299,7 @@ type CloudConfiguration struct {
 	NodeInstancePrefix *string `json:"nodeInstancePrefix,omitempty"`
 
 	// vSphere cloud-config specs
-	VSphereServer       string `json:"vSphereServer,omitempty"`
-	VSphereDatacenter   string `json:"vSphereDatacenter,omitempty"`
-	VSphereResourcePool string `json:"vSphereResourcePool,omitempty"`
+	VSphereServer       *string `json:"vSphereServer,omitempty"`
+	VSphereDatacenter   *string `json:"vSphereDatacenter,omitempty"`
+	VSphereResourcePool *string `json:"vSphereResourcePool,omitempty"`
 }
