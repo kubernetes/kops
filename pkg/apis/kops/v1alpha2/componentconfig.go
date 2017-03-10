89ab750e014c9cd0cec8c84ebd76098414186c21
Seth Pollack <seth@sethpollack.net>
2017-03-09 19:47:49 -0500

    add webhook authenticator flags

--- pkg/apis/kops/v1alpha2/componentconfig.go
+++ pkg/apis/kops/v1alpha2/componentconfig.go
@@ -206,6 +206,11 @@ type KubeAPIServerConfig struct {
 	// The maximum size in megabytes of the audit log file before it gets rotated. Defaults to 100MB.
 	AuditLogMaxSize *int32 `json:"auditLogMaxSize,omitempty" flag:"audit-log-maxsize"`
 
+	// File with webhook configuration for token authentication in kubeconfig format. The API server will query the remote service to determine authentication for bearer tokens.
+	AuthenticationTokenWebhookConfigFile *string `json:"authenticationTokenWebhookConfigFile,omitempty" flag:"authentication-token-webhook-config-file"`
+	// The duration to cache responses from the webhook token authenticator. Default is 2m. (default 2m0s)
+	AuthenticationTokenWebhookCacheTtl *metav1.Duration `json:"authenticationTokenWebhookCacheTtl,omitempty" flag:"authentication-token-webhook-cache-ttl"`
+
 	AuthorizationMode          *string `json:"authorizationMode,omitempty" flag:"authorization-mode"`
 	AuthorizationRBACSuperUser *string `json:"authorizationRbacSuperUser,omitempty" flag:"authorization-rbac-super-user"`
 }
