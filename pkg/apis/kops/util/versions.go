e6fb0a3d67ea2d344ce10cda3c5fa9874776ab52
Justin Santa Barbara <justin@fathomdb.com>
2017-03-21 23:59:45 -0400

    Move kube-scheduler to code & RBAC

--- pkg/apis/kops/util/versions.go
+++ pkg/apis/kops/util/versions.go
@@ -57,3 +57,25 @@ func ParseKubernetesVersion(version string) (*semver.Version, error) {
 
 	return &sv, nil
 }
+
+// TODO: Convert to our own KubernetesVersion type?
+
+func IsKubernetesGTE(version string, k8sVersion semver.Version) bool {
+	// The string-arg is a little annoying, but simplifies the calling code!
+	switch version {
+	case "1.2":
+		return k8sVersion.Major > 1 || (k8sVersion.Major == 1 && k8sVersion.Minor >= 2)
+	case "1.3":
+		return k8sVersion.Major > 1 || (k8sVersion.Major == 1 && k8sVersion.Minor >= 3)
+	case "1.4":
+		return k8sVersion.Major > 1 || (k8sVersion.Major == 1 && k8sVersion.Minor >= 4)
+	case "1.5":
+		return k8sVersion.Major > 1 || (k8sVersion.Major == 1 && k8sVersion.Minor >= 5)
+	case "1.6":
+		return k8sVersion.Major > 1 || (k8sVersion.Major == 1 && k8sVersion.Minor >= 6)
+	case "1.7":
+		return k8sVersion.Major > 1 || (k8sVersion.Major == 1 && k8sVersion.Minor >= 7)
+	default:
+		panic(fmt.Sprintf("IsKubernetesGTE not supported with version %q", version))
+	}
+}
