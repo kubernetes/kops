f11c91a09a441277d7839f74d44ef6d16e0a51df
SandeepPissay <SandeepPissay@users.noreply.github.com>
2017-03-23 11:24:23 -0700

    Implemented AttachISO task (#4)
    AttachISO task creates the user-data/meta-data cloud init files and creates cloud-init.iso file using "genisoimage" tool. It then uploads it to the datastore where the master/worker VM resides and inserts it into the cd-rom device of the master/worker VM. When the master/worker VM powers on, the cloud-init package in it runs the bootstrap script that downloads nodeup and runs it.
    
    Also removed redundant VirtualMachineModelBuilder that does nothing.
    
    Testing done:
    1. Tested end to end that the master and worker VMs executes the cloud-init script successfully.
    2, "make ci" is successful.

--- upup/pkg/fi/cloudup/vspheretasks/attachiso.go
+++ upup/pkg/fi/cloudup/vspheretasks/attachiso.go
@@ -17,19 +17,43 @@ limitations under the License.
 package vspheretasks
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/golang/glog"
+	"github.com/pborman/uuid"
+	"io/ioutil"
+	"k8s.io/kops/pkg/apis/kops"
+	"k8s.io/kops/pkg/model"
 	"k8s.io/kops/upup/pkg/fi"
 	"k8s.io/kops/upup/pkg/fi/cloudup/vsphere"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
 )
 
 // AttachISO represents the cloud-init ISO file attached to a VMware VM
 //go:generate fitask -type=AttachISO
 type AttachISO struct {
-	Name *string
-	VM   *VirtualMachine
+	Name            *string
+	VM              *VirtualMachine
+	IG              *kops.InstanceGroup
+	BootstrapScript *model.BootstrapScript
 }
 
 var _ fi.HasName = &AttachISO{}
+var _ fi.HasDependencies = &AttachISO{}
+
+func (o *AttachISO) GetDependencies(tasks map[string]fi.Task) []fi.Task {
+	var deps []fi.Task
+	vmCreateTask := tasks["VirtualMachine/"+*o.VM.Name]
+	if vmCreateTask == nil {
+		glog.Fatalf("Unable to find create VM task %s dependency for AttachISO %s", *o.VM.Name, *o.Name)
+	}
+	deps = append(deps, vmCreateTask)
+	return deps
+}
 
 // GetName returns the Name of the object, implementing fi.HasName
 func (o *AttachISO) GetName() *string {
@@ -56,7 +80,85 @@ func (_ *AttachISO) CheckChanges(a, e, changes *AttachISO) error {
 	return nil
 }
 
-func (_ *AttachISO) RenderVC(t *vsphere.VSphereAPITarget, a, e, changes *AttachISO) error {
-	glog.Info("AttachISO.RenderVC invoked!")
+func (_ *AttachISO) RenderVSphere(t *vsphere.VSphereAPITarget, a, e, changes *AttachISO) error {
+	startupScript, err := changes.BootstrapScript.ResourceNodeUp(changes.IG)
+	startupStr, err := startupScript.AsString()
+	if err != nil {
+		return fmt.Errorf("error rendering startup script: %v", err)
+	}
+	dir, err := ioutil.TempDir("", *changes.VM.Name)
+	defer os.RemoveAll(dir)
+
+	isoFile := createISO(changes, startupStr, dir)
+	err = t.Cloud.UploadAndAttachISO(changes.VM.Name, isoFile)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func createUserData(startupStr string, dir string) {
+	// Update the startup script to add the extra spaces for
+	// indentation when copied to the user-data file.
+	strArray := strings.Split(startupStr, "\n")
+	for i, str := range strArray {
+		if len(str) > 0 {
+			strArray[i] = "       " + str
+		}
+	}
+	startupStr = strings.Join(strArray, "\n")
+
+	data := strings.Replace(userDataTemplate, "$SCRIPT", startupStr, -1)
+	userDataFile := filepath.Join(dir, "user-data")
+	glog.V(4).Infof("User data file content: %s", data)
+
+	if err := ioutil.WriteFile(userDataFile, []byte(data), 0644); err != nil {
+		glog.Fatalf("Unable to write user-data into file %s", userDataFile)
+	}
+	return
+}
+
+func createMetaData(dir string, vmName string) {
+	data := strings.Replace(metaDataTemplate, "$INSTANCE_ID", uuid.NewUUID().String(), -1)
+	data = strings.Replace(data, "$LOCAL_HOST_NAME", vmName, -1)
+
+	glog.V(4).Infof("Meta data file content: %s", string(data))
+
+	metaDataFile := filepath.Join(dir, "meta-data")
+	if err := ioutil.WriteFile(metaDataFile, []byte(data), 0644); err != nil {
+		glog.Fatalf("Unable to write meta-data into file %s", metaDataFile)
+	}
+	return
+}
+
+func createISO(changes *AttachISO, startupStr string, dir string) string {
+	createUserData(startupStr, dir)
+	createMetaData(dir, *changes.VM.Name)
+
+	isoFile := filepath.Join(dir, *changes.VM.Name+".iso")
+	var commandName string
+
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		commandName = "mkisofs"
+	case "linux":
+		commandName = "genisoimage"
+
+	default:
+		glog.Fatalf("Cannot generate ISO file %s. Unsupported operation system (%s)!!!", isoFile, os)
+	}
+	cmd := exec.Command(commandName, "-o", isoFile, "-volid", "cidata", "-joliet", "-rock", dir)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+	if err != nil {
+		glog.Fatalf("Error %s occurred while executing command %+v", err, cmd)
+	}
+	glog.V(4).Infof("%s std output : %s\n", commandName, out.String())
+	glog.V(4).Infof("%s std error : %s\n", commandName, stderr.String())
+	return isoFile
 }
