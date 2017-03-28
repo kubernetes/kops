687ff420b49cc980e79f9687e8c6252cd029c31a
Kris Nova <kris@nivenly.com>
2017-03-28 06:09:04 +0200

    Reset server main.go and Makefile to 71e78cafc186b715a58c24788e9714abced26f85

--- cmd/kops-server/main.go
+++ cmd/kops-server/main.go
@@ -1,12 +1,9 @@
 /*
 Copyright 2016 The Kubernetes Authors.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,20 +17,15 @@ import (
 	"flag"
 	"os"
 	"runtime"
-	"math/rand"
-	"time"
-	"fmt"
-
 
 	"k8s.io/kops/pkg/apiserver/cmd/server"
 	cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
 	"k8s.io/kubernetes/pkg/util/logs"
+	"math/rand"
+	"time"
 )
 
 func main() {
-
-	fmt.Println("Main is running.")
-
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	logs.InitLogs()
@@ -45,10 +37,6 @@ func main() {
 
 	cmd := server.NewCommandStartKopsServer(os.Stdout, os.Stderr)
 	cmd.Flags().AddGoFlagSet(flag.CommandLine)
-
-	// Avoid glog warnings
-	flag.CommandLine.Parse([]string{})
-
 	if err := cmd.Execute(); err != nil {
 		cmdutil.CheckErr(err)
 	}
