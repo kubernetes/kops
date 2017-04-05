0448bf511d919a61209f9ad6eb635eb5b606794e
Otto Yiu <otto@live.ca>
2017-04-05 04:29:27 -0700

    Update Project Calico from 2.0.2 to 2.1.1

--- upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
+++ upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
@@ -243,7 +243,7 @@ func (b *BootstrapChannelBuilder) buildManifest() (*channelsapi.Addons, map[stri
 
 	if b.cluster.Spec.Networking.Calico != nil {
 		key := "networking.projectcalico.org"
-		version := "2.0.2"
+		version := "2.1.1"
 
 		// TODO: Create configuration object for cni providers (maybe create it but orphan it)?
 		location := key + "/v" + version + ".yaml"
