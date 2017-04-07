3273277ef5ce0f0fec317ceb71f05fbfea2b3c83
Justin Santa Barbara <justin@fathomdb.com>
2017-04-06 21:36:41 -0400

    Cleanup get command

--- cmd/kops/root.go
+++ cmd/kops/root.go
@@ -131,6 +131,7 @@ func NewCmdRoot(f *util.Factory, out io.Writer) *cobra.Command {
 	cmd.AddCommand(NewCmdDelete(f, out))
 	cmd.AddCommand(NewCmdEdit(f, out))
 	cmd.AddCommand(NewCmdExport(f, out))
+	cmd.AddCommand(NewCmdGet(f, out))
 	cmd.AddCommand(NewCmdUpdate(f, out))
 	cmd.AddCommand(NewCmdReplace(f, out))
 	cmd.AddCommand(NewCmdRollingUpdate(f, out))
