44cd82133b301f245af147098b71edee91fd25ec
chrislovecnm <clove@cnmconsulting.net>
2017-04-22 14:36:44 -0600

    Updating CNI version for k8s 1.6

--- upup/pkg/fi/cloudup/networking.go
+++ upup/pkg/fi/cloudup/networking.go
@@ -17,9 +17,13 @@ limitations under the License.
 package cloudup
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/blang/semver"
 	"github.com/golang/glog"
 	api "k8s.io/kops/pkg/apis/kops"
-	"os"
+	"k8s.io/kops/pkg/apis/kops/util"
 )
 
 func usesCNI(c *api.Cluster) bool {
@@ -78,19 +82,40 @@ func usesCNI(c *api.Cluster) bool {
 // https://github.com/kubernetes/kops/issues/724
 // https://github.com/kubernetes/kops/issues/626
 // https://github.com/kubernetes/kubernetes/issues/30338
+
 const (
-	defaultCNIAsset           = "https://storage.googleapis.com/kubernetes-release/network-plugins/cni-07a8a28637e97b22eb8dfe710eeae1344f69d16e.tar.gz"
-	defaultCNIAssetHashString = "19d49f7b2b99cd2493d5ae0ace896c64e289ccbb"
-	ENV_VAR_CNI_VERSION_URL   = "CNI_VERSION_URL"
+	// 1.5.x k8s uses release 07a8a28637e97b22eb8dfe710eeae1344f69d16e
+	defaultCNIAssetK8s1_5           = "https://storage.googleapis.com/kubernetes-release/network-plugins/cni-07a8a28637e97b22eb8dfe710eeae1344f69d16e.tar.gz"
+	defaultCNIAssetHashStringK8s1_5 = "19d49f7b2b99cd2493d5ae0ace896c64e289ccbb"
+
+	// 1.6.x k8s uses release 0799f5732f2a11b329d9e3d51b9c8f2e3759f2ff
+	defaultCNIAssetK8s1_6           = "https://storage.googleapis.com/kubernetes-release/network-plugins/cni-0799f5732f2a11b329d9e3d51b9c8f2e3759f2ff.tar.gz"
+	defaultCNIAssetHashStringK8s1_6 = "1d9788b0f5420e1a219aad2cb8681823fc515e7c"
+
+	// Environment variable for overriding CNI url
+	ENV_VAR_CNI_VERSION_URL = "CNI_VERSION_URL"
 )
 
-func findCNIAssets(c *api.Cluster) (string, string) {
+func findCNIAssets(c *api.Cluster) (string, string, error) {
 
 	if cniVersionURL := os.Getenv(ENV_VAR_CNI_VERSION_URL); cniVersionURL != "" {
 		glog.Infof("Using CNI asset version %q, as set in %s", cniVersionURL, ENV_VAR_CNI_VERSION_URL)
-		return cniVersionURL, ""
+		return cniVersionURL, "", nil
 	}
 
-	glog.V(2).Infof("Adding default CNI asset: %s", defaultCNIAsset)
-	return defaultCNIAsset, defaultCNIAssetHashString
+	sv, err := util.ParseKubernetesVersion(c.Spec.KubernetesVersion)
+	if err != nil {
+		return "", "", fmt.Errorf("Failed to lookup kubernetes version: %v", err)
+	}
+
+	sv.Pre = nil
+	sv.Build = nil
+
+	if sv.GTE(semver.Version{Major: 1, Minor: 6, Patch: 0, Pre: nil, Build: nil}) {
+		glog.V(2).Infof("Adding default CNI asset: %s", defaultCNIAssetK8s1_6)
+		return defaultCNIAssetK8s1_6, defaultCNIAssetHashStringK8s1_6, nil
+	}
+
+	glog.V(2).Infof("Adding default CNI asset: %s", defaultCNIAssetK8s1_5)
+	return defaultCNIAssetK8s1_5, defaultCNIAssetHashStringK8s1_5, nil
 }
