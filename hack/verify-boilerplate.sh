2a1b6c9af76f85cf765ad18c5f3e8d43aad3ee9b
chrislovecnm <clove@cnmconsulting.net>
2017-04-30 23:42:33 -0600

    updates

--- hack/verify-boilerplate.sh
+++ hack/verify-boilerplate.sh
@@ -21,7 +21,6 @@ set -x
 boiler="${KUBE_ROOT}/hack/boilerplate/boilerplate.py $@"
 
 files_need_boilerplate=( `${boiler}` )
-echo files_need_boilerplate=$files_need_boilerplate
 
 if [[ -z ${files_need_boilerplate+x} ]]; then
     echo "Boilerplate headers validated"
