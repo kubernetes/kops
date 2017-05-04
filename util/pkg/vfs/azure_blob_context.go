1c492bcdc3422d7172ed35d07862ceec8f32eb23
Kris Nova <kris@nivenly.com>
2017-05-03 19:37:42 -0600

    More work on the VFS system for Azure

--- util/pkg/vfs/azure_blob_context.go
+++ util/pkg/vfs/azure_blob_context.go
@@ -1,8 +1,44 @@
 package vfs
 
+import (
+	"github.com/Azure/azure-sdk-for-go/storage"
+	"os"
+	"github.com/pkg/errors"
+	"fmt"
+	"sync"
+)
+
 type AzureBlobContext struct {
+	mutex  sync.Mutex
+	client storage.Client
 }
 
 func NewAzureBlobContext() *AzureBlobContext {
 	return &AzureBlobContext{}
+}
+
+// getClient holds the primary connection logic for connecting to the Azure
+// API via the Azure Go SDK
+func (a *AzureBlobContext) getClient() (storage.Client, error) {
+	a.mutex.Lock()
+	defer a.mutex.Unlock()
+
+	if a.client == nil {
+		// Now we assume that they are defined as environmental variables
+		name := os.Getenv("AZURE_STORAGE_ACCOUNT_NAME")
+		if name == "" {
+			return nil, errors.New("invalid or empty value for $AZURE_STORAGE_ACCOUNT_NAME")
+		}
+		key := os.Getenv("AZURE_STORAGE_ACCOUNT_KEY")
+		if key == "" {
+			return nil, errors.New("invalid or empty value for $AZURE_STORAGE_ACCOUNT_KEY")
+		}
+
+		client, err := storage.NewBasicClient(name, key)
+		if err != nil {
+			return nil, fmt.Errorf("unable to create Azure connection: %v", err)
+		}
+		a.client = client
+	}
+	return a.client, nil
 }
--- util/pkg/vfs/azure_blob_fs.go
+++ util/pkg/vfs/azure_blob_fs.go
@@ -1,19 +1,31 @@
 package vfs
 
+import "path"
+
+// https://kopsdevel.blob.core.windows.net
 type AzureBlobPath struct {
-	container string
-	key       string
+	azureBlobContext *AzureBlobContext
+	container        string
+	key              string
 }
 
 func newAzureBlobPath(azureBlobCtx *AzureBlobContext, container string, key string) *AzureBlobPath {
 	return &AzureBlobPath{
-		container: container,
-		key:       key,
+		container:        container,
+		key:              key,
+		azureBlobContext: azureBlobCtx,
 	}
 }
 
-func (a *AzureBlobPath) Join(relativePath ...string) Path {
-	return &AzureBlobPath{}
+func (a *AzureBlobPath) Join (relativePath ...string) Path {
+	args := []string{a.key}
+	args = append(args, relativePath...)
+	joined := path.Join(args...)
+	return &AzureBlobPath{
+		azureBlobContext: a.azureBlobContext,
+		container:        a.container,
+		key:              joined,
+	}
 }
 
 func (a *AzureBlobPath) ReadFile() ([]byte, error) {
@@ -35,7 +47,7 @@ func (a *AzureBlobPath) Base() string {
 	return ""
 }
 func (a *AzureBlobPath) Path() string {
-	return ""
+	return "https://" + a.container + ".blob.core.windows.net/" + a.container + "/" + a.key
 }
 func (a *AzureBlobPath) ReadDir() ([]Path, error) {
 	var paths []Path
