eecd2072b21874735768530a746aaae8000a283b
andrewsykim <andrew@wattpad.com>
2017-05-26 15:51:19 -0400

    Generalize Cluster Resources

--- cmd/kops/delete_cluster.go
+++ cmd/kops/delete_cluster.go
@@ -18,7 +18,6 @@ package main
 
 import (
 	"fmt"
-
 	"io"
 
 	"github.com/golang/glog"
@@ -143,7 +142,7 @@ func RunDeleteCluster(f *util.Factory, out io.Writer, options *DeleteClusterOpti
 		}
 
 		// Todo lets make this smart enough to detect the cloud and switch on the ClusterResources interface
-		d := &resources.AwsCluster{}
+		d := &resources.ClusterResources{}
 		d.ClusterName = clusterName
 		d.Cloud = cloud
 
