71da609e8403d0ae7b7d2e692f5d2386af0904f6
Hanfei Shen <qqshfox@gmail.com>
2017-07-09 09:15:57 +0800

    support china region

--- upup/pkg/fi/cloudup/template_functions.go
+++ upup/pkg/fi/cloudup/template_functions.go
@@ -30,6 +30,7 @@ package cloudup
 import (
 	"encoding/base64"
 	"fmt"
+	"os"
 	"strings"
 	"text/template"
 
@@ -138,7 +139,11 @@ func (tf *TemplateFunctions) DnsControllerArgv() ([]string, error) {
 
 	switch kops.CloudProviderID(tf.cluster.Spec.CloudProvider) {
 	case kops.CloudProviderAWS:
-		argv = append(argv, "--dns=aws-route53")
+		if strings.HasPrefix(os.Getenv("AWS_REGION"), "cn-") {
+			argv = append(argv, "--dns=gossip")
+		} else {
+			argv = append(argv, "--dns=aws-route53")
+		}
 	case kops.CloudProviderGCE:
 		argv = append(argv, "--dns=google-clouddns")
 	case kops.CloudProviderVSphere:
