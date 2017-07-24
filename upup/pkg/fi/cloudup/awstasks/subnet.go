64f0920c8b869bc60c357e48747e922ca267b82f
Justin Santa Barbara <justin@fathomdb.com>
2017-07-24 10:37:07 -0400

    Add integration tests for shared subnet & VPC

--- upup/pkg/fi/cloudup/awstasks/subnet.go
+++ upup/pkg/fi/cloudup/awstasks/subnet.go
@@ -210,6 +210,9 @@ func (_ *Subnet) RenderTerraform(t *terraform.TerraformTarget, a, e, changes *Su
 	shared := fi.BoolValue(e.Shared)
 	if shared {
 		// Not terraform owned / managed
+		// We probably shouldn't output subnet_ids only in this case - we normally output them by role,
+		// but removing it now might break people.  We could always output subnet_ids though, if we
+		// ever get a request for that.
 		return t.AddOutputVariableArray("subnet_ids", terraform.LiteralFromStringValue(*e.ID))
 	}
 
