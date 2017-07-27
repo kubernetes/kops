3583b84e9e278b64f07ddef7b859beb2b9d31c57
Rohith <gambol99@gmail.com>
2017-07-28 00:40:21 +0100

    Custom Manifests via Hooks
    The present implementation of hooks only perform for docker exec, which isn't that flexible. This PR permits the user to greater customize systemd units on the instances
    
    - cleaned up the manifest code, added tests and permit setting a section raw
    - added the ability to filter hooks via master and node roles
    - updated the documentation to reflect the changes
    - cleaned up some of the vetting issues

--- nodeup/pkg/model/hooks.go
+++ nodeup/pkg/model/hooks.go
@@ -17,14 +17,16 @@ limitations under the License.
 package model
 
 import (
-	"strconv"
+	"errors"
+	"fmt"
 	"strings"
 
-	"github.com/golang/glog"
 	"k8s.io/kops/pkg/apis/kops"
 	"k8s.io/kops/pkg/systemd"
 	"k8s.io/kops/upup/pkg/fi"
 	"k8s.io/kops/upup/pkg/fi/nodeup/nodetasks"
+
+	"github.com/golang/glog"
 )
 
 // HookBuilder configures the hooks
@@ -34,37 +36,98 @@ type HookBuilder struct {
 
 var _ fi.ModelBuilder = &HookBuilder{}
 
-func (b *HookBuilder) Build(c *fi.ModelBuilderContext) error {
-	for i := range b.Cluster.Spec.Hooks {
-		hook := &b.Cluster.Spec.Hooks[i]
+// Build is responsible for implementing the cluster hook
+func (h *HookBuilder) Build(c *fi.ModelBuilderContext) error {
+	// iterate the hooks and render the systemd units
+	for i := range h.Cluster.Spec.Hooks {
+		hook := &h.Cluster.Spec.Hooks[i]
 
-		// TODO: Allow (alphanumeric?) names
-		name := strconv.Itoa(i + 1)
+		// filter out on master and node flags if required
+		if (hook.MasterOnly && !h.IsMaster) || (hook.NodeOnly && h.IsMaster) {
+			continue
+		}
 
-		service, err := b.buildSystemdService(name, hook)
+		// are we disabling the service?
+		if hook.Disabled {
+			enabled := false
+			managed := true
+			c.AddTask(&nodetasks.Service{
+				Name:        hook.Name,
+				ManageState: &managed,
+				Enabled:     &enabled,
+				Running:     &enabled,
+			})
+			continue
+		}
+
+		// use the default naming convention - kops-hook-<index>
+		name := fmt.Sprintf("kops-hook-%d", i)
+		if hook.Name != "" {
+			name = hook.Name
+		}
+
+		// generate the systemd service
+		service, err := h.buildSystemdService(name, hook)
 		if err != nil {
 			return err
 		}
-		c.AddTask(service)
+
+		if service != nil {
+			c.AddTask(service)
+		}
 	}
 
 	return nil
 }
 
-func (b *HookBuilder) buildSystemdService(name string, hook *kops.HookSpec) (*nodetasks.Service, error) {
-	// We could give the container a kubeconfig, but we would probably do better to have a real pod / daemonset / job at that point
-	execContainer := hook.ExecContainer
-	if execContainer == nil {
-		glog.Warningf("No ExecContainer found for hook: %v", hook)
+// buildSystemdService is responsible for generating the service
+func (h *HookBuilder) buildSystemdService(name string, hook *kops.HookSpec) (*nodetasks.Service, error) {
+	// perform some basic validation
+	if hook.ExecContainer == nil && hook.Manifest == "" {
+		glog.Warningf("hook: %s has neither a raw unit or exec image configured", name)
 		return nil, nil
 	}
+	if hook.ExecContainer != nil {
+		if err := isValidExecContainerAction(hook.ExecContainer); err != nil {
+			glog.Warningf("invalid hook action, name: %s, error: %v", name, err)
+			return nil, nil
+		}
+	}
+	// build the base unit file
+	unit := &systemd.Manifest{}
+	unit.Set("Unit", "Description", "Kops Hook "+name)
 
-	image := strings.TrimSpace(execContainer.Image)
-	if image == "" {
-		glog.Warningf("No Image found for hook: %v", hook)
-		return nil, nil
+	// add any service dependencies to the unit
+	for _, x := range hook.Requires {
+		unit.Set("Unit", "Requires", x)
+	}
+	for _, x := range hook.Before {
+		unit.Set("Unit", "Before", x)
 	}
 
+	// are we a raw unit file or a docker exec?
+	switch hook.ExecContainer {
+	case nil:
+		unit.SetSection("Service", hook.Manifest)
+	default:
+		if err := h.buildDockerService(unit, hook); err != nil {
+			return nil, err
+		}
+	}
+
+	service := &nodetasks.Service{
+		Name:       name,
+		Definition: s(unit.Render()),
+	}
+
+	service.InitDefaults()
+
+	return service, nil
+}
+
+// buildDockerService is responsible for generating a docker exec unit file
+func (h *HookBuilder) buildDockerService(unit *systemd.Manifest, hook *kops.HookSpec) error {
+	// the docker command line
 	dockerArgs := []string{
 		"/usr/bin/docker",
 		"run",
@@ -75,38 +138,26 @@ func (b *HookBuilder) buildSystemdService(name string, hook *kops.HookSpec) (*no
 		"--privileged",
 		hook.ExecContainer.Image,
 	}
-
-	dockerArgs = append(dockerArgs, execContainer.Command...)
+	dockerArgs = append(dockerArgs, hook.ExecContainer.Command...)
 
 	dockerRunCommand := systemd.EscapeCommand(dockerArgs)
 	dockerPullCommand := systemd.EscapeCommand([]string{"/usr/bin/docker", "pull", hook.ExecContainer.Image})
 
-	manifest := &systemd.Manifest{}
-	manifest.Set("Unit", "Description", "Kops Hook "+name)
-	if hook.Documentation != "" {
-		manifest.Set("Unit", "Documentation", hook.Documentation)
-	}
-	for _, x := range hook.Requires {
-		manifest.Set("Unit", "Requires", x)
-	}
-	for _, x := range hook.Before {
-		manifest.Set("Unit", "Before", x)
-	}
+	unit.Set("Service", "ExecStartPre", dockerPullCommand)
+	unit.Set("Service", "ExecStart", dockerRunCommand)
+	unit.Set("Service", "Type", "oneshot")
+	unit.Set("Install", "WantedBy", "multi-user.target")
 
-	manifest.Set("Service", "ExecStartPre", dockerPullCommand)
-	manifest.Set("Service", "ExecStart", dockerRunCommand)
-	manifest.Set("Service", "Type", "oneshot")
-	manifest.Set("Install", "WantedBy", "multi-user.target")
+	return nil
+}
 
-	manifestString := manifest.Render()
-	glog.V(8).Infof("Built systemd manifest %q\n%s", name, manifestString)
-
-	service := &nodetasks.Service{
-		Name:       "kops-hook-" + name + ".service",
-		Definition: s(manifestString),
+// isValidExecContainerAction checks the validatity of the execContainer - personally i think this validation
+// should be done high up the chain, but
+func isValidExecContainerAction(action *kops.ExecContainerAction) error {
+	action.Image = strings.TrimSpace(action.Image)
+	if action.Image == "" {
+		return errors.New("the image for the hook exec action not set")
 	}
 
-	service.InitDefaults()
-
-	return service, nil
+	return nil
 }
--- pkg/apis/kops/v1alpha2/zz_generated.conversion.go
+++ pkg/apis/kops/v1alpha2/zz_generated.conversion.go
@@ -1331,7 +1331,10 @@ func Convert_kops_HTTPProxy_To_v1alpha2_HTTPProxy(in *kops.HTTPProxy, out *HTTPP
 }
 
 func autoConvert_v1alpha2_HookSpec_To_kops_HookSpec(in *HookSpec, out *kops.HookSpec, s conversion.Scope) error {
-	out.Documentation = in.Documentation
+	out.Name = in.Name
+	out.Disabled = in.Disabled
+	out.MasterOnly = in.MasterOnly
+	out.NodeOnly = in.NodeOnly
 	out.Requires = in.Requires
 	out.Before = in.Before
 	if in.ExecContainer != nil {
@@ -1343,6 +1346,7 @@ func autoConvert_v1alpha2_HookSpec_To_kops_HookSpec(in *HookSpec, out *kops.Hook
 	} else {
 		out.ExecContainer = nil
 	}
+	out.Manifest = in.Manifest
 	return nil
 }
 
@@ -1352,7 +1356,10 @@ func Convert_v1alpha2_HookSpec_To_kops_HookSpec(in *HookSpec, out *kops.HookSpec
 }
 
 func autoConvert_kops_HookSpec_To_v1alpha2_HookSpec(in *kops.HookSpec, out *HookSpec, s conversion.Scope) error {
-	out.Documentation = in.Documentation
+	out.Name = in.Name
+	out.Disabled = in.Disabled
+	out.MasterOnly = in.MasterOnly
+	out.NodeOnly = in.NodeOnly
 	out.Requires = in.Requires
 	out.Before = in.Before
 	if in.ExecContainer != nil {
@@ -1364,6 +1371,7 @@ func autoConvert_kops_HookSpec_To_v1alpha2_HookSpec(in *kops.HookSpec, out *Hook
 	} else {
 		out.ExecContainer = nil
 	}
+	out.Manifest = in.Manifest
 	return nil
 }
 
