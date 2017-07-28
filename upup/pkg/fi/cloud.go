30d66b0960913bbd6afd0d545a98c0aeea8b2b3f
Rohith <gambol99@gmail.com>
2017-07-28 17:16:11 +0100

    Vetting / Formating / Cleanup
    - fixed any of the vettting / formatting issues that i'm came across on the update
    - removed the commented out lines from the componentconfig as it make its increasingly difficult to find what is supported, what is not and the difference between them.
    - added SerializeImagePulls, RegisterSchedulable to kubelet (by default they are ignored)
    - added FeatureGates to the kube-proxy
    
    Out of interest can someone point me to where these multi-versioned componentconfig are being used?

--- upup/pkg/fi/cloud.go
+++ upup/pkg/fi/cloud.go
@@ -79,6 +79,8 @@ var zonesToCloud = map[string]kops.CloudProviderID{
 	"eu-west-1c": kops.CloudProviderAWS,
 	"eu-west-1d": kops.CloudProviderAWS,
 	"eu-west-1e": kops.CloudProviderAWS,
+	"eu-west-2a": kops.CloudProviderAWS,
+	"eu-west-2b": kops.CloudProviderAWS,
 
 	"eu-central-1a": kops.CloudProviderAWS,
 	"eu-central-1b": kops.CloudProviderAWS,
