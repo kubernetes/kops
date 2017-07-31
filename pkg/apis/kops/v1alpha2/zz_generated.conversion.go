119f9fe50a2b62d91946c728d657c52c3a7ce1db
Ionut Craciunescu <icraciunescu@gmail.com>
2017-07-31 10:42:07 +0100

    updates to support aws disks with provisioned iops

--- pkg/apis/kops/v1alpha2/zz_generated.conversion.go
+++ pkg/apis/kops/v1alpha2/zz_generated.conversion.go
@@ -1370,6 +1370,7 @@ func autoConvert_v1alpha2_InstanceGroupSpec_To_kops_InstanceGroupSpec(in *Instan
 	out.MachineType = in.MachineType
 	out.RootVolumeSize = in.RootVolumeSize
 	out.RootVolumeType = in.RootVolumeType
+	out.RootVolumeIops = in.RootVolumeIops
 	out.RootVolumeOptimization = in.RootVolumeOptimization
 	out.Subnets = in.Subnets
 	out.MaxPrice = in.MaxPrice
@@ -1404,6 +1405,7 @@ func autoConvert_kops_InstanceGroupSpec_To_v1alpha2_InstanceGroupSpec(in *kops.I
 	out.MachineType = in.MachineType
 	out.RootVolumeSize = in.RootVolumeSize
 	out.RootVolumeType = in.RootVolumeType
+	out.RootVolumeIops = in.RootVolumeIops
 	out.RootVolumeOptimization = in.RootVolumeOptimization
 	out.Subnets = in.Subnets
 	out.MaxPrice = in.MaxPrice
