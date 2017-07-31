b81a39fb511edc375f81ae911b0445829351e0cf
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2017-07-31 11:00:22 +0200

    adding secrets part in to v1alpha1 to

--- pkg/apis/kops/v1alpha1/register.go
+++ pkg/apis/kops/v1alpha1/register.go
@@ -52,6 +52,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&InstanceGroupList{},
 		&Federation{},
 		&FederationList{},
+		&Secret{},
+		&SecretList{},
 	)
 
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
