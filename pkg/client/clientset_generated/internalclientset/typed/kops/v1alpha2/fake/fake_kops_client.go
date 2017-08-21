92ce379dec69dd952e6b8ba8844009aab08a9856
Alexander Brandstedt <alexander.brandstedt@ea.com>
2017-08-21 17:18:14 +0200

    adding generated core

--- pkg/client/clientset_generated/internalclientset/typed/kops/v1alpha2/fake/fake_kops_client.go
+++ pkg/client/clientset_generated/internalclientset/typed/kops/v1alpha2/fake/fake_kops_client.go
@@ -38,6 +38,10 @@ func (c *FakeKopsV1alpha2) InstanceGroups(namespace string) v1alpha2.InstanceGro
 	return &FakeInstanceGroups{c, namespace}
 }
 
+func (c *FakeKopsV1alpha2) SSHSecrets(namespace string) v1alpha2.SSHSecretInterface {
+	return &FakeSSHSecrets{c, namespace}
+}
+
 // RESTClient returns a RESTClient that is used to communicate
 // with API server by this client implementation.
 func (c *FakeKopsV1alpha2) RESTClient() rest.Interface {
