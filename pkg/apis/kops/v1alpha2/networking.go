79d331e5900c484925944ea44d66a7940c36f453
Caleb Gilmour <cgilmour@gmail.com>
2017-08-23 06:34:54 +0000

    Add support for Romana as a networking option

--- pkg/apis/kops/v1alpha2/networking.go
+++ pkg/apis/kops/v1alpha2/networking.go
@@ -28,6 +28,7 @@ type NetworkingSpec struct {
 	Calico     *CalicoNetworkingSpec     `json:"calico,omitempty"`
 	Canal      *CanalNetworkingSpec      `json:"canal,omitempty"`
 	Kuberouter *KuberouterNetworkingSpec `json:"kuberouter,omitempty"`
+	Romana     *RomanaNetworkingSpec     `json:"romana,omitempty"`
 }
 
 // ClassicNetworkingSpec is the specification of classic networking mode, integrated into kubernetes
@@ -79,4 +80,12 @@ type CanalNetworkingSpec struct {
 
 // Kuberouter declares that we want Canal networking
 type KuberouterNetworkingSpec struct {
+}
+
+// Romana declares that we want Romana networking
+type RomanaNetworkingSpec struct {
+	// DaemonServiceIP is the Kubernetes Service IP for the romana-daemon pod
+	DaemonServiceIP string `json:"daemonServiceIP,omitempty"`
+	// EtcdServiceIP is the Kubernetes Service IP for the etcd backend used by Romana
+	EtcdServiceIP string `json:"etcdServiceIP,omitempty"`
 }
--- upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
+++ upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
@@ -468,6 +468,27 @@ func (b *BootstrapChannelBuilder) buildManifest() (*channelsapi.Addons, map[stri
 		}
 	}
 
+	if b.cluster.Spec.Networking.Romana != nil {
+		key := "networking.romana"
+
+		version := "v2.0-preview.2"
+
+		{
+			location := key + "/k8s-1.6.yaml"
+			id := "k8s-1.6"
+
+			addons.Spec.Addons = append(addons.Spec.Addons, &channelsapi.AddonSpec{
+				Name:              fi.String(key),
+				Version:           fi.String(version),
+				Selector:          networkingSelector,
+				Manifest:          fi.String(location),
+				KubernetesVersion: ">=1.6.0",
+				Id:                id,
+			})
+			manifests[key+"-"+id] = "addons/" + location
+		}
+	}
+
 	authenticationSelector := map[string]string{"role.kubernetes.io/authentication": "1"}
 
 	if b.cluster.Spec.Authentication != nil && b.cluster.Spec.Authentication.Kopeio != nil {
