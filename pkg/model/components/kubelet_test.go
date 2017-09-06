1746e7d1b7ac2f3c216874bbb22370fc7ee0fccb
Andrey Chernih <andrey.chernih@gmail.com>
2017-09-05 21:19:59 -0700

    Enable ExperimentalCriticalPodAnnotation feature gate
    Otherwise, it is possible that critical system components will be
    evicted
    
    https://github.com/kubernetes/kops/issues/3194
    https://github.com/kubernetes/kubernetes/issues/51432
    
    Closes #3194

---
+++ pkg/model/components/kubelet_test.go
@@ -0,0 +1,58 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package components
+
+import (
+	"testing"
+
+	"k8s.io/kops/pkg/apis/kops"
+	"k8s.io/kops/pkg/assets"
+)
+
+func buildOptions() (*kops.ClusterSpec, error) {
+	spec := kops.ClusterSpec{
+		KubernetesVersion:     "1.6.2",
+		ServiceClusterIPRange: "10.10.0.0/16",
+	}
+
+	ab := assets.NewAssetBuilder(nil)
+
+	builder := KubeletOptionsBuilder{
+		Context: &OptionsContext{
+			AssetBuilder: ab,
+		},
+	}
+
+	err := builder.BuildOptions(&spec)
+	if err != nil {
+		return nil, err
+	}
+
+	return &spec, nil
+}
+
+func TestFeatureGates(t *testing.T) {
+	spec, err := buildOptions()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	gates := spec.Kubelet.FeatureGates
+	if gates["ExperimentalCriticalPodAnnotation"] != "true" {
+		t.Errorf("ExperimentalCriticalPodAnnotation feature gate should be enabled")
+	}
+}
