db5b9315797ded02b8202a46935fee24571aa524
chrislovecnm <clove@cnmconsulting.net>
2017-09-23 22:53:43 -0600

    Adding three new funcs for cloud providers, for refactoring delete and get

--- upup/pkg/fi/cloudup/baremetal/cloud.go
+++ upup/pkg/fi/cloudup/baremetal/cloud.go
@@ -18,6 +18,10 @@ package baremetal
 
 import (
 	"fmt"
+
+	"github.com/golang/glog"
+
+	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/kops/pkg/apis/kops"
 	"k8s.io/kops/upup/pkg/fi"
 	"k8s.io/kubernetes/federation/pkg/dnsprovider"
@@ -43,4 +47,25 @@ func (c *Cloud) DNS() (dnsprovider.Interface, error) {
 
 func (c *Cloud) FindVPCInfo(id string) (*fi.VPCInfo, error) {
 	return nil, fmt.Errorf("baremetal FindVPCInfo not supported")
+}
+
+// GetCloudGroups is not implemented yet, that needs to return the instances and groups that back a kops cluster.
+// Baremetal may not support this.
+func (c *Cloud) GetCloudGroups(cluster *kops.Cluster, instancegroups []*kops.InstanceGroup, warnUnmatched bool, nodeMap map[string]*v1.Node) (map[string]*fi.CloudGroup, error) {
+	glog.V(8).Infof("baremetal cloud GetCloudGroups not implemented yet")
+	return nil, fmt.Errorf("baremetal provider does not support getting cloud groups at this time.")
+}
+
+// DeleteGroup is not implemented yet, is a func that needs to delete a DO instance group.
+// Baremetal may not support this.
+func (c *Cloud) DeleteGroup(name string, template string) error {
+	glog.V(8).Infof("digitalocean cloud provider DeleteGroup not implemented yet")
+	return fmt.Errorf("digital ocean cloud provider does not support deleting cloud groups at this time.")
+}
+
+//DeleteInstance is not implemented yet, is func needs to delete a DO instance.
+//Baremetal may not support this.
+func (c *Cloud) DeleteInstance(id *string) error {
+	glog.V(8).Infof("baremetal cloud provider DeleteInstance not implemented yet")
+	return fmt.Errorf("baremetal cloud provider does not support deleting cloud instances at this time.")
 }
