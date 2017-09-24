01aeb487a3fa763f0f88c04b63302bd30d4f399e
Justin Santa Barbara <justin@fathomdb.com>
2017-09-23 20:52:54 -0400

    AWS mock provider should use the AWS cloudprovider id
    Otherwise our tests get a little confused; for example they weren't
    outputing the Terraform provider block.

--- tests/integration/privatedns1/kubernetes.tf
+++ tests/integration/privatedns1/kubernetes.tf
@@ -50,6 +50,10 @@ output "vpc_id" {
   value = "${aws_vpc.privatedns1-example-com.id}"
 }
 
+provider "aws" {
+  region = "us-test-1"
+}
+
 resource "aws_autoscaling_attachment" "bastion-privatedns1-example-com" {
   elb                    = "${aws_elb.bastion-privatedns1-example-com.id}"
   autoscaling_group_name = "${aws_autoscaling_group.bastion-privatedns1-example-com.id}"
