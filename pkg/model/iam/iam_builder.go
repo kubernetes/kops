704d4c70efad590255be0a859c030b9ffb1b867d
Kashif Saadat <kashifsaadat@gmail.com>
2017-10-23 10:11:27 +0100

    Add IAM Permissions so nodes can access AWS ECR

--- pkg/model/iam/iam_builder.go
+++ pkg/model/iam/iam_builder.go
@@ -100,13 +100,12 @@ func (l *Statement) Equal(r *Statement) bool {
 // PolicyBuilder struct defines all valid fields to be used when building the
 // AWS IAM policy document for a given instance group role.
 type PolicyBuilder struct {
-	Cluster        *kops.Cluster
-	CreateECRPerms bool
-	HostedZoneID   string
-	KMSKeys        []string
-	Region         string
-	ResourceARN    *string
-	Role           kops.InstanceGroupRole
+	Cluster      *kops.Cluster
+	HostedZoneID string
+	KMSKeys      []string
+	Region       string
+	ResourceARN  *string
+	Role         kops.InstanceGroupRole
 }
 
 // BuildAWSPolicy builds a set of IAM policy statements based on the
@@ -159,6 +158,7 @@ func (b *PolicyBuilder) BuildAWSPolicyMaster() (*Policy, error) {
 	addMasterASPolicies(p, resource, b.Cluster.Spec.IAM.Legacy, b.Cluster.GetName())
 	addMasterELBPolicies(p, resource, b.Cluster.Spec.IAM.Legacy)
 	addCertIAMPolicies(p, resource)
+	addECRPermissions(p)
 
 	var err error
 	if p, err = b.AddS3Permissions(p); err != nil {
@@ -167,10 +167,6 @@ func (b *PolicyBuilder) BuildAWSPolicyMaster() (*Policy, error) {
 
 	if b.KMSKeys != nil && len(b.KMSKeys) != 0 {
 		addKMSIAMPolicies(p, stringorslice.Slice(b.KMSKeys), b.Cluster.Spec.IAM.Legacy)
-	}
-
-	if b.Cluster.Spec.IAM.Legacy || b.CreateECRPerms {
-		addECRPermissions(p)
 	}
 
 	if b.HostedZoneID != "" {
@@ -193,14 +189,11 @@ func (b *PolicyBuilder) BuildAWSPolicyNode() (*Policy, error) {
 	}
 
 	addNodeEC2Policies(p, resource)
+	addECRPermissions(p)
 
 	var err error
 	if p, err = b.AddS3Permissions(p); err != nil {
 		return nil, fmt.Errorf("failed to generate AWS IAM S3 access statements: %v", err)
-	}
-
-	if b.Cluster.Spec.IAM.Legacy || b.CreateECRPerms {
-		addECRPermissions(p)
 	}
 
 	if b.Cluster.Spec.IAM.Legacy {
