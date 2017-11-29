96aefac31850440329512f7f611a088f420ea37a
Mike Splain <mike.splain@gmail.com>
2017-11-28 21:17:07 -0500

    Fix flannel error on starting

--- upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
+++ upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
@@ -439,7 +439,7 @@ func (b *BootstrapChannelBuilder) buildManifest() (*channelsapi.Addons, map[stri
 
 	if b.cluster.Spec.Networking.Flannel != nil {
 		key := "networking.flannel"
-		version := "0.9.1"
+		version := "0.9.1-kops.1"
 
 		{
 			location := key + "/pre-k8s-1.6.yaml"
