2e05dd17aade0f6d9c8d7438a69916430c9694d5
Manuel de Brito Fontes <aledbf@gmail.com>
2017-12-02 10:36:16 -0300

    Add support for Amazon VPC CNI plugin

--- upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
+++ upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
@@ -621,6 +621,26 @@ func (b *BootstrapChannelBuilder) buildManifest() (*channelsapi.Addons, map[stri
 		}
 	}
 
+	if b.cluster.Spec.Networking.AmazonVPC != nil {
+		key := "networking.amazon-vpc-routed-eni"
+		version := "0.1.1-kops.1"
+
+		{
+			location := fmt.Sprintf("%v/%v.yaml", key, version)
+			id := "k8s-1.7"
+
+			addons.Spec.Addons = append(addons.Spec.Addons, &channelsapi.AddonSpec{
+				Name:              fi.String(key),
+				Version:           fi.String(version),
+				Selector:          networkingSelector,
+				Manifest:          fi.String(location),
+				KubernetesVersion: ">=1.7.0",
+				Id:                id,
+			})
+			manifests[key+"-"+id] = "addons/" + location
+		}
+	}
+
 	authenticationSelector := map[string]string{"role.kubernetes.io/authentication": "1"}
 
 	if b.cluster.Spec.Authentication != nil && b.cluster.Spec.Authentication.Kopeio != nil {
