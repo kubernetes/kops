be85dfc4383d1a76ea11d1bb2c1d03ab7045e48a
Rodrigo Menezes <brdude@gmail.com>
2017-12-13 15:48:14 -0800

    Fix typo

--- pkg/apis/kops/v1alpha1/cluster.go
+++ pkg/apis/kops/v1alpha1/cluster.go
@@ -70,7 +70,7 @@ type ClusterSpec struct {
 	// AdditionalNetworkCIDRs is a list of aditional CIDR used for the AWS VPC
 	// or otherwise allocated to k8s. This is a real CIDR, not the internal k8s network
 	// On AWS, it maps to any aditional CIDRs added to a VPC.
-	AdditionalNetworkCIDRs []string `json:"AdditionalNetworkCIDRs,omitempty"`
+	AdditionalNetworkCIDRs []string `json:"additionalNetworkCIDRs,omitempty"`
 	// NetworkID is an identifier of a network, if we want to reuse/share an existing network (e.g. an AWS VPC)
 	NetworkID string `json:"networkID,omitempty"`
 	// Topology defines the type of network topology to use on the cluster - default public
