81cd409a663906e8cf877735a15a69c240c44b8d
Brian Schultz <schultz100190@gmail.com>
2018-01-09 12:40:06 -0600

    Makefile command "apimachinery" run
    - Updated copyright to 2018
    - Reordered some imports
    - Added ImagePullProgressDeadline copy code

--- pkg/client/clientset_generated/clientset/scheme/register.go
+++ pkg/client/clientset_generated/clientset/scheme/register.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 The Kubernetes Authors.
+Copyright 2018 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,8 +17,6 @@ limitations under the License.
 package scheme
 
 import (
-	os "os"
-
 	announced "k8s.io/apimachinery/pkg/apimachinery/announced"
 	registered "k8s.io/apimachinery/pkg/apimachinery/registered"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -26,6 +24,7 @@ import (
 	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	serializer "k8s.io/apimachinery/pkg/runtime/serializer"
 	kops "k8s.io/kops/pkg/apis/kops/install"
+	os "os"
 )
 
 var Scheme = runtime.NewScheme()
