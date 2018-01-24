bc06bd6b5961522f1b71716cac6fadacf3846dca
Dennis Webb <dennis@bluesentryit.com>
2018-01-24 10:56:12 -0600

    returns latest image if more than 1 found

--- pkg/testutils/integrationtestharness.go
+++ pkg/testutils/integrationtestharness.go
@@ -129,6 +129,7 @@ func (h *IntegrationTestHarness) SetupMockAWS() {
 	}})
 
 	mockEC2.Images = append(mockEC2.Images, &ec2.Image{
+		CreationDate:   aws.String("2016-10-21T20:07:19.000Z"),
 		ImageId:        aws.String("ami-12345678"),
 		Name:           aws.String("k8s-1.4-debian-jessie-amd64-hvm-ebs-2016-10-21"),
 		OwnerId:        aws.String(awsup.WellKnownAccountKopeio),
@@ -136,6 +137,7 @@ func (h *IntegrationTestHarness) SetupMockAWS() {
 	})
 
 	mockEC2.Images = append(mockEC2.Images, &ec2.Image{
+		CreationDate:   aws.String("2017-01-09T17:08:27.000Z"),
 		ImageId:        aws.String("ami-15000000"),
 		Name:           aws.String("k8s-1.5-debian-jessie-amd64-hvm-ebs-2017-01-09"),
 		OwnerId:        aws.String(awsup.WellKnownAccountKopeio),
