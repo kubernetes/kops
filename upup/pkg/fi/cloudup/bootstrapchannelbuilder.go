a015bde1a0b165c126e53d9dce01001ac1e488cd
Justin Santa Barbara <justin@fathomdb.com>
2018-02-03 02:25:05 -0500

    Update kopeio-networking to 1.0.20180203
    No major changes, just version updates.

--- upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
+++ upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
@@ -353,7 +353,7 @@ func (b *BootstrapChannelBuilder) buildManifest() (*channelsapi.Addons, map[stri
 
 	if b.cluster.Spec.Networking.Kopeio != nil {
 		key := "networking.kope.io"
-		version := "1.0.20180120"
+		version := "1.0.20180203"
 
 		{
 			location := key + "/pre-k8s-1.6.yaml"
