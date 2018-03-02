3b9384a23c3ff2fff505feab61d62a994eb1fa93
Kyle Rockman <krockman@underarmour.com>
2018-03-02 13:37:56 -0600

    Add cluster name environment variable
    - Add an environment variable to configure cluster name
    - Expose envionrment variable in CLI documentation

--- cmd/kops/root.go
+++ cmd/kops/root.go
@@ -126,9 +126,10 @@ func NewCmdRoot(f *util.Factory, out io.Writer) *cobra.Command {
 	if strings.HasSuffix(defaultStateStore, "/") {
 		defaultStateStore = strings.TrimSuffix(defaultStateStore, "/")
 	}
-	cmd.PersistentFlags().StringVarP(&rootCommand.RegistryPath, "state", "", defaultStateStore, "Location of state storage")
+	cmd.PersistentFlags().StringVarP(&rootCommand.RegistryPath, "state", "", defaultStateStore, "Location of state storage. Overrides $KOPS_STATE_STORE")
 
-	cmd.PersistentFlags().StringVarP(&rootCommand.clusterName, "name", "", "", "Name of cluster")
+	defaultClusterName := os.Getenv("KOPS_CLUSTER_NAME")
+	cmd.PersistentFlags().StringVarP(&rootCommand.clusterName, "name", "", defaultClusterName, "Name of cluster. Overrides $KOPS_CLUSTER_NAME")
 
 	// create subcommands
 	cmd.AddCommand(NewCmdCompletion(f, out))
