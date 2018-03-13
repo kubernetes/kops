cb57755c8ccf0bcfee6b5fbaa009d8662e8775f5
Gijs Kunze <gijs@usabilla.com>
2018-03-13 09:42:15 +0100

    Perform deep merge for template values

--- cmd/kops/toolbox_template.go
+++ cmd/kops/toolbox_template.go
@@ -223,13 +223,43 @@ func newTemplateContext(files []string) (map[string]interface{}, error) {
 				return nil, fmt.Errorf("unable decode the configuration file: %s, error: %v", j, err)
 			}
 
-			for k, v := range ctx {
-				context[k] = v
-			}
+			context = mergeValues(context, ctx)
 		}
 	}
 
 	return context, nil
+}
+
+// Merges source and destination map, preferring values from the source map
+func mergeValues(dest map[string]interface{}, src map[string]interface{}) map[string]interface{} {
+	for k, v := range src {
+		// If the key doesn't exist already, then just set the key to that value
+		if _, exists := dest[k]; !exists {
+			dest[k] = v
+			continue
+		}
+		nextMap, ok := v.(map[string]interface{})
+		// If it isn't another map, overwrite the value
+		if !ok {
+			dest[k] = v
+			continue
+		}
+		// If the key doesn't exist already, then just set the key to that value
+		if _, exists := dest[k]; !exists {
+			dest[k] = nextMap
+			continue
+		}
+		// Edge case: If the key exists in the destination, but isn't a map
+		destMap, isMap := dest[k].(map[string]interface{})
+		// If the source map has a map for this key, prefer it
+		if !isMap {
+			dest[k] = v
+			continue
+		}
+		// If we got to this point, it is a map in both, so merge them
+		dest[k] = mergeValues(destMap, nextMap)
+	}
+	return dest
 }
 
 // expandFiles is responsible for resolving any references to directories
