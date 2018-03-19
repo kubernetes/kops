18e160748efe69a331b2f76051af0c87e18390d9
Xiao An <hac@zju.edu.cn>
2018-03-19 21:18:31 +0800

    add VFS implementation with Aliyun OSS
    Signed-off-by: Xiao An <hac@zju.edu.cn>

--- util/pkg/vfs/vfs.go
+++ util/pkg/vfs/vfs.go
@@ -102,7 +102,7 @@ func IsClusterReadable(p Path) bool {
 	}
 
 	switch p.(type) {
-	case *S3Path, *GSPath, *SwiftPath:
+	case *S3Path, *GSPath, *SwiftPath, *OSSPath:
 		return true
 
 	case *KubernetesPath:
