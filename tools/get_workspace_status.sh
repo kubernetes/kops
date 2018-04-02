210a7bfe937919fe2c16a4589ad949906799dc93
Justin Santa Barbara <justin@fathomdb.com>
2018-04-02 06:12:53 -0700

    Release 1.9.0-beta.1

--- tools/get_workspace_status.sh
+++ tools/get_workspace_status.sh
@@ -47,8 +47,8 @@ GITSHA=$(git describe --always)
 
 # These variables need to match the values in our Makefile
 # When we cut a new release we need to increment these accordingly
-KOPS_RELEASE_VERSION=1.9.0-alpha.3
-KOPS_CI_VERSION=1.9.0-alpha.4
+KOPS_RELEASE_VERSION=1.9.0-beta.1
+KOPS_CI_VERSION=1.9.0-beta.2
 
 if [[ -z "${VERSION}" ]]; then
   if [[ -z "${CI}" ]]; then
