42a7606cf8dedd6016ee97620fcd3733851a43f9
mahuihuang <mahuihuang@ghostcloud.cn>
2018-04-09 11:26:18 +0800

    fix typo

--- cmd/kops/delete_confirm_test.go
+++ cmd/kops/delete_confirm_test.go
@@ -24,7 +24,7 @@ import (
 	"k8s.io/kops/util/pkg/ui"
 )
 
-// TestConfirmation attempts to test the majority of the ui.GetConfirm function used in the 'kogs delete' commands
+// TestConfirmation attempts to test the majority of the ui.GetConfirm function used in the 'kops delete' commands
 func TestConfirmation(t *testing.T) {
 	var out bytes.Buffer
 	c := &ui.ConfirmArgs{
