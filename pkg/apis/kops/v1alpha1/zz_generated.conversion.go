8fe37816d34287eff99718360672e12f9f515214
Simone Sciarrati <s.sciarrati@gmail.com>
2018-05-18 08:49:02 +0200

    add generated code

--- pkg/apis/kops/v1alpha1/zz_generated.conversion.go
+++ pkg/apis/kops/v1alpha1/zz_generated.conversion.go
@@ -246,7 +246,7 @@ func Convert_kops_AlwaysAllowAuthorizationSpec_To_v1alpha1_AlwaysAllowAuthorizat
 
 func autoConvert_v1alpha1_AmazonVPCNetworkingSpec_To_kops_AmazonVPCNetworkingSpec(in *AmazonVPCNetworkingSpec, out *kops.AmazonVPCNetworkingSpec, s conversion.Scope) error {
 	out.Vethprefix = in.Vethprefix
-	out.Version = in.Version
+	out.Image = in.Image
 	return nil
 }
 
@@ -257,7 +257,7 @@ func Convert_v1alpha1_AmazonVPCNetworkingSpec_To_kops_AmazonVPCNetworkingSpec(in
 
 func autoConvert_kops_AmazonVPCNetworkingSpec_To_v1alpha1_AmazonVPCNetworkingSpec(in *kops.AmazonVPCNetworkingSpec, out *AmazonVPCNetworkingSpec, s conversion.Scope) error {
 	out.Vethprefix = in.Vethprefix
-	out.Version = in.Version
+	out.Image = in.Image
 	return nil
 }
 
