7aaadefe6692c08bbbfac93ed0ec7497dc219de9
Christian Kampka <christian@kampka.net>
2018-07-13 22:28:27 +0200

    Generate locals for terraform target

--- tests/integration/update_cluster/bastionadditional_user-data/kubernetes.tf
+++ tests/integration/update_cluster/bastionadditional_user-data/kubernetes.tf
@@ -1,3 +1,24 @@
+locals = {
+  bastion_security_group_ids        = ["${aws_security_group.bastion-bastionuserdata-example-com.id}"]
+  bastions_role_arn                 = "${aws_iam_role.bastions-bastionuserdata-example-com.arn}"
+  bastions_role_name                = "${aws_iam_role.bastions-bastionuserdata-example-com.name}"
+  cluster_name                      = "bastionuserdata.example.com"
+  master_security_group_ids         = ["${aws_security_group.masters-bastionuserdata-example-com.id}"]
+  masters_role_arn                  = "${aws_iam_role.masters-bastionuserdata-example-com.arn}"
+  masters_role_name                 = "${aws_iam_role.masters-bastionuserdata-example-com.name}"
+  node_security_group_ids           = ["${aws_security_group.nodes-bastionuserdata-example-com.id}"]
+  node_subnet_ids                   = ["${aws_subnet.us-test-1a-bastionuserdata-example-com.id}"]
+  nodes_role_arn                    = "${aws_iam_role.nodes-bastionuserdata-example-com.arn}"
+  nodes_role_name                   = "${aws_iam_role.nodes-bastionuserdata-example-com.name}"
+  region                            = "us-test-1"
+  route_table_private-us-test-1a_id = "${aws_route_table.private-us-test-1a-bastionuserdata-example-com.id}"
+  route_table_public_id             = "${aws_route_table.bastionuserdata-example-com.id}"
+  subnet_us-test-1a-private_id      = "${aws_subnet.us-test-1a-bastionuserdata-example-com.id}"
+  subnet_us-test-1a-utility_id      = "${aws_subnet.utility-us-test-1a-bastionuserdata-example-com.id}"
+  vpc_cidr_block                    = "${aws_vpc.bastionuserdata-example-com.cidr_block}"
+  vpc_id                            = "${aws_vpc.bastionuserdata-example-com.id}"
+}
+
 output "bastion_security_group_ids" {
   value = ["${aws_security_group.bastion-bastionuserdata-example-com.id}"]
 }
--- tests/integration/update_cluster/private-shared-subnet/kubernetes.tf
+++ tests/integration/update_cluster/private-shared-subnet/kubernetes.tf
@@ -1,3 +1,22 @@
+locals = {
+  bastion_security_group_ids   = ["${aws_security_group.bastion-private-shared-subnet-example-com.id}"]
+  bastions_role_arn            = "${aws_iam_role.bastions-private-shared-subnet-example-com.arn}"
+  bastions_role_name           = "${aws_iam_role.bastions-private-shared-subnet-example-com.name}"
+  cluster_name                 = "private-shared-subnet.example.com"
+  master_security_group_ids    = ["${aws_security_group.masters-private-shared-subnet-example-com.id}"]
+  masters_role_arn             = "${aws_iam_role.masters-private-shared-subnet-example-com.arn}"
+  masters_role_name            = "${aws_iam_role.masters-private-shared-subnet-example-com.name}"
+  node_security_group_ids      = ["${aws_security_group.nodes-private-shared-subnet-example-com.id}"]
+  node_subnet_ids              = ["subnet-12345678"]
+  nodes_role_arn               = "${aws_iam_role.nodes-private-shared-subnet-example-com.arn}"
+  nodes_role_name              = "${aws_iam_role.nodes-private-shared-subnet-example-com.name}"
+  region                       = "us-test-1"
+  subnet_ids                   = ["subnet-12345678", "subnet-abcdef"]
+  subnet_us-test-1a-private_id = "subnet-12345678"
+  subnet_us-test-1a-utility_id = "subnet-abcdef"
+  vpc_id                       = "vpc-12345678"
+}
+
 output "bastion_security_group_ids" {
   value = ["${aws_security_group.bastion-private-shared-subnet-example-com.id}"]
 }
