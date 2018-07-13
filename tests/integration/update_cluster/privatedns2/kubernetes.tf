978f66266c98ede54b356a629d5eb16fe7bc6768
Christian Kampka <christian@kampka.net>
2018-07-13 22:28:27 +0200

    Generate locals for terraform target

--- tests/integration/update_cluster/privatedns2/kubernetes.tf
+++ tests/integration/update_cluster/privatedns2/kubernetes.tf
@@ -1,3 +1,23 @@
+locals = {
+  bastion_security_group_ids        = ["${aws_security_group.bastion-privatedns2-example-com.id}"]
+  bastions_role_arn                 = "${aws_iam_role.bastions-privatedns2-example-com.arn}"
+  bastions_role_name                = "${aws_iam_role.bastions-privatedns2-example-com.name}"
+  cluster_name                      = "privatedns2.example.com"
+  master_security_group_ids         = ["${aws_security_group.masters-privatedns2-example-com.id}"]
+  masters_role_arn                  = "${aws_iam_role.masters-privatedns2-example-com.arn}"
+  masters_role_name                 = "${aws_iam_role.masters-privatedns2-example-com.name}"
+  node_security_group_ids           = ["${aws_security_group.nodes-privatedns2-example-com.id}"]
+  node_subnet_ids                   = ["${aws_subnet.us-test-1a-privatedns2-example-com.id}"]
+  nodes_role_arn                    = "${aws_iam_role.nodes-privatedns2-example-com.arn}"
+  nodes_role_name                   = "${aws_iam_role.nodes-privatedns2-example-com.name}"
+  region                            = "us-test-1"
+  route_table_private-us-test-1a_id = "${aws_route_table.private-us-test-1a-privatedns2-example-com.id}"
+  route_table_public_id             = "${aws_route_table.privatedns2-example-com.id}"
+  subnet_us-test-1a-private_id      = "${aws_subnet.us-test-1a-privatedns2-example-com.id}"
+  subnet_us-test-1a-utility_id      = "${aws_subnet.utility-us-test-1a-privatedns2-example-com.id}"
+  vpc_id                            = "vpc-12345678"
+}
+
 output "bastion_security_group_ids" {
   value = ["${aws_security_group.bastion-privatedns2-example-com.id}"]
 }
--- tests/integration/update_cluster/shared_subnet/kubernetes.tf
+++ tests/integration/update_cluster/shared_subnet/kubernetes.tf
@@ -1,3 +1,18 @@
+locals = {
+  cluster_name                = "sharedsubnet.example.com"
+  master_security_group_ids   = ["${aws_security_group.masters-sharedsubnet-example-com.id}"]
+  masters_role_arn            = "${aws_iam_role.masters-sharedsubnet-example-com.arn}"
+  masters_role_name           = "${aws_iam_role.masters-sharedsubnet-example-com.name}"
+  node_security_group_ids     = ["${aws_security_group.nodes-sharedsubnet-example-com.id}"]
+  node_subnet_ids             = ["subnet-12345678"]
+  nodes_role_arn              = "${aws_iam_role.nodes-sharedsubnet-example-com.arn}"
+  nodes_role_name             = "${aws_iam_role.nodes-sharedsubnet-example-com.name}"
+  region                      = "us-test-1"
+  subnet_ids                  = ["subnet-12345678"]
+  subnet_us-test-1a-public_id = "subnet-12345678"
+  vpc_id                      = "vpc-12345678"
+}
+
 output "cluster_name" {
   value = "sharedsubnet.example.com"
 }
