8ccf42f4a2ac31aac6b4fc41245eecf2a4413407
Rob Graham <robgraham1984@gmail.com>
2018-07-23 13:48:35 +0100

    GH-4252 Better name for the config value and also add to v1alpha1 API

--- pkg/apis/kops/v1alpha2/cluster.go
+++ pkg/apis/kops/v1alpha2/cluster.go
@@ -286,7 +286,7 @@ type LoadBalancerAccessSpec struct {
 	Type                     LoadBalancerType `json:"type,omitempty"`
 	IdleTimeoutSeconds       *int64           `json:"idleTimeoutSeconds,omitempty"`
 	AdditionalSecurityGroups []string         `json:"additionalSecurityGroups,omitempty"`
-	UseApiInternal           bool             `json:"useApiInternal,omitempty"`
+	UseForInternalApi        bool             `json:"useForInternalApi,omitempty"`
 }
 
 // KubeDNSConfig defines the kube dns configuration
