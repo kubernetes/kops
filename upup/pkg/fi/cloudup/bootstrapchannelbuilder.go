a846b217c1660ed3c2354791a195483129e3241a
Zivago Lee <z@ziff.net>
2018-07-26 07:45:42 -0700

    bump version

--- upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
+++ upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
@@ -681,7 +681,7 @@ func (b *BootstrapChannelBuilder) buildManifest() (*channelsapi.Addons, map[stri
 
 	if b.cluster.Spec.Networking.Kuberouter != nil {
 		key := "networking.kuberouter"
-		version := "0.1.1-kops.2"
+		version := "0.1.1-kops.3"
 
 		{
 			location := key + "/k8s-1.6.yaml"
