0a0bbbd7435223463b0cf670f2b8500893f8030a
Justin Santa Barbara <justin@fathomdb.com>
2018-10-15 13:07:15 -0400

    Mark release 1.11.0-alpha.1

--- upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
+++ upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
@@ -284,7 +284,7 @@ func (b *BootstrapChannelBuilder) buildManifest() (*channelsapi.Addons, map[stri
 	if externalDNS == nil || !externalDNS.Disable {
 		{
 			key := "dns-controller.addons.k8s.io"
-			version := "1.10.0"
+			version := "1.11.0-alpha.1"
 
 			{
 				location := key + "/pre-k8s-1.6.yaml"
