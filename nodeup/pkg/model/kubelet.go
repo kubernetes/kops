e1ffda05c65dc5ce6573b8d5213e704d0dab69ad
Jean-Philippe Belanger <jpbelanger@upgrade.com>
2019-01-18 20:02:24 -0500

    Executing apimachinery step

--- nodeup/pkg/model/kubelet.go
+++ nodeup/pkg/model/kubelet.go
@@ -581,7 +581,7 @@ func (b *KubeletBuilder) buildMasterKubeletKubeconfig() (*nodetasks.File, error)
 
 	template := &x509.Certificate{
 		BasicConstraintsValid: true,
-		IsCA: false,
+		IsCA:                  false,
 	}
 
 	template.Subject = pkix.Name{
