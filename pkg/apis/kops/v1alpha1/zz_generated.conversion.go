7bca47772c40cf6f2dde5c5fdd30a4bc71a687be
José Luis Ledesma <joseluis.ledesma@socialpoint.es>
2019-03-27 14:36:28 +0100

    Rename RegistryQPS to RegistryPullQPS + add RegistryBurst

--- pkg/apis/kops/v1alpha1/zz_generated.conversion.go
+++ pkg/apis/kops/v1alpha1/zz_generated.conversion.go
@@ -3400,7 +3400,8 @@ func autoConvert_v1alpha1_KubeletConfigSpec_To_kops_KubeletConfigSpec(in *Kubele
 	out.CPUCFSQuota = in.CPUCFSQuota
 	out.CPUCFSQuotaPeriod = in.CPUCFSQuotaPeriod
 	out.CpuManagerPolicy = in.CpuManagerPolicy
-	out.RegistryQPS = in.RegistryQPS
+	out.RegistryPullQPS = in.RegistryPullQPS
+	out.RegistryBurst = in.RegistryBurst
 	return nil
 }
 
@@ -3483,7 +3484,8 @@ func autoConvert_kops_KubeletConfigSpec_To_v1alpha1_KubeletConfigSpec(in *kops.K
 	out.CPUCFSQuota = in.CPUCFSQuota
 	out.CPUCFSQuotaPeriod = in.CPUCFSQuotaPeriod
 	out.CpuManagerPolicy = in.CpuManagerPolicy
-	out.RegistryQPS = in.RegistryQPS
+	out.RegistryPullQPS = in.RegistryPullQPS
+	out.RegistryBurst = in.RegistryBurst
 	return nil
 }
 
