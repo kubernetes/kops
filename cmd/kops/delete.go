737e038e8c17538511229d413ae8240a2d8a138b
Justin SB <justinsb@google.com>
2019-04-29 15:35:57 -0400

    Update kubernetes dependencies to 1.13.5
    Notable changes:
    
    * openapi-gen moved to k8s.io/kube-openapi/cmd/openapi-gen
    * templates moved to k8s.io/kubernetes/pkg/kubectl/util/templates

--- cmd/kops/delete.go
+++ cmd/kops/delete.go
@@ -32,9 +32,9 @@ import (
 	"k8s.io/kops/pkg/sshcredentials"
 	"k8s.io/kops/util/pkg/text"
 	"k8s.io/kops/util/pkg/vfs"
-	"k8s.io/kubernetes/pkg/kubectl/cmd/templates"
 	cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
 	"k8s.io/kubernetes/pkg/kubectl/util/i18n"
+	"k8s.io/kubernetes/pkg/kubectl/util/templates"
 )
 
 type DeleteOptions struct {
