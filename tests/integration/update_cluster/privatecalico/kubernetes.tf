851b96fab81620254867b673929f47c146b8a36d
Austin Moore <austin.moore@capitalone.com>
2019-05-20 17:22:38 -0400

    Default CrossZoneLoadBalancing to false if not set

--- tests/integration/update_cluster/privatecalico/kubernetes.tf
+++ tests/integration/update_cluster/privatecalico/kubernetes.tf
@@ -268,7 +268,8 @@ resource "aws_elb" "api-privatecalico-example-com" {
     timeout             = 5
   }
 
-  idle_timeout = 300
+  cross_zone_load_balancing = false
+  idle_timeout              = 300
 
   tags = {
     KubernetesCluster                                 = "privatecalico.example.com"
