9ae39f793af2d3254f99c0df470631674b826a54
Austin Moore <austin.moore@capitalone.com>
2019-05-20 17:22:38 -0400

    Default CrossZoneLoadBalancing to false if not set

--- tests/integration/update_cluster/privateweave/kubernetes.tf
+++ tests/integration/update_cluster/privateweave/kubernetes.tf
@@ -268,7 +268,8 @@ resource "aws_elb" "api-privateweave-example-com" {
     timeout             = 5
   }
 
-  idle_timeout = 300
+  cross_zone_load_balancing = false
+  idle_timeout              = 300
 
   tags = {
     KubernetesCluster                                = "privateweave.example.com"
