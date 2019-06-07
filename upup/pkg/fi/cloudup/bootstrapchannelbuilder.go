79474ffc0b24ae052cc0c0ee8e425c7536c66e1e
Peter Rifel <pgrifel@gmail.com>
2019-06-07 16:05:55 -0700

    Upgrade AWS VPC CNI provider to 1.5.0
    Released a few days ago: https://github.com/aws/amazon-vpc-cni-k8s/releases/tag/v1.5.0

--- upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
+++ upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
@@ -1061,7 +1061,7 @@ func (b *BootstrapChannelBuilder) buildManifest() (*channelsapi.Addons, map[stri
 
 	if b.cluster.Spec.Networking.AmazonVPC != nil {
 		key := "networking.amazon-vpc-routed-eni"
-		version := "1.3.3-kops.1"
+		version := "1.5.0-kops.1"
 
 		{
 			id := "k8s-1.7"
