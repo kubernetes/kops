6f7cb2c62560758f7a5ea2c30f8ba95f4cf78357
Rodrigo Menezes <brdude@gmail.com>
2019-06-17 23:06:26 -0700

    add --kube-api-burst flag

--- pkg/apis/kops/v1alpha2/zz_generated.conversion.go
+++ pkg/apis/kops/v1alpha2/zz_generated.conversion.go
@@ -3402,6 +3402,7 @@ func autoConvert_v1alpha2_KubeControllerManagerConfig_To_kops_KubeControllerMana
 	out.TLSMinVersion = in.TLSMinVersion
 	out.MinResyncPeriod = in.MinResyncPeriod
 	out.KubeAPIQPS = in.KubeAPIQPS
+	out.KubeAPIBurst = in.KubeAPIBurst
 	return nil
 }
 
@@ -3450,6 +3451,7 @@ func autoConvert_kops_KubeControllerManagerConfig_To_v1alpha2_KubeControllerMana
 	out.TLSMinVersion = in.TLSMinVersion
 	out.MinResyncPeriod = in.MinResyncPeriod
 	out.KubeAPIQPS = in.KubeAPIQPS
+	out.KubeAPIBurst = in.KubeAPIBurst
 	return nil
 }
 
--- pkg/apis/kops/v1alpha2/zz_generated.deepcopy.go
+++ pkg/apis/kops/v1alpha2/zz_generated.deepcopy.go
@@ -2035,6 +2035,11 @@ func (in *KubeControllerManagerConfig) DeepCopyInto(out *KubeControllerManagerCo
 		*out = new(float32)
 		**out = **in
 	}
+	if in.KubeAPIBurst != nil {
+		in, out := &in.KubeAPIBurst, &out.KubeAPIBurst
+		*out = new(int32)
+		**out = **in
+	}
 	return
 }
 
