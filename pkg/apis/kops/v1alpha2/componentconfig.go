4d5ce12714417b9c5af76d28f3309fbe873b08c4
Jesse Haka <haka.jesse@gmail.com>
2019-06-27 08:26:31 +0300

    do not append admissionplugins

--- pkg/apis/kops/v1alpha2/componentconfig.go
+++ pkg/apis/kops/v1alpha2/componentconfig.go
@@ -266,7 +266,7 @@ type KubeAPIServerConfig struct {
 	// Deprecated: AdmissionControl is a list of admission controllers to use
 	AdmissionControl []string `json:"admissionControl,omitempty" flag:"admission-control"`
 	// AppendAdmissionPlugins appends list of enabled admission plugins
-	AppendAdmissionPlugins []string `json:"appendAdmissionPlugins,omitempty" flag:"append-admission-plugins"`
+	AppendAdmissionPlugins []string `json:"appendAdmissionPlugins,omitempty"`
 	// EnableAdmissionPlugins is a list of enabled admission plugins
 	EnableAdmissionPlugins []string `json:"enableAdmissionPlugins,omitempty" flag:"enable-admission-plugins"`
 	// DisableAdmissionPlugins is a list of disabled admission plugins
