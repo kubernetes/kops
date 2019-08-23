783f627acb92a48e23f9f5a182f1a7d2c6758654
Justin SB <justinsb@google.com>
2019-08-22 21:25:46 -0400

    Release 1.14.0-beta.2

--- version.go
+++ version.go
@@ -23,7 +23,7 @@ var Version = KOPS_RELEASE_VERSION
 
 // These constants are parsed by build tooling - be careful about changing the formats
 const (
-	KOPS_RELEASE_VERSION = "1.14.0-beta.1"
+	KOPS_RELEASE_VERSION = "1.14.0-beta.2"
 	KOPS_CI_VERSION      = "1.14.0-beta.2"
 )
 
