2ec47e374dc7682fbacac5968e124bf1a53d1095
jakub.coufal <jakub.coufal@wandera.com>
2019-09-24 16:39:50 +0200

    Align AWS and kops validation for spot allocation strategy
    Signed-off-by: jakub.coufal <jakub.coufal@wandera.com>

--- pkg/apis/kops/v1alpha1/instancegroup.go
+++ pkg/apis/kops/v1alpha1/instancegroup.go
@@ -141,10 +141,10 @@ type InstanceGroupSpec struct {
 }
 
 const (
-	// SpotAllocationStrategyLowestPrices indicates a lowest price strategy
-	SpotAllocationStrategyLowestPrices = "LowestPrice"
+	// SpotAllocationStrategyLowestPrices indicates a lowest-price strategy
+	SpotAllocationStrategyLowestPrices = "lowest-price"
 	// SpotAllocationStrategyDiversified indicates a diversified strategy
-	SpotAllocationStrategyDiversified = "Diversified"
+	SpotAllocationStrategyDiversified = "diversified"
 )
 
 // SpotAllocationStrategies is a collection of supported strategies
