d5078612f641d89190edb39f3fedcee2548ba68f
Justin SB <justinsb@google.com>
2019-10-01 10:51:59 -0400

    Release 1.14.0

--- upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
+++ upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
@@ -390,7 +390,7 @@ func (b *BootstrapChannelBuilder) buildAddons() *channelsapi.Addons {
 	if externalDNS == nil || !externalDNS.Disable {
 		{
 			key := "dns-controller.addons.k8s.io"
-			version := "1.14.0-beta.2"
+			version := "1.14.0"
 
 			{
 				location := key + "/pre-k8s-1.6.yaml"
