de65223e5226c27a72b169fc9ff0cd06ecabd265
tanjunchen <2799194073@qq.com>
2019-10-29 13:54:27 +0800

    fix-up staticcheck problems

--- node-authorizer/pkg/server/admission.go
+++ node-authorizer/pkg/server/admission.go
@@ -35,7 +35,7 @@ var (
 
 // authorizeNodeRequest is responsible for handling the incoming authorization request
 func (n *NodeAuthorizer) authorizeNodeRequest(ctx context.Context, request *NodeRegistration) error {
-	doneCh := make(chan error, 0)
+	doneCh := make(chan error)
 
 	// @step: create a context to run under
 	ctx, cancel := context.WithTimeout(ctx, n.config.AuthorizationTimeout)
