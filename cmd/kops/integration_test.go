61e7315cffa99d43bf4e8f542b9a1ddc68066aeb
Austin Moore <austin.moore@capitalone.com>
2019-11-20 13:47:29 -0500

    Add integration test for static docker binary spec
    # Conflicts:
    #	cmd/kops/integration_test.go

--- cmd/kops/integration_test.go
+++ cmd/kops/integration_test.go
@@ -81,7 +81,11 @@ func TestComplex(t *testing.T) {
 	runTestCloudformation(t, "complex.example.com", "complex", "v1alpha2", false, nil)
 }
 
-// TestCrossZone tests that the cross zone setting on the API ELB is set properly
+func TestStaticDockerBinary(t *testing.T) {
+	runTestAWS(t, "staticdocker.example.com", "static_docker_binary", "v1alpha2", false, 1, true, false, nil, true)
+}
+
+// TestCrossZone tests that the the cross zone setting on the API ELB is set properly
 func TestCrossZone(t *testing.T) {
 	runTestAWS(t, "crosszone.example.com", "api_elb_cross_zone", "v1alpha2", false, 1, true, false, nil)
 }
