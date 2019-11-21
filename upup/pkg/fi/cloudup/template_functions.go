24cde79144860abc535f0657a7dae0605855730f
Rodrigo Chacon <rochacon@gmail.com>
2019-11-21 21:58:16 +0800

    cloudup: Map sprig indent template function
    Map Sprig indent string template function into cloudup template
    functions.

--- upup/pkg/fi/cloudup/template_functions.go
+++ upup/pkg/fi/cloudup/template_functions.go
@@ -35,6 +35,7 @@ import (
 	"strings"
 	"text/template"
 
+	"github.com/Masterminds/sprig"
 	"k8s.io/kops/pkg/apis/kops"
 	"k8s.io/kops/pkg/dns"
 	"k8s.io/kops/pkg/featureflag"
@@ -73,6 +74,9 @@ func (tf *TemplateFunctions) AddTo(dest template.FuncMap, secretStore fi.SecretS
 	dest["join"] = func(a []string, sep string) string {
 		return strings.Join(a, sep)
 	}
+
+	sprigTxtFuncMap := sprig.TxtFuncMap()
+	dest["indent"] = sprigTxtFuncMap["indent"]
 
 	dest["ClusterName"] = tf.modelContext.ClusterName
 	dest["HasTag"] = tf.HasTag
