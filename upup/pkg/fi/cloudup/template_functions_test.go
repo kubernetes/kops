e44126d6f26a65598bfcc4657c3876747c20bd75
David Siecinski <david.siecinski@goldenoakit.pl>
2019-11-22 12:37:37 +0100

    run hack/update-bazel.sh and go fmt and BUILD.bazel file in addons

--- upup/pkg/fi/cloudup/template_functions_test.go
+++ upup/pkg/fi/cloudup/template_functions_test.go
@@ -1,33 +1,48 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package cloudup
 
 import (
-	"k8s.io/kops/pkg/featureflag"
-	"path/filepath"
-	"io/ioutil"
 	"bytes"
-	"text/template"
-	"k8s.io/kops/pkg/apis/kops"
-	"reflect"
-	"testing"
 	"fmt"
-	"strings"
+	"io/ioutil"
+	"k8s.io/kops/pkg/apis/kops"
+	"k8s.io/kops/pkg/featureflag"
 	"k8s.io/kops/upup/pkg/fi"
 	"k8s.io/kubernetes/pkg/controller/nodeipam/ipam"
+	"path/filepath"
+	"reflect"
+	"strings"
+	"testing"
+	"text/template"
 )
 
 func Test_TemplateFunctions_CloudControllerConfigArgv(t *testing.T) {
 	tests := []struct {
-		desc         string
-		cluster      *kops.Cluster
-		expectedArgv []string
+		desc          string
+		cluster       *kops.Cluster
+		expectedArgv  []string
 		expectedError error
 	}{
 		{
 			desc: "Default Configuration",
 			cluster: &kops.Cluster{Spec: kops.ClusterSpec{
-				CloudProvider: string(kops.CloudProviderOpenstack),
+				CloudProvider:                  string(kops.CloudProviderOpenstack),
 				ExternalCloudControllerManager: &kops.CloudControllerManagerConfig{},
-
 			}},
 			expectedArgv: []string{
 				"--v=2",
@@ -57,7 +72,7 @@ func Test_TemplateFunctions_CloudControllerConfigArgv(t *testing.T) {
 				Spec: kops.ClusterSpec{
 					ExternalCloudControllerManager: &kops.CloudControllerManagerConfig{
 						CloudProvider: string(kops.CloudProviderOpenstack),
-						LogLevel: 3,
+						LogLevel:      3,
 					},
 				},
 			},
@@ -85,8 +100,6 @@ func Test_TemplateFunctions_CloudControllerConfigArgv(t *testing.T) {
 				ExternalCloudControllerManager: &kops.CloudControllerManagerConfig{
 					ClusterName: "k8s",
 				},
-
-
 			}},
 			expectedArgv: []string{
 				"--v=2",
@@ -98,7 +111,7 @@ func Test_TemplateFunctions_CloudControllerConfigArgv(t *testing.T) {
 		{
 			desc: "Default Configuration",
 			cluster: &kops.Cluster{Spec: kops.ClusterSpec{
-				CloudProvider:                  string(kops.CloudProviderOpenstack),
+				CloudProvider: string(kops.CloudProviderOpenstack),
 				ExternalCloudControllerManager: &kops.CloudControllerManagerConfig{
 					Master: "127.0.0.1",
 				},
@@ -113,7 +126,7 @@ func Test_TemplateFunctions_CloudControllerConfigArgv(t *testing.T) {
 		{
 			desc: "Cluster-cidr Configuration",
 			cluster: &kops.Cluster{Spec: kops.ClusterSpec{
-				CloudProvider:                  string(kops.CloudProviderOpenstack),
+				CloudProvider: string(kops.CloudProviderOpenstack),
 				ExternalCloudControllerManager: &kops.CloudControllerManagerConfig{
 					ClusterCIDR: "10.0.0.0/24",
 				},
@@ -128,7 +141,7 @@ func Test_TemplateFunctions_CloudControllerConfigArgv(t *testing.T) {
 		{
 			desc: "AllocateNodeCIDRs Configuration",
 			cluster: &kops.Cluster{Spec: kops.ClusterSpec{
-				CloudProvider:                  string(kops.CloudProviderOpenstack),
+				CloudProvider: string(kops.CloudProviderOpenstack),
 				ExternalCloudControllerManager: &kops.CloudControllerManagerConfig{
 					AllocateNodeCIDRs: fi.Bool(true),
 				},
@@ -138,53 +151,52 @@ func Test_TemplateFunctions_CloudControllerConfigArgv(t *testing.T) {
 				"--cloud-provider=openstack",
 				"--allocate-node-cidrs=true",
 				"--use-service-account-credentials=true",
-
 			},
 		},
 		{
-		 			desc: "ConfigureCloudRoutes Configuration",
-		 			cluster: &kops.Cluster{Spec: kops.ClusterSpec{
-		 				CloudProvider:                  string(kops.CloudProviderOpenstack),
-		 				ExternalCloudControllerManager: &kops.CloudControllerManagerConfig{
-		 					ConfigureCloudRoutes: fi.Bool(true),
-		 				},
-		 			}},
-		 			expectedArgv: []string{
-		 				"--v=2",
-		 				"--cloud-provider=openstack",
-		 				"--configure-cloud-routes=true",
-		 				"--use-service-account-credentials=true",
-		 			},
-		 		},
-		 		{
-		 			desc: "CIDRAllocatorType Configuration",
-		 			cluster: &kops.Cluster{Spec: kops.ClusterSpec{
-		 				CloudProvider:                  string(kops.CloudProviderOpenstack),
-		 				ExternalCloudControllerManager: &kops.CloudControllerManagerConfig{
-		 					CIDRAllocatorType: fi.String(string(ipam.RangeAllocatorType)),
-		 				},
-		 			}},
-		 			expectedArgv: []string{
-		 				"--v=2",
-		 				"--cloud-provider=openstack",
-		 				"--cidr-allocator-type=RangeAllocator",
-		 				"--use-service-account-credentials=true",
-		 			},
-		 		},
-		 		{
-		 			desc: "CIDRAllocatorType Configuration",
-		 			cluster: &kops.Cluster{Spec: kops.ClusterSpec{
-		 				CloudProvider:                  string(kops.CloudProviderOpenstack),
-		 				ExternalCloudControllerManager: &kops.CloudControllerManagerConfig{
-		 					UseServiceAccountCredentials: fi.Bool(false),
-		 				},
-		 			}},
-		 			expectedArgv: []string{
-		 				"--v=2",
-		 				"--cloud-provider=openstack",
-		 				"--use-service-account-credentials=false",
-		 			},
-		 		},
+			desc: "ConfigureCloudRoutes Configuration",
+			cluster: &kops.Cluster{Spec: kops.ClusterSpec{
+				CloudProvider: string(kops.CloudProviderOpenstack),
+				ExternalCloudControllerManager: &kops.CloudControllerManagerConfig{
+					ConfigureCloudRoutes: fi.Bool(true),
+				},
+			}},
+			expectedArgv: []string{
+				"--v=2",
+				"--cloud-provider=openstack",
+				"--configure-cloud-routes=true",
+				"--use-service-account-credentials=true",
+			},
+		},
+		{
+			desc: "CIDRAllocatorType Configuration",
+			cluster: &kops.Cluster{Spec: kops.ClusterSpec{
+				CloudProvider: string(kops.CloudProviderOpenstack),
+				ExternalCloudControllerManager: &kops.CloudControllerManagerConfig{
+					CIDRAllocatorType: fi.String(string(ipam.RangeAllocatorType)),
+				},
+			}},
+			expectedArgv: []string{
+				"--v=2",
+				"--cloud-provider=openstack",
+				"--cidr-allocator-type=RangeAllocator",
+				"--use-service-account-credentials=true",
+			},
+		},
+		{
+			desc: "CIDRAllocatorType Configuration",
+			cluster: &kops.Cluster{Spec: kops.ClusterSpec{
+				CloudProvider: string(kops.CloudProviderOpenstack),
+				ExternalCloudControllerManager: &kops.CloudControllerManagerConfig{
+					UseServiceAccountCredentials: fi.Bool(false),
+				},
+			}},
+			expectedArgv: []string{
+				"--v=2",
+				"--cloud-provider=openstack",
+				"--use-service-account-credentials=false",
+			},
+		},
 	}
 	for _, testCase := range tests {
 		t.Run(testCase.desc, func(t *testing.T) {
@@ -202,41 +214,40 @@ func Test_TemplateFunctions_CloudControllerConfigArgv(t *testing.T) {
 	}
 }
 
-
-func Test_executeTemplate(t *testing.T ) {
-	tests := []struct{
-			desc string
-			cluster *kops.Cluster
-			templateFilename string
-			expectedManifestPath string
+func Test_executeTemplate(t *testing.T) {
+	tests := []struct {
+		desc                 string
+		cluster              *kops.Cluster
+		templateFilename     string
+		expectedManifestPath string
 	}{
 		{
 			desc: "test cloud controller template",
-			cluster:  &kops.Cluster{Spec: kops.ClusterSpec{
+			cluster: &kops.Cluster{Spec: kops.ClusterSpec{
 				CloudProvider: string(kops.CloudProviderOpenstack),
 				ExternalCloudControllerManager: &kops.CloudControllerManagerConfig{
 					ClusterName: "k8s",
-					Image: "docker.io/k8scloudprovider/openstack-cloud-controller-manager:1.13",
-				},
-		},
-	},
-	templateFilename: "../../../models/cloudup/resources/addons/openstack.addons.k8s.io/k8s-1.13.yaml.template",
-	expectedManifestPath: "./tests/manifests/k8s-1.13.yaml",
-},
-{
-		desc: "test cloud controller template",
-		cluster:  &kops.Cluster{Spec: kops.ClusterSpec{
-			CloudProvider: string(kops.CloudProviderOpenstack),
-			ExternalCloudControllerManager: &kops.CloudControllerManagerConfig{
-				ClusterName: "k8s",
-				Image: "docker.io/k8scloudprovider/openstack-cloud-controller-manager:1.13",
+					Image:       "docker.io/k8scloudprovider/openstack-cloud-controller-manager:1.13",
 				},
 			},
+			},
+			templateFilename:     "../../../models/cloudup/resources/addons/openstack.addons.k8s.io/k8s-1.13.yaml.template",
+			expectedManifestPath: "./tests/manifests/k8s-1.13.yaml",
 		},
-		templateFilename: "../../../models/cloudup/resources/addons/openstack.addons.k8s.io/k8s-1.11.yaml.template",
-		expectedManifestPath: "./tests/manifests/k8s-1.11.yaml",
+		{
+			desc: "test cloud controller template",
+			cluster: &kops.Cluster{Spec: kops.ClusterSpec{
+				CloudProvider: string(kops.CloudProviderOpenstack),
+				ExternalCloudControllerManager: &kops.CloudControllerManagerConfig{
+					ClusterName: "k8s",
+					Image:       "docker.io/k8scloudprovider/openstack-cloud-controller-manager:1.13",
+				},
+			},
+			},
+			templateFilename:     "../../../models/cloudup/resources/addons/openstack.addons.k8s.io/k8s-1.11.yaml.template",
+			expectedManifestPath: "./tests/manifests/k8s-1.11.yaml",
 		},
-}
+	}
 
 	for _, testCase := range tests {
 		t.Run(testCase.desc, func(t *testing.T) {
@@ -246,7 +257,7 @@ func Test_executeTemplate(t *testing.T ) {
 				t.Fatalf("error getting path to template: %v", filePathError)
 			}
 
-				tpl := template.New(filepath.Base(templateFileAbsolutePath))
+			tpl := template.New(filepath.Base(templateFileAbsolutePath))
 
 			funcMap := make(template.FuncMap)
 			templateFunctions := TemplateFunctions{cluster: testCase.cluster}
@@ -254,25 +265,25 @@ func Test_executeTemplate(t *testing.T ) {
 
 			tpl.Funcs(funcMap)
 
-				tpl.Option("missingkey=zero")
-				_, err := tpl.ParseFiles(templateFileAbsolutePath)
-				if err != nil {
-					t.Fatalf("error parsing template %q: %v",  "template", err)
-				}
-				var buffer bytes.Buffer
-				err = tpl.Execute(&buffer, testCase.cluster.Spec)
-				if err != nil {
-					 t.Fatalf("error executing template %q: %v", "template", err)
-				}
-				actualManifest := buffer.Bytes()
-				expectedFileAbsolutePath, _ := filepath.Abs(testCase.expectedManifestPath)
-				expectedManifest, _ := ioutil.ReadFile( expectedFileAbsolutePath )
+			tpl.Option("missingkey=zero")
+			_, err := tpl.ParseFiles(templateFileAbsolutePath)
+			if err != nil {
+				t.Fatalf("error parsing template %q: %v", "template", err)
+			}
+			var buffer bytes.Buffer
+			err = tpl.Execute(&buffer, testCase.cluster.Spec)
+			if err != nil {
+				t.Fatalf("error executing template %q: %v", "template", err)
+			}
+			actualManifest := buffer.Bytes()
+			expectedFileAbsolutePath, _ := filepath.Abs(testCase.expectedManifestPath)
+			expectedManifest, _ := ioutil.ReadFile(expectedFileAbsolutePath)
 
-				actualString := strings.TrimSpace(string(actualManifest))
-				expectedString := strings.TrimSpace(string(expectedManifest))
-				if !reflect.DeepEqual(actualString,expectedString,) {
-					t.Fatalf("Manifests differs: %+v instead of %+v", actualString, expectedString )
-				}
-			})
+			actualString := strings.TrimSpace(string(actualManifest))
+			expectedString := strings.TrimSpace(string(expectedManifest))
+			if !reflect.DeepEqual(actualString, expectedString) {
+				t.Fatalf("Manifests differs: %+v instead of %+v", actualString, expectedString)
+			}
+		})
 	}
 }
