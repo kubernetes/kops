39bc5a75d00e73f679abed445688ebfdb3e612e0
Justin Santa Barbara <justinsb@google.com>
2020-01-11 14:06:40 -0500

    Move kubectl dependency to k8s.io/kubectl
    Helps pare down our dependency on kubernetes/kubernetes

--- cmd/kops/create.go
+++ cmd/kops/create.go
@@ -33,9 +33,9 @@ import (
 	"k8s.io/kops/upup/pkg/fi/cloudup"
 	"k8s.io/kops/util/pkg/text"
 	"k8s.io/kops/util/pkg/vfs"
-	cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
-	"k8s.io/kubernetes/pkg/kubectl/util/i18n"
-	"k8s.io/kubernetes/pkg/kubectl/util/templates"
+	cmdutil "k8s.io/kubectl/pkg/cmd/util"
+	"k8s.io/kubectl/pkg/util/i18n"
+	"k8s.io/kubectl/pkg/util/templates"
 )
 
 type CreateOptions struct {
