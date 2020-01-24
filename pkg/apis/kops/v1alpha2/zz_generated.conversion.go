8d2b0cc966ceb7d6722de0e7edc1aa467370d4da
Roberto Rodriguez Alcala <rrodriguezalcala@pinterest.com>
2020-01-24 01:42:49 -0800

    Fixing ci tests

--- pkg/apis/kops/v1alpha2/zz_generated.conversion.go
+++ pkg/apis/kops/v1alpha2/zz_generated.conversion.go
@@ -3926,6 +3926,9 @@ func autoConvert_v1alpha2_KubeSchedulerConfig_To_kops_KubeSchedulerConfig(in *Ku
 	out.UsePolicyConfigMap = in.UsePolicyConfigMap
 	out.FeatureGates = in.FeatureGates
 	out.MaxPersistentVolumes = in.MaxPersistentVolumes
+	out.Qps = in.Qps
+	out.Burst = in.Burst
+	out.KubeConfig = in.KubeConfig
 	return nil
 }
 
@@ -3950,6 +3953,9 @@ func autoConvert_kops_KubeSchedulerConfig_To_v1alpha2_KubeSchedulerConfig(in *ko
 	out.UsePolicyConfigMap = in.UsePolicyConfigMap
 	out.FeatureGates = in.FeatureGates
 	out.MaxPersistentVolumes = in.MaxPersistentVolumes
+	out.Qps = in.Qps
+	out.Burst = in.Burst
+	out.KubeConfig = in.KubeConfig
 	return nil
 }
 
