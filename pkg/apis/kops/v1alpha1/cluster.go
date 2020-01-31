c6eb6113fad1b996b7b6264afcfcbd58588f0535
Peter Rifel <pgrifel@gmail.com>
2020-01-31 13:34:39 -0600

    Add an EnvVar type to the API
    This was copied from the k8s API [0] with the ValueFrom field removed for simplicity.
    
    It will be used by the aws-vpc-cni provider and etcd
    
    [0] https://github.com/kubernetes/api/blob/bb84ecda436d0529a0ca484be5cafd6a42793ccb/core/v1/types.go#L1827-L1846

--- pkg/apis/kops/v1alpha1/cluster.go
+++ pkg/apis/kops/v1alpha1/cluster.go
@@ -527,6 +527,23 @@ func (t *TerraformSpec) IsEmpty() bool {
 	return t.ProviderExtraConfig == nil
 }
 
+// EnvVar represents an environment variable present in a Container.
+type EnvVar struct {
+	// Name of the environment variable. Must be a C_IDENTIFIER.
+	Name string `json:"name"`
+
+	// Variable references $(VAR_NAME) are expanded
+	// using the previous defined environment variables in the container and
+	// any service environment variables. If a variable cannot be resolved,
+	// the reference in the input string will be unchanged. The $(VAR_NAME)
+	// syntax can be escaped with a double $$, ie: $$(VAR_NAME). Escaped
+	// references will never be expanded, regardless of whether the variable
+	// exists or not.
+	// Defaults to "".
+	// +optional
+	Value string `json:"value,omitempty"`
+}
+
 type GossipConfig struct {
 	Protocol  *string       `json:"protocol,omitempty"`
 	Listen    *string       `json:"listen,omitempty"`
