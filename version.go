65df260870c8be7d1004ada01137c888495670af
Justin SB <justinsb@google.com>
2020-02-10 21:49:56 -0500

    Release 1.17.0-alpha.4

--- version.go
+++ version.go
@@ -23,8 +23,8 @@ var Version = KOPS_RELEASE_VERSION
 
 // These constants are parsed by build tooling - be careful about changing the formats
 const (
-	KOPS_RELEASE_VERSION = "1.17.0-alpha.3"
-	KOPS_CI_VERSION      = "1.17.0-alpha.4"
+	KOPS_RELEASE_VERSION = "1.17.0-alpha.4"
+	KOPS_CI_VERSION      = "1.17.0-alpha.5"
 )
 
 // GitVersion should be replaced by the makefile
