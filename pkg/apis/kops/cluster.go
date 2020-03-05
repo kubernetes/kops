c6985c24386638acdeb21b0e15b8dd9660d37c63
Ole Markus With <olemarkus@gmail.com>
2020-03-05 09:22:09 +0100

    Revert "Automated cherry pick of #8589: Bump Cilium to 1.7 for k8s 1.12+ #8591: Fix typo in the cilium default version"

--- pkg/apis/kops/cluster.go
+++ pkg/apis/kops/cluster.go
@@ -576,6 +576,9 @@ func (c *Cluster) FillDefaults() error {
 	} else if c.Spec.Networking.AmazonVPC != nil {
 		// OK
 	} else if c.Spec.Networking.Cilium != nil {
+		if c.Spec.Networking.Cilium.Version == "" {
+			c.Spec.Networking.Cilium.Version = CiliumDefaultVersion
+		}
 		// OK
 	} else if c.Spec.Networking.LyftVPC != nil {
 		// OK
