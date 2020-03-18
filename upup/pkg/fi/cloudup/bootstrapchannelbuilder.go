0cbd24105bfc85e6d99e53739ef71fc40e5283c9
liranp <liran.polak@gmail.com>
2020-03-18 18:48:37 +0200

    feature(spotinst): upgrade controller (v1.0.57)

--- upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
+++ upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
@@ -540,7 +540,7 @@ func (b *BootstrapChannelBuilder) buildAddons() *channelsapi.Addons {
 		{
 			id := "v1.14.0"
 			location := key + "/" + id + ".yaml"
-			version := "1.0.50"
+			version := "1.0.57"
 
 			addons.Spec.Addons = append(addons.Spec.Addons, &channelsapi.AddonSpec{
 				Name:              fi.String(key),
