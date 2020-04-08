7e006954cc17cbee2063e90158b93d8cbfcc86eb
Rodrigo Menezes <brdude@gmail.com>
2020-04-08 15:45:39 -0700

    add verify-bindata.sh & separate bindata check and update make commands.

---
+++ hack/verify-bindata.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Copyright 2020 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+. "$(dirname "${BASH_SOURCE[0]}")/common.sh"
+
+cd "${KOPS_ROOT}"
+
+make update-bindata
+
+changes=$(git status --porcelain || true)
+if [ -n "${changes}" ]; then
+  echo "ERROR: bindata is not up to date; please run: make update-bindata"
+  echo "git diff:"
+  git --no-pager diff
+  exit 1
+fi
