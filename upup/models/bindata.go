4d5d59f405bad9b1295922819a0ca310f89a705f
Ash Caire <ash.caire@gmail.com>
2020-04-29 17:01:43 +0800

    style(spotinst): Remove erroneous YAML separators
    This was causing the following error when applying the channel:
    
    I0429 09:05:44.828460   15185 apply.go:71] configmap/spotinst-kubernetes-cluster-controller-config unchanged
    secret/spotinst-kubernetes-cluster-controller-certs unchanged
    serviceaccount/spotinst-kubernetes-cluster-controller unchanged
    clusterrole.rbac.authorization.k8s.io/spotinst-kubernetes-cluster-controller unchanged
    clusterrolebinding.rbac.authorization.k8s.io/spotinst-kubernetes-cluster-controller unchanged
    deployment.apps/spotinst-kubernetes-cluster-controller configured
    error: error validating "/tmp/channel152526040/manifest.yaml": error validating data: [apiVersion not set, kind not set]; if you choose to ignore these errors, turn validation off with --validate=false
    Error: error updating "spotinst-kubernetes-cluster-controller.addons.k8s.io": error applying update from "s3://REDACTED/kops/REDACTED/addons/spotinst-kubernetes-cluster-controller.addons.k8s.io/v1.9.0.yaml": error running kubectl

--- upup/models/bindata.go
+++ upup/models/bindata.go
@@ -16484,7 +16484,6 @@ spec:
         tolerationSeconds: 150
       - key: node-role.kubernetes.io/master
         operator: Exists
----
 `)
 
 func cloudupResourcesAddonsSpotinstKubernetesClusterControllerAddonsK8sIoV1140YamlTemplateBytes() ([]byte, error) {
@@ -16639,7 +16638,6 @@ spec:
       tolerations:
       - key: node-role.kubernetes.io/master
         effect: NoSchedule
----
 `)
 
 func cloudupResourcesAddonsSpotinstKubernetesClusterControllerAddonsK8sIoV190YamlTemplateBytes() ([]byte, error) {
