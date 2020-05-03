85f611fdca66fb2706e8a103f93ecb472552a378
John Gardiner Myers <jgmyers@proofpoint.com>
2020-05-02 21:43:29 -0700

    Test more static pods during cluster validation

--- pkg/validation/validate_cluster.go
+++ pkg/validation/validate_cluster.go
@@ -199,13 +199,22 @@ func (v *ValidationCluster) collectComponentFailures(ctx context.Context, client
 	return nil
 }
 
+var masterStaticPods = []string{
+	"kube-apiserver",
+	"kube-controller-manager",
+	"kube-scheduler",
+}
+
 func (v *ValidationCluster) collectPodFailures(ctx context.Context, client kubernetes.Interface, nodes []v1.Node) error {
-	masterWithoutManager := map[string]bool{}
+	masterWithoutPod := map[string]map[string]bool{}
 	nodeByAddress := map[string]string{}
 	for _, node := range nodes {
 		labels := node.GetLabels()
 		if labels != nil && labels["kubernetes.io/role"] == "master" {
-			masterWithoutManager[node.Name] = true
+			masterWithoutPod[node.Name] = map[string]bool{}
+			for _, pod := range masterStaticPods {
+				masterWithoutPod[node.Name][pod] = true
+			}
 		}
 		for _, nodeAddress := range node.Status.Addresses {
 			nodeByAddress[nodeAddress.Address] = node.Name
@@ -254,9 +263,9 @@ func (v *ValidationCluster) collectPodFailures(ctx context.Context, client kuber
 
 		}
 
-		labels := pod.GetLabels()
-		if pod.Namespace == "kube-system" && labels != nil && labels["k8s-app"] == "kube-controller-manager" {
-			delete(masterWithoutManager, nodeByAddress[pod.Status.HostIP])
+		app := pod.GetLabels()["k8s-app"]
+		if pod.Namespace == "kube-system" && masterWithoutPod[nodeByAddress[pod.Status.HostIP]][app] {
+			delete(masterWithoutPod[nodeByAddress[pod.Status.HostIP]], app)
 		}
 		return nil
 	})
@@ -264,12 +273,14 @@ func (v *ValidationCluster) collectPodFailures(ctx context.Context, client kuber
 		return fmt.Errorf("error listing Pods: %v", err)
 	}
 
-	for node := range masterWithoutManager {
-		v.addError(&ValidationError{
-			Kind:    "Node",
-			Name:    node,
-			Message: fmt.Sprintf("master %q is missing kube-controller-manager pod", node),
-		})
+	for node, nodeMap := range masterWithoutPod {
+		for app := range nodeMap {
+			v.addError(&ValidationError{
+				Kind:    "Node",
+				Name:    node,
+				Message: fmt.Sprintf("master %q is missing %s pod", node, app),
+			})
+		}
 	}
 
 	return nil
