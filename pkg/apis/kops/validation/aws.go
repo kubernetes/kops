97efc5e26c163b8aee65138e20d805ca3b3183cc
John Gardiner Myers <jgmyers@proofpoint.com>
2020-05-04 23:19:41 -0700

    Remove no-longer-needed validation

--- pkg/apis/kops/validation/aws.go
+++ pkg/apis/kops/validation/aws.go
@@ -17,13 +17,11 @@ limitations under the License.
 package validation
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apimachinery/pkg/util/validation/field"
-	"k8s.io/klog"
 	"k8s.io/kops/pkg/apis/kops"
 	"k8s.io/kops/upup/pkg/fi/cloudup/awsup"
 )
@@ -46,8 +44,6 @@ func awsValidateInstanceGroup(ig *kops.InstanceGroup) field.ErrorList {
 	allErrs = append(allErrs, awsValidateAdditionalSecurityGroups(field.NewPath("spec", "additionalSecurityGroups"), ig.Spec.AdditionalSecurityGroups)...)
 
 	allErrs = append(allErrs, awsValidateMachineType(field.NewPath(ig.GetName(), "spec", "machineType"), ig.Spec.MachineType)...)
-
-	allErrs = append(allErrs, awsValidateAMIforNVMe(field.NewPath(ig.GetName(), "spec", "machineType"), ig)...)
 
 	allErrs = append(allErrs, awsValidateSpotDurationInMinute(field.NewPath(ig.GetName(), "spec", "spotDurationInMinutes"), ig)...)
 
@@ -86,28 +82,6 @@ func awsValidateMachineType(fieldPath *field.Path, machineType string) field.Err
 		}
 	}
 
-	return allErrs
-}
-
-// TODO: make image validation smarter? graduate from jessie to stretch? This is quick and dirty because we keep getting reports
-func awsValidateAMIforNVMe(fieldPath *field.Path, ig *kops.InstanceGroup) field.ErrorList {
-	// TODO: how can we put this list somewhere better?
-	NVMe_INSTANCE_PREFIXES := []string{"P3", "C5", "M5", "H1", "I3"}
-
-	allErrs := field.ErrorList{}
-
-	for _, prefix := range NVMe_INSTANCE_PREFIXES {
-		for _, machineType := range strings.Split(ig.Spec.MachineType, ",") {
-			if strings.Contains(strings.ToUpper(machineType), strings.ToUpper(prefix)) {
-				klog.V(2).Infof("machineType %s requires an image based on stretch to operate. Trying to check compatibility", machineType)
-				if strings.Contains(ig.Spec.Image, "jessie") {
-					errString := fmt.Sprintf("%s cannot use machineType %s with image based on Debian jessie.", ig.Name, machineType)
-					allErrs = append(allErrs, field.Forbidden(fieldPath, errString))
-					continue
-				}
-			}
-		}
-	}
 	return allErrs
 }
 
