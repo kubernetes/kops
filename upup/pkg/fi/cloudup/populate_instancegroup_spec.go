ec4fe1e7e883a42cdbe86d34bf0848bfbcbb013f
John Gardiner Myers <jgmyers@proofpoint.com>
2020-05-12 21:11:29 -0700

    Don't put bastions in the utility subnets

--- upup/pkg/fi/cloudup/populate_instancegroup_spec.go
+++ upup/pkg/fi/cloudup/populate_instancegroup_spec.go
@@ -137,14 +137,6 @@ func PopulateInstanceGroupSpec(cluster *kops.Cluster, input *kops.InstanceGroup,
 		if len(ig.Spec.Subnets) == 0 {
 			return nil, fmt.Errorf("Master InstanceGroup %s did not specify any Subnets", ig.ObjectMeta.Name)
 		}
-	} else if ig.Spec.Role == kops.InstanceGroupRoleBastion {
-		if len(ig.Spec.Subnets) == 0 {
-			for _, subnet := range cluster.Spec.Subnets {
-				if subnet.Type == kops.SubnetTypeUtility {
-					ig.Spec.Subnets = append(ig.Spec.Subnets, subnet.Name)
-				}
-			}
-		}
 	} else {
 		if len(ig.Spec.Subnets) == 0 {
 			for _, subnet := range cluster.Spec.Subnets {
