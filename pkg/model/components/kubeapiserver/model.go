3b3c55f533cc1ce98a54a593dd68a94fbe2ce45e
John Gardiner Myers <jgmyers@proofpoint.com>
2020-06-16 07:39:53 -0700

    Revert "Fix kube-apiserver-healthcheck image"
    This reverts commit 7e52f223

--- pkg/model/components/kubeapiserver/model.go
+++ pkg/model/components/kubeapiserver/model.go
@@ -94,7 +94,7 @@ kind: Pod
 spec:
   containers:
   - name: healthcheck
-    image: kope/kube-apiserver-healthcheck:1.18.0-beta.1
+    image: kope/kube-apiserver-healthcheck:1.18.0-alpha.3
     livenessProbe:
       httpGet:
         # The sidecar serves a healthcheck on the same port,
