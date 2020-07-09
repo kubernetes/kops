f213854330ad7afe2a213a88cc0da502bb10b2d3
Peter Rifel <pgrifel@gmail.com>
2020-07-08 22:15:24 -0500

    Update integration test outputs with filebase64

--- tests/integration/update_cluster/privateweave/kubernetes.tf
+++ tests/integration/update_cluster/privateweave/kubernetes.tf
@@ -494,7 +494,7 @@ resource "aws_launch_template" "master-us-test-1a-masters-privateweave-example-c
     "kops.k8s.io/instancegroup"                      = "master-us-test-1a"
     "kubernetes.io/cluster/privateweave.example.com" = "owned"
   }
-  user_data = file("${path.module}/data/aws_launch_template_master-us-test-1a.masters.privateweave.example.com_user_data")
+  user_data = filebase64("${path.module}/data/aws_launch_template_master-us-test-1a.masters.privateweave.example.com_user_data")
 }
 
 resource "aws_launch_template" "nodes-privateweave-example-com" {
@@ -548,7 +548,7 @@ resource "aws_launch_template" "nodes-privateweave-example-com" {
     "kops.k8s.io/instancegroup"                      = "nodes"
     "kubernetes.io/cluster/privateweave.example.com" = "owned"
   }
-  user_data = file("${path.module}/data/aws_launch_template_nodes.privateweave.example.com_user_data")
+  user_data = filebase64("${path.module}/data/aws_launch_template_nodes.privateweave.example.com_user_data")
 }
 
 resource "aws_nat_gateway" "us-test-1a-privateweave-example-com" {
