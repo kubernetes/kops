f3e6ac874da5fe588ad37ed9f7cdc51d386f8384
Peter Rifel <pgrifel@gmail.com>
2020-07-08 23:09:24 -0500

    Update integration test outputs with new ssh key tags

--- tests/integration/update_cluster/shared_subnet/kubernetes.tf
+++ tests/integration/update_cluster/shared_subnet/kubernetes.tf
@@ -214,6 +214,11 @@ resource "aws_iam_role" "nodes-sharedsubnet-example-com" {
 resource "aws_key_pair" "kubernetes-sharedsubnet-example-com-c4a6ed9aa889b9e2c39cd663eb9c7157" {
   key_name   = "kubernetes.sharedsubnet.example.com-c4:a6:ed:9a:a8:89:b9:e2:c3:9c:d6:63:eb:9c:71:57"
   public_key = file("${path.module}/data/aws_key_pair_kubernetes.sharedsubnet.example.com-c4a6ed9aa889b9e2c39cd663eb9c7157_public_key")
+  tags = {
+    "KubernetesCluster"                              = "sharedsubnet.example.com"
+    "Name"                                           = "sharedsubnet.example.com"
+    "kubernetes.io/cluster/sharedsubnet.example.com" = "owned"
+  }
 }
 
 resource "aws_launch_template" "master-us-test-1a-masters-sharedsubnet-example-com" {
