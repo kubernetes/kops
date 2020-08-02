698bf974d8ec16dd5c7fb8b8682d337fee0e6793
Justin SB <justinsb@google.com>
2020-08-02 16:16:01 -0400

    Release 1.18.0

--- pkg/model/components/kubeapiserver/model.go
+++ pkg/model/components/kubeapiserver/model.go
@@ -95,7 +95,7 @@ kind: Pod
 spec:
   containers:
   - name: healthcheck
-    image: kope/kube-apiserver-healthcheck:1.18.0-beta.2
+    image: kope/kube-apiserver-healthcheck:1.18.0
     livenessProbe:
       httpGet:
         # The sidecar serves a healthcheck on the same port,
