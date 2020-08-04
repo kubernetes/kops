77613b3cafdb974b0b4f27f5dec47f9afbf34157
liranp <liran.polak@gmail.com>
2020-08-04 23:39:02 +0300

    fix(spot): change `ScaleDown.MaxPercentage` from int to float64

--- vendor/github.com/spotinst/spotinst-sdk-go/spotinst/credentials/provider.go
+++ vendor/github.com/spotinst/spotinst-sdk-go/spotinst/credentials/provider.go
@@ -5,13 +5,13 @@ import "fmt"
 // A Value is the Spotinst credentials value for individual credential fields.
 type Value struct {
 	// Spotinst API token.
-	Token string `json:"token"`
+	Token string `ini:"token" json:"token"`
 
 	// Spotinst account ID.
-	Account string `json:"account"`
+	Account string `ini:"account" json:"account"`
 
 	// Provider used to get credentials.
-	ProviderName string `json:"-"`
+	ProviderName string `ini:"-" json:"-"`
 }
 
 // A Provider is the interface for any component which will provide credentials
@@ -25,4 +25,20 @@ type Provider interface {
 	// Refresh returns nil if it successfully retrieved the value. Error is
 	// returned if the value were not obtainable, or empty.
 	Retrieve() (Value, error)
+}
+
+// IsEmpty if all fields of a Value are empty.
+func (v *Value) IsEmpty() bool { return v.Token == "" && v.Account == "" }
+
+// IsComplete if all fields of a Value are set.
+func (v *Value) IsComplete() bool { return v.Token != "" && v.Account != "" }
+
+// Merge merges the passed in Value into the existing Value object.
+func (v *Value) Merge(v2 Value) {
+	if v.Token == "" {
+		v.Token = v2.Token
+	}
+	if v.Account == "" {
+		v.Account = v2.Account
+	}
 }
