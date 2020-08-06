96ef74eeadbdc5774ae2a1de8b6241651a1ac52d
Peter Rifel <pgrifel@gmail.com>
2020-08-06 08:05:32 -0500

    Implement CRUD logic for cloudmock openstack
    This is the bulk of the changes necessary to support openstack integration tests.
    
    As mentioned in cloudmock/openstack/README.md, this involved a lot of wireshark to understand
    how the gophercloud clients builds the requests and expects the responses.

--- cloudmock/openstack/mocknetworking/ports.go
+++ cloudmock/openstack/mocknetworking/ports.go
@@ -17,18 +17,176 @@ limitations under the License.
 package mocknetworking
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"net/url"
+	"regexp"
+	"strings"
+
+	"github.com/google/uuid"
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/ports"
 )
 
+type portListResponse struct {
+	Ports []ports.Port `json:"ports"`
+}
+
+type portGetResponse struct {
+	Port ports.Port `json:"port"`
+}
+
+type portCreateRequest struct {
+	Port ports.CreateOpts `json:"port"`
+}
+
 func (m *MockClient) mockPorts() {
+	re := regexp.MustCompile(`/ports/?`)
 
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		m.mutex.Lock()
 		defer m.mutex.Unlock()
 
 		w.Header().Add("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
+
+		portID := re.ReplaceAllString(r.URL.Path, "")
+		switch r.Method {
+		case http.MethodGet:
+			if portID == "" {
+				r.ParseForm()
+				m.listPorts(w, r.Form)
+			} else {
+				m.getPort(w, portID)
+			}
+		case http.MethodPut:
+			m.tagPort(w, r)
+		case http.MethodPost:
+			m.createPort(w, r)
+		case http.MethodDelete:
+			m.deletePort(w, portID)
+		default:
+			w.WriteHeader(http.StatusBadRequest)
+		}
 	}
 	m.Mux.HandleFunc("/ports/", handler)
 	m.Mux.HandleFunc("/ports", handler)
+}
+
+func (m *MockClient) listPorts(w http.ResponseWriter, vals url.Values) {
+
+	w.WriteHeader(http.StatusOK)
+
+	ports := make([]ports.Port, 0)
+	nameFilter := vals.Get("name")
+	for _, p := range m.ports {
+		if nameFilter != "" && nameFilter != p.Name {
+			continue
+		}
+		ports = append(ports, p)
+	}
+
+	resp := portListResponse{
+		Ports: ports,
+	}
+	respB, err := json.Marshal(resp)
+	if err != nil {
+		panic(fmt.Sprintf("failed to marshal %+v", resp))
+	}
+	_, err = w.Write(respB)
+	if err != nil {
+		panic("failed to write body")
+	}
+}
+
+func (m *MockClient) getPort(w http.ResponseWriter, portID string) {
+	if port, ok := m.ports[portID]; ok {
+		resp := portGetResponse{
+			Port: port,
+		}
+		respB, err := json.Marshal(resp)
+		if err != nil {
+			panic(fmt.Sprintf("failed to marshal %+v", resp))
+		}
+		_, err = w.Write(respB)
+		if err != nil {
+			panic("failed to write body")
+		}
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
+func (m *MockClient) deletePort(w http.ResponseWriter, portID string) {
+	if _, ok := m.ports[portID]; ok {
+		delete(m.ports, portID)
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
+func (m *MockClient) createPort(w http.ResponseWriter, r *http.Request) {
+	var create portCreateRequest
+	err := json.NewDecoder(r.Body).Decode(&create)
+	if err != nil {
+		panic("error decoding create port request")
+	}
+
+	fixedIPs := make([]ports.IP, 0)
+	// The request type uses a []interface{map[string]interface{}]} to represent []ports.IP
+	if rawIPs, ok := create.Port.FixedIPs.([]interface{}); ok {
+		for _, rawFixedIP := range rawIPs {
+			if rawIP, ok := rawFixedIP.(map[string]interface{}); ok {
+				if subnetID, ok := rawIP["subnet_id"]; ok {
+					if subnet, ok := subnetID.(string); ok {
+						fixedIPs = append(fixedIPs, ports.IP{SubnetID: subnet})
+					}
+				}
+			}
+		}
+	}
+	w.WriteHeader(http.StatusAccepted)
+
+	p := ports.Port{
+		ID:             uuid.New().String(),
+		Name:           create.Port.Name,
+		NetworkID:      create.Port.NetworkID,
+		SecurityGroups: *create.Port.SecurityGroups,
+		FixedIPs:       fixedIPs,
+	}
+	m.ports[p.ID] = p
+
+	resp := portGetResponse{
+		Port: p,
+	}
+	respB, err := json.Marshal(resp)
+	if err != nil {
+		panic(fmt.Sprintf("failed to marshal %+v", resp))
+	}
+	_, err = w.Write(respB)
+	if err != nil {
+		panic("failed to write body")
+	}
+}
+
+func (m *MockClient) tagPort(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	// /ports/<portid>/tags/<tag>
+	parts := strings.Split(strings.Trim(path, "/"), "/")
+	if len(parts) != 4 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	portID := parts[1]
+	tag := parts[3]
+
+	if _, ok := m.ports[portID]; !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	port := m.ports[portID]
+	port.Tags = append(port.Tags, tag)
+	m.ports[portID] = port
+
+	w.WriteHeader(http.StatusCreated)
 }
--- cloudmock/openstack/mocknetworking/securitygroups.go
+++ cloudmock/openstack/mocknetworking/securitygroups.go
@@ -17,18 +17,136 @@ limitations under the License.
 package mocknetworking
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"net/url"
+	"regexp"
+
+	"github.com/google/uuid"
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/security/groups"
 )
 
+type groupListResponse struct {
+	SecurityGroups []groups.SecGroup `json:"security_groups"`
+}
+
+type groupGetResponse struct {
+	SecurityGroup groups.SecGroup `json:"security_group"`
+}
+
+type groupCreateRequest struct {
+	SecurityGroup groups.CreateOpts `json:"security_group"`
+}
+
 func (m *MockClient) mockSecurityGroups() {
+	re := regexp.MustCompile(`/security-groups/?`)
 
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		m.mutex.Lock()
 		defer m.mutex.Unlock()
 
 		w.Header().Add("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
+
+		sgID := re.ReplaceAllString(r.URL.Path, "")
+		switch r.Method {
+		case http.MethodGet:
+			if sgID == "" {
+				r.ParseForm()
+				m.listSecurityGroups(w, r.Form)
+			} else {
+				m.getSecurityGroup(w, sgID)
+			}
+		case http.MethodPost:
+			m.createSecurityGroup(w, r)
+		case http.MethodDelete:
+			m.deleteSecurityGroup(w, sgID)
+		default:
+			w.WriteHeader(http.StatusBadRequest)
+		}
 	}
 	m.Mux.HandleFunc("/security-groups/", handler)
 	m.Mux.HandleFunc("/security-groups", handler)
+}
+
+func (m *MockClient) listSecurityGroups(w http.ResponseWriter, vals url.Values) {
+
+	w.WriteHeader(http.StatusOK)
+
+	sgs := make([]groups.SecGroup, 0)
+	nameFilter := vals.Get("name")
+	for _, s := range m.securityGroups {
+		if nameFilter != "" && s.Name != nameFilter {
+			continue
+		}
+		sgs = append(sgs, s)
+	}
+
+	resp := groupListResponse{
+		SecurityGroups: sgs,
+	}
+	respB, err := json.Marshal(resp)
+	if err != nil {
+		panic(fmt.Sprintf("failed to marshal %+v", resp))
+	}
+	_, err = w.Write(respB)
+	if err != nil {
+		panic("failed to write body")
+	}
+}
+
+func (m *MockClient) getSecurityGroup(w http.ResponseWriter, groupID string) {
+	if sg, ok := m.securityGroups[groupID]; ok {
+		resp := groupGetResponse{
+			SecurityGroup: sg,
+		}
+		respB, err := json.Marshal(resp)
+		if err != nil {
+			panic(fmt.Sprintf("failed to marshal %+v", resp))
+		}
+		_, err = w.Write(respB)
+		if err != nil {
+			panic("failed to write body")
+		}
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
+func (m *MockClient) deleteSecurityGroup(w http.ResponseWriter, groupID string) {
+	if _, ok := m.securityGroups[groupID]; ok {
+		delete(m.securityGroups, groupID)
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
+func (m *MockClient) createSecurityGroup(w http.ResponseWriter, r *http.Request) {
+	var create groupCreateRequest
+	err := json.NewDecoder(r.Body).Decode(&create)
+	if err != nil {
+		panic("error decoding create group request")
+	}
+
+	w.WriteHeader(http.StatusAccepted)
+
+	s := groups.SecGroup{
+		ID:          uuid.New().String(),
+		Name:        create.SecurityGroup.Name,
+		Description: create.SecurityGroup.Description,
+	}
+	m.securityGroups[s.ID] = s
+
+	resp := groupGetResponse{
+		SecurityGroup: s,
+	}
+	respB, err := json.Marshal(resp)
+	if err != nil {
+		panic(fmt.Sprintf("failed to marshal %+v", resp))
+	}
+	_, err = w.Write(respB)
+	if err != nil {
+		panic("failed to write body")
+	}
 }
