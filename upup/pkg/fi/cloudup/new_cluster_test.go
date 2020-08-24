bd7c5e4045c97ece8aba5cad66e08fa6665ed23e
Ciprian Hacman <ciprian.hacman@sematext.com>
2020-08-24 12:32:57 +0300

    Update validation for Calico to assume etcd3 as default

--- upup/pkg/fi/cloudup/new_cluster_test.go
+++ upup/pkg/fi/cloudup/new_cluster_test.go
@@ -20,8 +20,12 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/ghodss/yaml"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/kops/pkg/apis/kops"
+	api "k8s.io/kops/pkg/apis/kops"
+	"k8s.io/kops/pkg/diff"
+	"k8s.io/kops/upup/pkg/fi"
 )
 
 func TestRemoveSharedPrefix(t *testing.T) {
@@ -59,7 +63,7 @@ func TestRemoveSharedPrefix(t *testing.T) {
 }
 
 func TestCreateEtcdCluster(t *testing.T) {
-	masters := []*kops.InstanceGroup{
+	masters := []*api.InstanceGroup{
 		{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "master1",
@@ -73,4 +77,273 @@ func TestCreateEtcdCluster(t *testing.T) {
 		t.Errorf("Expected: %v, Got: %v", name, etcd.Name)
 	}
 
+}
+
+func TestSetupNetworking(t *testing.T) {
+	tests := []struct {
+		options  NewClusterOptions
+		actual   api.Cluster
+		expected api.Cluster
+	}{
+		{
+			options: NewClusterOptions{
+				Networking: "kubenet",
+			},
+			expected: api.Cluster{
+				Spec: api.ClusterSpec{
+					Networking: &api.NetworkingSpec{
+						Kubenet: &api.KubenetNetworkingSpec{},
+					},
+				},
+			},
+		},
+		{
+			options: NewClusterOptions{
+				Networking: "external",
+			},
+			expected: api.Cluster{
+				Spec: api.ClusterSpec{
+					Networking: &api.NetworkingSpec{
+						External: &api.ExternalNetworkingSpec{},
+					},
+				},
+			},
+		},
+		{
+			options: NewClusterOptions{
+				Networking: "cni",
+			},
+			expected: api.Cluster{
+				Spec: api.ClusterSpec{
+					Networking: &api.NetworkingSpec{
+						CNI: &api.CNINetworkingSpec{},
+					},
+				},
+			},
+		},
+		{
+			options: NewClusterOptions{
+				Networking: "kopeio",
+			},
+			expected: api.Cluster{
+				Spec: api.ClusterSpec{
+					Networking: &api.NetworkingSpec{
+						Kopeio: &api.KopeioNetworkingSpec{},
+					},
+				},
+			},
+		},
+		{
+			options: NewClusterOptions{
+				Networking: "kopeio-vxlan",
+			},
+			expected: api.Cluster{
+				Spec: api.ClusterSpec{
+					Networking: &api.NetworkingSpec{
+						Kopeio: &api.KopeioNetworkingSpec{},
+					},
+				},
+			},
+		},
+		{
+			options: NewClusterOptions{
+				Networking: "weave",
+			},
+			expected: api.Cluster{
+				Spec: api.ClusterSpec{
+					Networking: &api.NetworkingSpec{
+						Weave: &api.WeaveNetworkingSpec{},
+					},
+				},
+			},
+		},
+		{
+			options: NewClusterOptions{
+				Networking: "flannel",
+			},
+			expected: api.Cluster{
+				Spec: api.ClusterSpec{
+					Networking: &api.NetworkingSpec{
+						Flannel: &api.FlannelNetworkingSpec{
+							Backend: "vxlan",
+						},
+					},
+				},
+			},
+		},
+		{
+			options: NewClusterOptions{
+				Networking: "flannel-vxlan",
+			},
+			expected: api.Cluster{
+				Spec: api.ClusterSpec{
+					Networking: &api.NetworkingSpec{
+						Flannel: &api.FlannelNetworkingSpec{
+							Backend: "vxlan",
+						},
+					},
+				},
+			},
+		},
+		{
+			options: NewClusterOptions{
+				Networking: "flannel-udp",
+			},
+			expected: api.Cluster{
+				Spec: api.ClusterSpec{
+					Networking: &api.NetworkingSpec{
+						Flannel: &api.FlannelNetworkingSpec{
+							Backend: "udp",
+						},
+					},
+				},
+			},
+		},
+		{
+			options: NewClusterOptions{
+				Networking: "calico",
+			},
+			expected: api.Cluster{
+				Spec: api.ClusterSpec{
+					Networking: &api.NetworkingSpec{
+						Calico: &api.CalicoNetworkingSpec{
+							MajorVersion: "v3",
+						},
+					},
+				},
+			},
+		},
+		{
+			options: NewClusterOptions{
+				Networking: "canal",
+			},
+			expected: api.Cluster{
+				Spec: api.ClusterSpec{
+					Networking: &api.NetworkingSpec{
+						Canal: &api.CanalNetworkingSpec{},
+					},
+				},
+			},
+		},
+		{
+			options: NewClusterOptions{
+				Networking: "kube-router",
+			},
+			expected: api.Cluster{
+				Spec: api.ClusterSpec{
+					KubeProxy: &api.KubeProxyConfig{
+						Enabled: fi.Bool(false),
+					},
+					Networking: &api.NetworkingSpec{
+						Kuberouter: &api.KuberouterNetworkingSpec{},
+					},
+				},
+			},
+		},
+		{
+			options: NewClusterOptions{
+				Networking: "amazonvpc",
+			},
+			expected: api.Cluster{
+				Spec: api.ClusterSpec{
+					Networking: &api.NetworkingSpec{
+						AmazonVPC: &api.AmazonVPCNetworkingSpec{},
+					},
+				},
+			},
+		},
+		{
+			options: NewClusterOptions{
+				Networking: "amazon-vpc-routed-eni",
+			},
+			expected: api.Cluster{
+				Spec: api.ClusterSpec{
+					Networking: &api.NetworkingSpec{
+						AmazonVPC: &api.AmazonVPCNetworkingSpec{},
+					},
+				},
+			},
+		},
+		{
+			options: NewClusterOptions{
+				Networking: "cilium",
+			},
+			expected: api.Cluster{
+				Spec: api.ClusterSpec{
+					KubeProxy: &api.KubeProxyConfig{
+						Enabled: fi.Bool(false),
+					},
+					Networking: &api.NetworkingSpec{
+						Cilium: &api.CiliumNetworkingSpec{
+							EnableNodePort: true,
+							Hubble:         api.HubbleSpec{},
+						},
+					},
+				},
+			},
+		},
+		{
+			options: NewClusterOptions{
+				Networking: "cilium-etcd",
+			},
+			expected: api.Cluster{
+				Spec: api.ClusterSpec{
+					KubeProxy: &api.KubeProxyConfig{
+						Enabled: fi.Bool(false),
+					},
+					Networking: &api.NetworkingSpec{
+						Cilium: &api.CiliumNetworkingSpec{
+							EnableNodePort: true,
+							EtcdManaged:    true,
+							Hubble:         api.HubbleSpec{},
+						},
+					},
+				},
+			},
+		},
+		{
+			options: NewClusterOptions{
+				Networking: "lyftvpc",
+			},
+			expected: api.Cluster{
+				Spec: api.ClusterSpec{
+					Networking: &api.NetworkingSpec{
+						LyftVPC: &api.LyftVPCNetworkingSpec{},
+					},
+				},
+			},
+		},
+		{
+			options: NewClusterOptions{
+				Networking: "gce",
+			},
+			expected: api.Cluster{
+				Spec: api.ClusterSpec{
+					Networking: &api.NetworkingSpec{
+						GCE: &api.GCENetworkingSpec{},
+					},
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		actual := api.Cluster{}
+		err := setupNetworking(&test.options, &actual)
+		if err != nil {
+			t.Errorf("error during network setup: %v", err)
+		}
+		expectedYaml, err := yaml.Marshal(test.expected)
+		if err != nil {
+			t.Errorf("error converting expected cluster spec to yaml: %v", err)
+		}
+		actualYaml, err := yaml.Marshal(actual)
+		if err != nil {
+			t.Errorf("error converting actual cluster spec to yaml: %v", err)
+		}
+		if string(expectedYaml) != string(actualYaml) {
+			diffString := diff.FormatDiff(string(expectedYaml), string(actualYaml))
+			t.Errorf("unexpected cluster networking setup:\n%s", diffString)
+		}
+	}
 }
