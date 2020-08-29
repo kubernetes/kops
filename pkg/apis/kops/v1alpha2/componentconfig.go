ffe219773b1a5535f1add4df5010ceab5fc59316
etwillbefine <tim.gatzemeier@gmx.de>
2020-08-29 22:11:24 +0200

    use list of strings for CORS

--- pkg/apis/kops/v1alpha2/componentconfig.go
+++ pkg/apis/kops/v1alpha2/componentconfig.go
@@ -476,8 +476,9 @@ type KubeAPIServerConfig struct {
 	// EnableProfiling enables profiling via web interface host:port/debug/pprof/
 	EnableProfiling *bool `json:"enableProfiling,omitempty" flag:"profiling"`
 
-	// CorsAllowedOrigins enables CORS for comma separated value
-	CorsAllowedOrigins string `json:"corsAllowedOrigins,omitempty" flag:"cors-allowed-origins"`
+	// CorsAllowedOrigins is a list of origins for CORS. An allowed origin can be a regular
+	// expression to support subdomain matching. If this list is empty CORS will not be enabled.
+	CorsAllowedOrigins []string `json:"corsAllowedOrigins,omitempty" flag:"cors-allowed-origins"`
 }
 
 // KubeControllerManagerConfig is the configuration for the controller
