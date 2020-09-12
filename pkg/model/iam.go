73a899b741405beedddfd38cbc1f78134eeb68b8
Ciprian Hacman <ciprian.hacman@sematext.com>
2020-09-12 08:20:17 +0300

    Only add additional policies to kops managed IAMRoles

--- pkg/model/iam.go
+++ pkg/model/iam.go
@@ -187,7 +187,7 @@ func (b *IAMModelBuilder) buildIAMTasks(igRole kops.InstanceGroupRole, iamName s
 		}
 
 		// Generate additional policies if needed, and attach to existing role
-		{
+		if !shared {
 			additionalPolicy := ""
 			if b.Cluster.Spec.AdditionalPolicies != nil {
 				additionalPolicies := *(b.Cluster.Spec.AdditionalPolicies)
