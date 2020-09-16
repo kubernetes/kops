ad516548f40ad99f2fae8d765962dc013ab78959
Ciprian Hacman <ciprian.hacman@sematext.com>
2020-09-16 09:27:49 +0300

    Release 1.19.0-alpha.4

--- pkg/model/components/kubeapiserver/model.go
+++ pkg/model/components/kubeapiserver/model.go
@@ -95,7 +95,7 @@ kind: Pod
 spec:
   containers:
   - name: healthcheck
-    image: k8s.gcr.io/kops/kube-apiserver-healthcheck:1.19.0-alpha.3
+    image: k8s.gcr.io/kops/kube-apiserver-healthcheck:1.19.0-alpha.4
     livenessProbe:
       httpGet:
         # The sidecar serves a healthcheck on the same port,
