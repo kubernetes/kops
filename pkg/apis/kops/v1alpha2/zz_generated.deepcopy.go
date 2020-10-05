dca69323b707741312b0fcb3a8f158b90fc3c945
nareshku <nareshkumar4ever@gmail.com>
2020-10-04 18:29:42 -0700

    enable kubelet --housekeeping-interval flag

--- pkg/apis/kops/v1alpha2/zz_generated.deepcopy.go
+++ pkg/apis/kops/v1alpha2/zz_generated.deepcopy.go
@@ -2983,6 +2983,11 @@ func (in *KubeletConfigSpec) DeepCopyInto(out *KubeletConfigSpec) {
 		*out = new(bool)
 		**out = **in
 	}
+	if in.HousekeepingInterval != nil {
+		in, out := &in.HousekeepingInterval, &out.HousekeepingInterval
+		*out = new(v1.Duration)
+		**out = **in
+	}
 	return
 }
 
