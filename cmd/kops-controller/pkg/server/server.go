500781528654e3810ac353eee699378f01b5bf60
Ole Markus With <olemarkus@gmail.com>
2020-10-09 08:06:14 +0200

    Apply suggestions from code review
    Co-authored-by: John Gardiner Myers <jgmyers@proofpoint.com>

--- cmd/kops-controller/pkg/server/server.go
+++ cmd/kops-controller/pkg/server/server.go
@@ -179,6 +179,7 @@ func (s *Server) issueCert(name string, pubKey string, id *fi.VerifyResult, vali
 		issueReq.Subject = pkix.Name{
 			CommonName: id.NodeName,
 		}
+		issueReq.AlternateNames = []string{id.NodeName}
 		issueReq.Type = "server"
 	case "kube-proxy":
 		issueReq.Subject = pkix.Name{
