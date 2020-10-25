7263ea99aa547c4bfb94fc1e07237732601da9a3
liranp <liran.polak@gmail.com>
2020-10-25 12:06:36 +0200

    feat(spot): bump spot controller (v1.0.68)

--- upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
+++ upup/pkg/fi/cloudup/bootstrapchannelbuilder.go
@@ -695,7 +695,7 @@ func (b *BootstrapChannelBuilder) buildAddons(c *fi.ModelBuilderContext) (*chann
 		{
 			id := "v1.14.0"
 			location := key + "/" + id + ".yaml"
-			version := "1.0.67"
+			version := "1.0.68"
 
 			addons.Spec.Addons = append(addons.Spec.Addons, &channelsapi.AddonSpec{
 				Name:              fi.String(key),
