1b87f1d5327d0e440aab420c7fd55f670b52ccc2
Peter Rifel <pgrifel@gmail.com>
2020-10-29 17:22:04 -0500

    Create cloudmock implementations for elbv2 API calls used by future NLB support

--- cloudmock/aws/mockelbv2/api.go
+++ cloudmock/aws/mockelbv2/api.go
@@ -19,10 +19,8 @@ package mockelbv2
 import (
 	"sync"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/elbv2"
 	"github.com/aws/aws-sdk-go/service/elbv2/elbv2iface"
-	"k8s.io/klog/v2"
 )
 
 type MockELBV2 struct {
@@ -31,7 +29,14 @@ type MockELBV2 struct {
 	mutex sync.Mutex
 
 	LoadBalancers map[string]*loadBalancer
+	lbCount       int
 	TargetGroups  map[string]*targetGroup
+	tgCount       int
+	Listeners     map[string]*listener
+	listenerCount int
+	LBAttributes  map[string][]*elbv2.LoadBalancerAttribute
+
+	Tags map[string]*elbv2.TagDescription
 }
 
 type loadBalancer struct {
@@ -42,99 +47,6 @@ type targetGroup struct {
 	description elbv2.TargetGroup
 }
 
-func (m *MockELBV2) DescribeLoadBalancers(request *elbv2.DescribeLoadBalancersInput) (*elbv2.DescribeLoadBalancersOutput, error) {
-	m.mutex.Lock()
-	defer m.mutex.Unlock()
-
-	klog.V(2).Infof("DescribeLoadBalancers v2 %v", request)
-
-	if request.PageSize != nil {
-		klog.Warningf("PageSize not implemented")
-	}
-	if request.Marker != nil {
-		klog.Fatalf("Marker not implemented")
-	}
-
-	var elbs []*elbv2.LoadBalancer
-	for _, elb := range m.LoadBalancers {
-		match := false
-
-		if len(request.LoadBalancerArns) > 0 {
-			for _, name := range request.LoadBalancerArns {
-				if aws.StringValue(elb.description.LoadBalancerArn) == aws.StringValue(name) {
-					match = true
-				}
-			}
-		} else {
-			match = true
-		}
-
-		if match {
-			elbs = append(elbs, &elb.description)
-		}
-	}
-
-	return &elbv2.DescribeLoadBalancersOutput{
-		LoadBalancers: elbs,
-	}, nil
-}
-
-func (m *MockELBV2) DescribeLoadBalancersPages(request *elbv2.DescribeLoadBalancersInput, callback func(p *elbv2.DescribeLoadBalancersOutput, lastPage bool) (shouldContinue bool)) error {
-	// For the mock, we just send everything in one page
-	page, err := m.DescribeLoadBalancers(request)
-	if err != nil {
-		return err
-	}
-
-	callback(page, false)
-
-	return nil
-}
-
-func (m *MockELBV2) DescribeTargetGroups(request *elbv2.DescribeTargetGroupsInput) (*elbv2.DescribeTargetGroupsOutput, error) {
-	m.mutex.Lock()
-	defer m.mutex.Unlock()
-
-	klog.V(2).Infof("DescribeTargetGroups %v", request)
-
-	if request.PageSize != nil {
-		klog.Warningf("PageSize not implemented")
-	}
-	if request.Marker != nil {
-		klog.Fatalf("Marker not implemented")
-	}
-
-	var tgs []*elbv2.TargetGroup
-	for _, tg := range m.TargetGroups {
-		match := false
-
-		if len(request.TargetGroupArns) > 0 {
-			for _, name := range request.TargetGroupArns {
-				if aws.StringValue(tg.description.TargetGroupArn) == aws.StringValue(name) {
-					match = true
-				}
-			}
-		} else {
-			match = true
-		}
-
-		if match {
-			tgs = append(tgs, &tg.description)
-		}
-	}
-
-	return &elbv2.DescribeTargetGroupsOutput{
-		TargetGroups: tgs,
-	}, nil
-}
-
-func (m *MockELBV2) DescribeTargetGroupsPages(request *elbv2.DescribeTargetGroupsInput, callback func(p *elbv2.DescribeTargetGroupsOutput, lastPage bool) (shouldContinue bool)) error {
-	page, err := m.DescribeTargetGroups(request)
-	if err != nil {
-		return err
-	}
-
-	callback(page, false)
-
-	return nil
+type listener struct {
+	description elbv2.Listener
 }
