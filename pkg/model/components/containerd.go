2778702cb690448cb8f6183daf9e03cb9e9c66a5
Ciprian Hacman <ciprian.hacman@sematext.com>
2020-12-02 09:53:57 +0200

    Update containerd to v1.4.3

--- pkg/model/components/containerd.go
+++ pkg/model/components/containerd.go
@@ -53,7 +53,7 @@ func (b *ContainerdOptionsBuilder) BuildOptions(o interface{}) error {
 		// Set containerd based on Kubernetes version
 		if fi.StringValue(containerd.Version) == "" {
 			if b.IsKubernetesGTE("1.19") {
-				containerd.Version = fi.String("1.4.1")
+				containerd.Version = fi.String("1.4.3")
 			} else if b.IsKubernetesGTE("1.18") {
 				containerd.Version = fi.String("1.3.4")
 			} else {
