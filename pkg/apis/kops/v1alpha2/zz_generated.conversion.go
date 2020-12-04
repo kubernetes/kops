b29d6c9490656ce43797ca6003de6c8269dc9030
Steven E. Harris <seh@panix.com>
2020-12-04 16:49:16 -0500

    Allow use of Calico's VXLAN networking backend
    Introduce a new "encapsulationMode" field in Calico's portion of the
    Cluster specification to allow switching between the the IP-in-IP and
    VXLAN encapsulation protocols. For now, we accept the values "ipip"
    and "vxlan," and forgo a possible "none" value that would disable
    encapsulation altogether (at least for the default Calico IP pool).
    
    Augment the default-populating procedure for Calico to take this field
    into account when deciding both which networking backend to use and
    whether to use IP-in-IP or VXLAN encapsulation for the default IP
    pool. Note that these values supplied for the "CALICO_IPV4POOL_IPIP"
    and "CALICO_IPV4POOL_VXLAN" environment variables in the "calico-node"
    DaemonSet pod spec only matter for creating the "default" IPPool pool
    object when no such objects already exist.
    
    Generalize the documentation for the "crossSubnet" field to cover
    environments more broad than just AWS, as Calico can employ this
    selective encapsulation in any environment in which it can detect
    boundaries between subnets.

--- pkg/apis/kops/v1alpha2/zz_generated.conversion.go
+++ pkg/apis/kops/v1alpha2/zz_generated.conversion.go
@@ -1345,6 +1345,7 @@ func autoConvert_v1alpha2_CalicoNetworkingSpec_To_kops_CalicoNetworkingSpec(in *
 	out.ChainInsertMode = in.ChainInsertMode
 	out.CPURequest = in.CPURequest
 	out.CrossSubnet = in.CrossSubnet
+	out.EncapsulationMode = in.EncapsulationMode
 	out.IPIPMode = in.IPIPMode
 	out.IPv4AutoDetectionMethod = in.IPv4AutoDetectionMethod
 	out.IPv6AutoDetectionMethod = in.IPv6AutoDetectionMethod
@@ -1379,6 +1380,7 @@ func autoConvert_kops_CalicoNetworkingSpec_To_v1alpha2_CalicoNetworkingSpec(in *
 	out.ChainInsertMode = in.ChainInsertMode
 	out.CPURequest = in.CPURequest
 	out.CrossSubnet = in.CrossSubnet
+	out.EncapsulationMode = in.EncapsulationMode
 	out.IPIPMode = in.IPIPMode
 	out.IPv4AutoDetectionMethod = in.IPv4AutoDetectionMethod
 	out.IPv6AutoDetectionMethod = in.IPv6AutoDetectionMethod
