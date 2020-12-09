c518089bb80156f8c7ecb1ab7ade4828294a84b7
Justin SB <justinsb@google.com>
2020-12-09 09:30:44 -0500

    Update etcd-manager to 20201209
    Highlights:
    
    * Fix arm64 images, which were built with an incorrect base image.
    * Initial (experimental) Azure support
    
    Full change list:
    
    * Update Kops dependency for Azure Blob Storage support [#372](https://github.com/kopeio/etcd-manager/pull/372)
    * Exclude gazelle from tools/deb-tools [#373](https://github.com/kopeio/etcd-manager/pull/373)
    * Regenerate bazel in tools/deb-tools [#374](https://github.com/kopeio/etcd-manager/pull/374)
    * Release notes for 3.0.20201202 [#375](https://github.com/kopeio/etcd-manager/pull/375)
    * Remove travis CI [#377](https://github.com/kopeio/etcd-manager/pull/377)
    * Fix vendor generation for tools/deb-tools subproject [#376](https://github.com/kopeio/etcd-manager/pull/376)
    * Add script to verify image hashes [#380](https://github.com/kopeio/etcd-manager/pull/380)
    * Fix some incorrect base image hashes for arm64 [#379](https://github.com/kopeio/etcd-manager/pull/379)
    * Support Azure [#378](https://github.com/kopeio/etcd-manager/pull/378)
    * Add more descriptions to wait loops [#383](https://github.com/kopeio/etcd-manager/pull/383)
    * Rename fields in the azure client struct [#382](https://github.com/kopeio/etcd-manager/pull/382)
    * Fix small typo in code comment [#381](https://github.com/kopeio/etcd-manager/pull/381)

--- pkg/model/components/etcdmanager/model.go
+++ pkg/model/components/etcdmanager/model.go
@@ -170,7 +170,7 @@ metadata:
   namespace: kube-system
 spec:
   containers:
-  - image: kopeio/etcd-manager:3.0.20201202
+  - image: kopeio/etcd-manager:3.0.20201209
     name: etcd-manager
     resources:
       requests:
