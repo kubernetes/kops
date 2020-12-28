e9a747224fea817436c3955af20ed85bce40bd9d
Ciprian Hacman <ciprian.hacman@sematext.com>
2020-12-28 17:11:33 +0200

    Add containerd option for registry mirrors
    (cherry picked from commit 66039f150e05f753fcee84290f99f7c2e824aea5)

--- pkg/apis/kops/v1alpha2/zz_generated.deepcopy.go
+++ pkg/apis/kops/v1alpha2/zz_generated.deepcopy.go
@@ -986,6 +986,21 @@ func (in *ContainerdConfig) DeepCopyInto(out *ContainerdConfig) {
 		*out = new(PackagesConfig)
 		(*in).DeepCopyInto(*out)
 	}
+	if in.RegistryMirrors != nil {
+		in, out := &in.RegistryMirrors, &out.RegistryMirrors
+		*out = make(map[string][]string, len(*in))
+		for key, val := range *in {
+			var outVal []string
+			if val == nil {
+				(*out)[key] = nil
+			} else {
+				in, out := &val, &outVal
+				*out = make([]string, len(*in))
+				copy(*out, *in)
+			}
+			(*out)[key] = outVal
+		}
+	}
 	if in.Root != nil {
 		in, out := &in.Root, &out.Root
 		*out = new(string)
