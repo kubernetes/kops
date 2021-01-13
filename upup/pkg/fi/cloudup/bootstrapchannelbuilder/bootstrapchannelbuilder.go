f5cb758d553ec6bb614d1bf15e530054b0c4500f
liranp <liran.polak@gmail.com>
2021-01-13 15:08:43 +0200

    feat(spot/addon): bump ocean-controller to 1.0.70

--- upup/pkg/fi/cloudup/bootstrapchannelbuilder/bootstrapchannelbuilder.go
+++ upup/pkg/fi/cloudup/bootstrapchannelbuilder/bootstrapchannelbuilder.go
@@ -645,7 +645,7 @@ func (b *BootstrapChannelBuilder) buildAddons(c *fi.ModelBuilderContext) (*chann
 		{
 			id := "v1.14.0"
 			location := key + "/" + id + ".yaml"
-			version := "1.0.69"
+			version := "1.0.70"
 
 			addons.Spec.Addons = append(addons.Spec.Addons, &channelsapi.AddonSpec{
 				Name:              fi.String(key),
