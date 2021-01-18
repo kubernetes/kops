ecc269d7726e4e92db07e476486e7d00eb94adb5
Peter Rifel <pgrifel@gmail.com>
2021-01-18 13:08:49 -0600

    Add --networking flag

--- tests/e2e/kubetest2-kops/deployer/up.go
+++ tests/e2e/kubetest2-kops/deployer/up.go
@@ -124,6 +124,10 @@ func (d *deployer) createCluster(zones []string, adminAccess string) error {
 		args = append(args, "--master-size", "s-8vcpu-16gb")
 	}
 
+	if d.Networking != "" {
+		args = append(args, "--networking", d.Networking)
+	}
+
 	klog.Info(strings.Join(args, " "))
 	cmd := exec.Command(args[0], args[1:]...)
 	cmd.SetEnv(d.env()...)
