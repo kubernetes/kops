3eacca47d993c4ffd8e2ae87b97fbed9a8691843
Timothy Clarke <Timothy.Clarke@ServerBox.co.uk>
2021-02-19 11:19:36 +0000

    Revert "Simplifying conditional per code review comments"
    This reverts commit 1a3c675212c4da751496e130c1a45db8ea553be7
    as it turned an optional feature subnets[0].allocationID into a required one

--- pkg/apis/kops/validation/aws.go
+++ pkg/apis/kops/validation/aws.go
@@ -264,8 +264,14 @@ func awsValidateLoadBalancerSubnets(fieldPath *field.Path, spec kops.ClusterSpec
 			}
 		}
 
-		if fi.StringValue(subnet.AllocationID) != "" && (lbSpec.Class != kops.LoadBalancerClassNetwork || lbSpec.Type == kops.LoadBalancerTypeInternal) {
-			allErrs = append(allErrs, field.Forbidden(fieldPath.Index(i).Child("allocationID"), "allocationID only allowed for Public NLBs"))
+		if subnet.AllocationID != nil {
+			if *subnet.AllocationID == "" {
+				allErrs = append(allErrs, field.Required(fieldPath.Index(i).Child("allocationID"), "allocationID can't be empty"))
+			}
+
+			if lbSpec.Class != kops.LoadBalancerClassNetwork || lbSpec.Type == kops.LoadBalancerTypeInternal {
+				allErrs = append(allErrs, field.Forbidden(fieldPath.Index(i).Child("allocationID"), "allocationID only allowed for Public NLBs"))
+			}
 		}
 	}
 
