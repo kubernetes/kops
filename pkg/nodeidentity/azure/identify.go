4f442a827ba0e46697fd2d546517a0dff1f090f8
Kenji Kaneda <kenji@cloudnatix.com>
2021-02-28 21:13:50 -0800

    Log an error message on an unexpected tag value

--- pkg/nodeidentity/azure/identify.go
+++ pkg/nodeidentity/azure/identify.go
@@ -113,6 +113,7 @@ func (i *nodeIdentifier) IdentifyNode(ctx context.Context, node *corev1.Node) (*
 		}
 		l := strings.SplitN(*v, "=", 2)
 		if len(l) <= 1 {
+			klog.Errorf("Expected 'key=value' for the tag value, but got %s", *v)
 			continue
 		}
 		labels[l[0]] = l[1]
