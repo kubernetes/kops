02e2acb0350b5b2604291c1705afb96ccd528148
Ole Markus With <o.with@sportradar.com>
2021-03-16 19:48:07 +0100

    Remove commented logic we'll probably never add back

--- dns-controller/pkg/watchers/node.go
+++ dns-controller/pkg/watchers/node.go
@@ -144,56 +144,6 @@ func (c *NodeController) runWatcher(stopCh <-chan struct{}) {
 func (c *NodeController) updateNodeRecords(node *v1.Node) string {
 	var records []dns.Record
 
-	//dnsLabel := node.Labels[LabelNameDns]
-	//if dnsLabel != "" {
-	//	var ips []string
-	//	for _, a := range node.Status.Addresses {
-	//		if a.Type != v1.NodeExternalIP {
-	//			continue
-	//		}
-	//		ips = append(ips, a.Address)
-	//	}
-	//	tokens := strings.Split(dnsLabel, ",")
-	//	for _, token := range tokens {
-	//		token = strings.TrimSpace(token)
-	//
-	//		// Assume a FQDN A record
-	//		fqdn := token
-	//		for _, ip := range ips {
-	//			records = append(records, dns.Record{
-	//				RecordType: dns.RecordTypeA,
-	//				FQDN: fqdn,
-	//				Value: ip,
-	//			})
-	//		}
-	//	}
-	//}
-	//
-	//dnsLabelInternal := node.Annotations[AnnotationNameDNSInternal]
-	//if dnsLabelInternal != "" {
-	//	var ips []string
-	//	for _, a := range node.Status.Addresses {
-	//		if a.Type != v1.NodeInternalIP {
-	//			continue
-	//		}
-	//		ips = append(ips, a.Address)
-	//	}
-	//	tokens := strings.Split(dnsLabelInternal, ",")
-	//	for _, token := range tokens {
-	//		token = strings.TrimSpace(token)
-	//
-	//		// Assume a FQDN A record
-	//		fqdn := dns.EnsureDotSuffix(token)
-	//		for _, ip := range ips {
-	//			records = append(records, dns.Record{
-	//				RecordType: dns.RecordTypeA,
-	//				FQDN: fqdn,
-	//				Value: ip,
-	//			})
-	//		}
-	//	}
-	//}
-
 	// Alias targets
 
 	// node/<name>/internal -> InternalIP
