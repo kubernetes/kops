02c3457d39a4ffb72f6ea36d10ec4baa3ca51023
Barry Melbourne <9964974+bmelbourne@users.noreply.github.com>
2021-03-20 16:09:09 +0000

    Update Docker to v20.10.5

--- pkg/model/components/docker.go
+++ pkg/model/components/docker.go
@@ -47,7 +47,9 @@ func (b *DockerOptionsBuilder) BuildOptions(o interface{}) error {
 
 	// Set the Docker version for known Kubernetes versions
 	if fi.StringValue(clusterSpec.Docker.Version) == "" {
-		if b.IsKubernetesGTE("1.17") {
+		if b.IsKubernetesGTE("1.21") {
+			docker.Version = fi.String("20.10.5")
+		} else if b.IsKubernetesGTE("1.17") {
 			docker.Version = fi.String("19.03.15")
 		} else if b.IsKubernetesGTE("1.16") {
 			docker.Version = fi.String("18.09.9")
