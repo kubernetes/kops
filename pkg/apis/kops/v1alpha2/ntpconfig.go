94a2b7953ab9269d2988392d70b7328dbada6e79
Kenji Kaneda <kenji@cloudnatix.com>
2021-03-30 00:19:23 -0700

    Add an option to skip NTP installation
    Add NTPConfig to ClusterSpec. NTPConfig has the SkipInstall option.
    
    https://github.com/kubernetes/kops/issues/9661

---
+++ pkg/apis/kops/v1alpha2/ntpconfig.go
@@ -0,0 +1,24 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha2
+
+// NTPConfig is the configuration for NTP.
+type NTPConfig struct {
+	// Managed controls if the NTP configuration is managed by kOps.
+	// The NTP configuration task is skipped if this is set to false.
+	Managed *bool `json:"managed,omitempty"`
+}
