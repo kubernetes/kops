893d07784bc08997d242f751228859a08bcf8cfc
Ole Markus With <o.with@sportradar.com>
2021-04-28 10:06:13 +0200

    Bump cilium to 1.9.6

--- pkg/model/components/cilium.go
+++ pkg/model/components/cilium.go
@@ -40,7 +40,7 @@ func (b *CiliumOptionsBuilder) BuildOptions(o interface{}) error {
 	}
 
 	if c.Version == "" {
-		c.Version = "v1.9.4"
+		c.Version = "v1.9.6"
 	}
 
 	version, _ := semver.ParseTolerant(c.Version)
