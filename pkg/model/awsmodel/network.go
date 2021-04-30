9c1ebe0f45801adab4f7a2a392bb6563bb9519ff
Ciprian Hacman <ciprian@hakman.dev>
2021-04-30 11:37:56 +0300

    Move network to awsmodel

--- pkg/model/awsmodel/network.go
+++ pkg/model/awsmodel/network.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package model
+package awsmodel
 
 import (
 	"fmt"
@@ -30,7 +30,7 @@ import (
 
 // NetworkModelBuilder configures network objects
 type NetworkModelBuilder struct {
-	*KopsModelContext
+	*AWSModelContext
 	Lifecycle *fi.Lifecycle
 }
 
