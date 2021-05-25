2773da9551a0b06fedd1084e3a8d23db7dcb7c5e
Ciprian Hacman <ciprian@hakman.dev>
2021-05-25 07:34:50 +0300

    Update Calico to v3.19.1

--- upup/pkg/fi/cloudup/bootstrapchannelbuilder/bootstrapchannelbuilder.go
+++ upup/pkg/fi/cloudup/bootstrapchannelbuilder/bootstrapchannelbuilder.go
@@ -747,8 +747,7 @@ func (b *BootstrapChannelBuilder) buildAddons(c *fi.ModelBuilderContext) (*chann
 	if b.Cluster.Spec.Networking.Calico != nil {
 		key := "networking.projectcalico.org"
 		versions := map[string]string{
-			"k8s-1.12": "3.9.6-kops.2",
-			"k8s-1.16": "3.19.0-kops.1",
+			"k8s-1.16": "3.19.1-kops.1",
 		}
 
 		{
