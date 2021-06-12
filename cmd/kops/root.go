363e1c0e5789d4b8c371b342cd5a573355743a46
John Gardiner Myers <jgmyers@proofpoint.com>
2021-06-11 22:32:14 -0700

    Allow unsetting fields from the command line

--- cmd/kops/root.go
+++ cmd/kops/root.go
@@ -150,6 +150,7 @@ func NewCmdRoot(f *util.Factory, out io.Writer) *cobra.Command {
 	cmd.AddCommand(NewCmdRollingUpdate(f, out))
 	cmd.AddCommand(NewCmdSet(f, out))
 	cmd.AddCommand(NewCmdToolbox(f, out))
+	cmd.AddCommand(NewCmdUnset(f, out))
 	cmd.AddCommand(NewCmdValidate(f, out))
 	cmd.AddCommand(NewCmdVersion(f, out))
 
