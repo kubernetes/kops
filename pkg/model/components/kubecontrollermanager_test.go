1a0b1e452bfb9a64f227c7ee2b07444530259b2f
John Gardiner Myers <jgmyers@proofpoint.com>
2021-06-14 20:31:44 -0700

    Default the NodeCIDRMaskSize appropriately for IPv6

--- pkg/model/components/kubecontrollermanager_test.go
+++ pkg/model/components/kubecontrollermanager_test.go
@@ -20,9 +20,12 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	api "k8s.io/kops/pkg/apis/kops"
 	"k8s.io/kops/pkg/assets"
+	"k8s.io/kops/upup/pkg/fi"
 )
 
 func buildCluster() *api.Cluster {
@@ -86,6 +89,79 @@ func Test_Build_KCM_Builder_Change_Duration(t *testing.T) {
 
 	if c.Spec.KubeControllerManager.AttachDetachReconcileSyncPeriod.Duration != time.Minute*5 {
 		t.Fatalf("AttachDetachReconcileSyncPeriod should be set to 5m - %s, for k8s version %s", c.Spec.KubeControllerManager.AttachDetachReconcileSyncPeriod.Duration.String(), c.Spec.KubernetesVersion)
+	}
+
+}
+
+func Test_Build_KCM_Builder_CIDR_Mask_Size(t *testing.T) {
+	grid := []struct {
+		PodCIDR          string
+		ClusterCIDR      string
+		ExpectedMaskSize *int32
+	}{
+		{
+			PodCIDR:          "100.64.1.0/11",
+			ExpectedMaskSize: nil,
+		},
+		{
+			PodCIDR:          "2001:DB8::/32",
+			ExpectedMaskSize: fi.Int32(48),
+		},
+		{
+			PodCIDR:          "2001:DB8::/65",
+			ExpectedMaskSize: fi.Int32(81),
+		},
+		{
+			PodCIDR:          "2001:DB8::/32",
+			ClusterCIDR:      "2001:DB8::/65",
+			ExpectedMaskSize: fi.Int32(81),
+		},
+		{
+			PodCIDR:          "2001:DB8::/95",
+			ExpectedMaskSize: fi.Int32(111),
+		},
+		{
+			PodCIDR:          "2001:DB8::/96",
+			ExpectedMaskSize: fi.Int32(112),
+		},
+		{
+			PodCIDR:          "2001:DB8::/97",
+			ExpectedMaskSize: fi.Int32(112),
+		},
+		{
+			PodCIDR:          "2001:DB8::/98",
+			ExpectedMaskSize: fi.Int32(113),
+		},
+		{
+			PodCIDR:          "2001:DB8::/99",
+			ExpectedMaskSize: fi.Int32(113),
+		},
+		{
+			PodCIDR:          "2001:DB8::/100",
+			ExpectedMaskSize: fi.Int32(114),
+		},
+	}
+	for _, tc := range grid {
+		t.Run(tc.PodCIDR+":"+tc.ClusterCIDR, func(t *testing.T) {
+			c := buildCluster()
+			b := assets.NewAssetBuilder(c, false)
+
+			kcm := &KubeControllerManagerOptionsBuilder{
+				OptionsContext: &OptionsContext{
+					AssetBuilder: b,
+				},
+			}
+
+			c.Spec.PodCIDR = tc.PodCIDR
+			c.Spec.KubeControllerManager = &api.KubeControllerManagerConfig{
+				ClusterCIDR: tc.ClusterCIDR,
+			}
+
+			err := kcm.BuildOptions(&c.Spec)
+			require.NoError(t, err)
+
+			assert.Equal(t, tc.ExpectedMaskSize, c.Spec.KubeControllerManager.NodeCIDRMaskSize)
+		})
 	}
 
 }
