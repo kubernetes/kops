e6ca69f55e91f87080c897216115efb811673718
Justin SB <justinsb@google.com>
2021-06-18 10:37:10 +0000

    Release 1.20.2
    Bump versions, update golden output.

--- upup/models/bindata.go
+++ upup/models/bindata.go
@@ -29305,7 +29305,7 @@ metadata:
   labels:
     k8s-addon: dns-controller.addons.k8s.io
     k8s-app: dns-controller
-    version: v1.20.1
+    version: v1.20.2
 spec:
   replicas: 1
   strategy:
@@ -29318,7 +29318,7 @@ spec:
       labels:
         k8s-addon: dns-controller.addons.k8s.io
         k8s-app: dns-controller
-        version: v1.20.1
+        version: v1.20.2
       annotations:
         scheduler.alpha.kubernetes.io/critical-pod: ''
     spec:
@@ -29332,7 +29332,7 @@ spec:
       serviceAccount: dns-controller
       containers:
       - name: dns-controller
-        image: k8s.gcr.io/kops/dns-controller:1.20.1
+        image: k8s.gcr.io/kops/dns-controller:1.20.2
         command:
 {{ range $arg := DnsControllerArgv }}
         - "{{ $arg }}"
@@ -29604,7 +29604,7 @@ metadata:
   labels:
     k8s-addon: kops-controller.addons.k8s.io
     k8s-app: kops-controller
-    version: v1.20.1
+    version: v1.20.2
 spec:
   selector:
     matchLabels:
@@ -29618,7 +29618,7 @@ spec:
       labels:
         k8s-addon: kops-controller.addons.k8s.io
         k8s-app: kops-controller
-        version: v1.20.1
+        version: v1.20.2
 {{ if UseKopsControllerForNodeBootstrap }}
       annotations:
         dns.alpha.kubernetes.io/internal: kops-controller.internal.{{ ClusterName }}
@@ -29636,7 +29636,7 @@ spec:
       serviceAccount: kops-controller
       containers:
       - name: kops-controller
-        image: k8s.gcr.io/kops/kops-controller:1.20.1
+        image: k8s.gcr.io/kops/kops-controller:1.20.2
         volumeMounts:
 {{ if .UseHostCertificates }}
         - mountPath: /etc/ssl/certs
