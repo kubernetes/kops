3c1cc72d51e43166b74c70034bf8061491e03310
Peter Rifel <pgrifel@gmail.com>
2021-07-01 08:15:52 -0400

    Include GCP Project in terraform HCL2 output
    This has been included in the JSON output but was missing from HCL2

--- tests/integration/update_cluster/ha_gce/kubernetes.tf
+++ tests/integration/update_cluster/ha_gce/kubernetes.tf
@@ -17,7 +17,8 @@ output "region" {
 }
 
 provider "google" {
-  region = "us-test1"
+  project = "us-test1"
+  region  = "us-test1"
 }
 
 resource "google_compute_disk" "d1-etcd-events-ha-gce-example-com" {
