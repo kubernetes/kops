a836afdc5ece2586823ca72efe029c25a0a9b217
John Gardiner Myers <jgmyers@proofpoint.com>
2021-07-16 22:55:50 -0700

    Refactor kube-apiserver static credentials

--- nodeup/pkg/model/secrets.go
+++ nodeup/pkg/model/secrets.go
@@ -17,11 +17,8 @@ limitations under the License.
 package model
 
 import (
-	"fmt"
 	"path/filepath"
-	"strings"
 
-	"k8s.io/kops/pkg/tokens"
 	"k8s.io/kops/upup/pkg/fi"
 	"k8s.io/kops/upup/pkg/fi/nodeup/nodetasks"
 	"k8s.io/kops/util/pkg/vfs"
@@ -41,10 +38,6 @@ const (
 
 // Build is responsible for pulling down the secrets
 func (b *SecretBuilder) Build(c *fi.ModelBuilderContext) error {
-	if b.KeyStore == nil {
-		return fmt.Errorf("KeyStore not set")
-	}
-
 	// @step: write out the platform ca
 	c.AddTask(&nodetasks.File{
 		Path:     filepath.Join(b.PathSrvKubernetes(), "ca.crt"),
@@ -68,75 +61,7 @@ func (b *SecretBuilder) Build(c *fi.ModelBuilderContext) error {
 		}
 	}
 
-	// If we do not run the Kubernetes API server we can stop here.
-	if !b.HasAPIServer {
-		return nil
-	}
-
-	// Support for basic auth was deprecated 1.16 and removed in 1.19
-	// https://github.com/kubernetes/kubernetes/pull/89069
-	if b.IsKubernetesLT("1.19") && b.SecretStore != nil {
-		key := "kube"
-		token, err := b.SecretStore.FindSecret(key)
-		if err != nil {
-			return err
-		}
-		if token == nil {
-			return fmt.Errorf("token not found: %q", key)
-		}
-		csv := string(token.Data) + "," + adminUser + "," + adminUser + "," + adminGroup
-
-		t := &nodetasks.File{
-			Path:     filepath.Join(b.PathSrvKubernetes(), "basic_auth.csv"),
-			Contents: fi.NewStringResource(csv),
-			Type:     nodetasks.FileType_File,
-			Mode:     s("0600"),
-		}
-		c.AddTask(t)
-	}
-
-	if b.SecretStore != nil {
-		allTokens, err := b.allAuthTokens()
-		if err != nil {
-			return err
-		}
-
-		var lines []string
-		for id, token := range allTokens {
-			if id == adminUser {
-				lines = append(lines, token+","+id+","+id+","+adminGroup)
-			} else {
-				lines = append(lines, token+","+id+","+id)
-			}
-		}
-		csv := strings.Join(lines, "\n")
-
-		c.AddTask(&nodetasks.File{
-			Path:     filepath.Join(b.PathSrvKubernetes(), "known_tokens.csv"),
-			Contents: fi.NewStringResource(csv),
-			Type:     nodetasks.FileType_File,
-			Mode:     s("0600"),
-		})
-	}
-
 	return nil
-}
-
-// allTokens returns a map of all auth tokens that are present
-func (b *SecretBuilder) allAuthTokens() (map[string]string, error) {
-	possibleTokens := tokens.GetKubernetesAuthTokens_Deprecated()
-
-	tokens := make(map[string]string)
-	for _, id := range possibleTokens {
-		token, err := b.SecretStore.FindSecret(id)
-		if err != nil {
-			return nil, err
-		}
-		if token != nil {
-			tokens[id] = string(token.Data)
-		}
-	}
-	return tokens, nil
 }
 
 func getInstanceAddress() (string, error) {
