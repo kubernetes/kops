ffabc3bf682bfc25ffdce99e54005345df94b467
Justin SB <justinsb@google.com>
2021-08-22 23:21:18 +0000

    Release 1.21.1

--- pkg/model/components/kubeapiserver/model.go
+++ pkg/model/components/kubeapiserver/model.go
@@ -95,7 +95,7 @@ kind: Pod
 spec:
   containers:
   - name: healthcheck
-    image: k8s.gcr.io/kops/kube-apiserver-healthcheck:1.21.0
+    image: k8s.gcr.io/kops/kube-apiserver-healthcheck:1.21.1
     livenessProbe:
       httpGet:
         # The sidecar serves a healthcheck on the same port,
