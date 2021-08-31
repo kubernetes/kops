f558f2441a57b556e37f30dab27bf453c981f750
dntosas <ntosas@gmail.com>
2021-08-31 14:07:19 +0300

    [addons/nodelocaldns] Bump image to latest stable v1.20.0
     As per
     https://github.com/kubernetes/dns/commit/3b17e06879a46b2ec5d97105c611a315897fdb48,
     node-local-dns addon is now builded with latest coreDNS base v1.8 and
     that brings great consistency between cache and upstream servers in a
     manner of configuration, metrics name convention, etc.
    
     So in this commit, we bump node-local-dns image to latest v1.20.0 which
     is build upon latest coreDNS and also add support for overriding this
     field.
    
    Signed-off-by: dntosas <ntosas@gmail.com>

--- pkg/apis/kops/v1alpha2/zz_generated.conversion.go
+++ pkg/apis/kops/v1alpha2/zz_generated.conversion.go
@@ -6026,6 +6026,7 @@ func Convert_kops_NodeAuthorizerSpec_To_v1alpha2_NodeAuthorizerSpec(in *kops.Nod
 
 func autoConvert_v1alpha2_NodeLocalDNSConfig_To_kops_NodeLocalDNSConfig(in *NodeLocalDNSConfig, out *kops.NodeLocalDNSConfig, s conversion.Scope) error {
 	out.Enabled = in.Enabled
+	out.Image = in.Image
 	out.LocalIP = in.LocalIP
 	out.ForwardToKubeDNS = in.ForwardToKubeDNS
 	out.MemoryRequest = in.MemoryRequest
@@ -6040,6 +6041,7 @@ func Convert_v1alpha2_NodeLocalDNSConfig_To_kops_NodeLocalDNSConfig(in *NodeLoca
 
 func autoConvert_kops_NodeLocalDNSConfig_To_v1alpha2_NodeLocalDNSConfig(in *kops.NodeLocalDNSConfig, out *NodeLocalDNSConfig, s conversion.Scope) error {
 	out.Enabled = in.Enabled
+	out.Image = in.Image
 	out.LocalIP = in.LocalIP
 	out.ForwardToKubeDNS = in.ForwardToKubeDNS
 	out.MemoryRequest = in.MemoryRequest
