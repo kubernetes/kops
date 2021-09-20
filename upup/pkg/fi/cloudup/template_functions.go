6bf823a539c92a90e2a5eb540a331b3dd2a63dde
justinsb <justinsb@google.com>
2021-09-20 08:59:25 -0400

    gossip: support resolution of k8s.local names from pods
    We add the hosts plugin to CoreDNS, and we populate a ConfigMap from
    kops-controller (when in gossip mode).
    
    This enables resolution of the internal apiserver DNS name from Pods,
    even when gossip mode (k8s.local) is in use.  This should fix the
    failing e2e tests which are assuming that the name in the JWT token is
    resolvable from inside the cluster.
    
    This is also a possible step towards a simpler gossip mode, now that
    we have a central controller.

--- upup/pkg/fi/cloudup/template_functions.go
+++ upup/pkg/fi/cloudup/template_functions.go
@@ -114,6 +114,16 @@ func (tf *TemplateFunctions) AddTo(dest template.FuncMap, secretStore fi.SecretS
 		return cluster.Spec.KubeDNS
 	}
 
+	dest["GossipDomains"] = func() []string {
+		var names []string
+
+		if dns.IsGossipHostname(cluster.Spec.MasterInternalName) {
+			names = append(names, "k8s.local")
+		}
+
+		return names
+	}
+
 	dest["NodeLocalDNSClusterIP"] = func() string {
 		if cluster.Spec.KubeProxy.ProxyMode == "ipvs" {
 			return cluster.Spec.KubeDNS.ServerIP
@@ -589,6 +599,12 @@ func (tf *TemplateFunctions) KopsControllerConfig() (string, error) {
 
 	if tf.Cluster.Spec.IsKopsControllerIPAM() {
 		config.EnableCloudIPAM = true
+	}
+
+	if dns.IsGossipHostname(cluster.Spec.MasterInternalName) {
+		config.Discovery = &kopscontrollerconfig.DiscoveryOptions{
+			Enabled: true,
+		}
 	}
 
 	// To avoid indentation problems, we marshal as json.  json is a subset of yaml
