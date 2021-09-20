8e42467128e1a8135a9959e5577063900c1ec01d
Ciprian Hacman <ciprian@hakman.dev>
2021-09-20 07:09:48 +0300

    Use "//go:build" lines together with "// +build" lines
    The go command now understands //go:build lines and prefers them over // +build lines. The new syntax uses boolean expressions, just like Go, and should be less error-prone. As of this release, the new syntax is fully supported, and all Go files should be updated to have both forms with the same meaning. To aid in migration, gofmt now automatically synchronizes the two forms. For more details on the syntax and migration plan, see https://golang.org/design/draft-gobuild.

--- hack/boilerplate/boilerplate.py
+++ hack/boilerplate/boilerplate.py
@@ -219,7 +219,7 @@ def get_regexs():
     regexs["date"] = re.compile(get_dates())
     # strip // +build \n\n build constraints
     regexs["go_build_constraints"] = re.compile(
-        r"^(// \+build.*\n)+\n", re.MULTILINE)
+        r"^(//go:build.*\n// \+build.*\n)+\n", re.MULTILINE)
     # strip #!.* from scripts
     regexs["shebang"] = re.compile(r"^(#!.*\n)\n*", re.MULTILINE)
     # Search for generated files
