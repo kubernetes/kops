8465290a4eadc0bc53897f7f6431c2f2087e5023
Anthony Hausman <anthony.hausman@backmarket.com>
2021-09-24 10:47:12 +0200

    FIX: Correct documentation

--- pkg/apis/kops/v1alpha2/networking.go
+++ pkg/apis/kops/v1alpha2/networking.go
@@ -303,7 +303,6 @@ type CiliumNetworkingSpec struct {
 	// With Cilium CNI chaining, the base network connectivity and IP address management is managed
 	// by the non-Cilium CNI plugin, but Cilium attaches eBPF programs to the network devices created
 	// by the non-Cilium plugin to provide L3/L4 network visibility, policy enforcement and other advanced features.
-	// Setting this has no effect.
 	// Default: none
 	ChainingMode string `json:"chainingMode,omitempty"`
 	// ContainerRuntime is not implemented and may be removed in the future.
