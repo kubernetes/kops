8999f9eb1eb4c7ef8651e8298c95d57439b23518
Ciprian Hacman <ciprian@hakman.dev>
2021-10-04 20:19:52 +0300

    Add hash for containerd v1.5.7

--- upup/pkg/fi/cloudup/containerd.go
+++ upup/pkg/fi/cloudup/containerd.go
@@ -197,6 +197,7 @@ func findAllContainerdHashesAmd64() map[string]string {
 		"1.5.4":  "591e4e087ea2f5007e6c64deb382df58d419b7b6922eab45a1923d843d57615f",
 		"1.5.5":  "45f02cfc65db47cf088c95555906e1dcba7baf5a3fbad3d947dd6b9af476a144",
 		"1.5.6":  "afc51718ebe46cb9b985edac816e63fe86c07e37d28cdd21b2c0302dec6fa7ae",
+		"1.5.7":  "7fce75bab43a39d6f9efb3c370de2da49723f0e1dbaa9732d68fa7f620d720c8",
 	}
 
 	return hashes
