11e68308d1d418bc99f39218063382229ee2efb7
Ole Markus With <o.with@sportradar.com>
2021-10-20 08:39:39 +0200

    Disable CNP status updates by default

--- pkg/apis/kops/v1alpha3/zz_generated.deepcopy.go
+++ pkg/apis/kops/v1alpha3/zz_generated.deepcopy.go
@@ -582,6 +582,11 @@ func (in *CiliumNetworkingSpec) DeepCopyInto(out *CiliumNetworkingSpec) {
 		*out = new(HubbleSpec)
 		(*in).DeepCopyInto(*out)
 	}
+	if in.DisableCNPStatusUpdates != nil {
+		in, out := &in.DisableCNPStatusUpdates, &out.DisableCNPStatusUpdates
+		*out = new(bool)
+		**out = **in
+	}
 	return
 }
 
