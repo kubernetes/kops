74483b50f0805b9230157c909ab2331ed1f40cfb
Peter Rifel <pgrifel@gmail.com>
2021-10-24 22:29:55 -0600

    address feedback

--- upup/pkg/fi/cloudup/awstasks/eventbridgetarget.go
+++ upup/pkg/fi/cloudup/awstasks/eventbridgetarget.go
@@ -74,7 +74,7 @@ func (eb *EventBridgeTarget) Find(c *fi.Context) (*EventBridgeTarget, error) {
 		return nil, nil
 	}
 	for _, target := range response.Targets {
-		if *target.Arn == *eb.SQSQueue.ARN {
+		if fi.StringValue(target.Arn) == fi.StringValue(eb.SQSQueue.ARN) {
 			actual := &EventBridgeTarget{
 				ID:        target.Id,
 				Name:      eb.Name,
