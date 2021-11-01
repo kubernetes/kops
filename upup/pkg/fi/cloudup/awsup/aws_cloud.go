aa9d363673fd3650f3461c718e4a362981e910a6
Peter Rifel <pgrifel@gmail.com>
2021-11-01 08:59:56 -0500

    Use new DescribeAutoScalingGroups tag filter support
    https://github.com/aws/aws-sdk-go/releases/tag/v1.41.3
    
    This eliminates the autoscaling.DescribeTags call entirely and may reduce the DescribeAutoScalingGroups call volume due to previously insufficient tag filtering support in DescribeTags.

--- upup/pkg/fi/cloudup/awsup/aws_cloud.go
+++ upup/pkg/fi/cloudup/awsup/aws_cloud.go
@@ -654,98 +654,38 @@ func getCloudGroups(c AWSCloud, cluster *kops.Cluster, instancegroups []*kops.In
 }
 
 // FindAutoscalingGroups finds autoscaling groups matching the specified tags
-// This isn't entirely trivial because autoscaling doesn't let us filter with as much precision as we would like
 func FindAutoscalingGroups(c AWSCloud, tags map[string]string) ([]*autoscaling.Group, error) {
 	var asgs []*autoscaling.Group
 
 	klog.V(2).Infof("Listing all Autoscaling groups matching cluster tags")
-	var asgNames []*string
-	{
-		var asFilters []*autoscaling.Filter
-		for _, v := range tags {
-			// Not an exact match, but likely the best we can do
-			asFilters = append(asFilters, &autoscaling.Filter{
-				Name:   aws.String("value"),
-				Values: []*string{aws.String(v)},
-			})
-		}
-		request := &autoscaling.DescribeTagsInput{
-			Filters: asFilters,
-		}
 
-		err := c.Autoscaling().DescribeTagsPages(request, func(p *autoscaling.DescribeTagsOutput, lastPage bool) bool {
-			for _, t := range p.Tags {
-				switch *t.ResourceType {
-				case "auto-scaling-group":
-					asgNames = append(asgNames, t.ResourceId)
-				default:
-					klog.Warningf("Unknown resource type: %v", *t.ResourceType)
-
-				}
-			}
-			return true
+	var asFilters []*autoscaling.Filter
+	for k, v := range tags {
+		asFilters = append(asFilters, &autoscaling.Filter{
+			Name:   aws.String(fmt.Sprintf("tag:%v", k)),
+			Values: []*string{aws.String(v)},
 		})
-		if err != nil {
-			return nil, fmt.Errorf("error listing autoscaling cluster tags: %v", err)
-		}
 	}
 
-	if len(asgNames) != 0 {
-		for i := 0; i < len(asgNames); i += 50 {
-			batch := asgNames[i:minInt(i+50, len(asgNames))]
-			request := &autoscaling.DescribeAutoScalingGroupsInput{
-				AutoScalingGroupNames: batch,
+	request := &autoscaling.DescribeAutoScalingGroupsInput{
+		Filters: asFilters,
+	}
+	err := c.Autoscaling().DescribeAutoScalingGroupsPages(request, func(p *autoscaling.DescribeAutoScalingGroupsOutput, lastPage bool) bool {
+		for _, asg := range p.AutoScalingGroups {
+			// Check for "Delete in progress" (the only use of .Status)
+			if asg.Status != nil {
+				klog.Warningf("Skipping ASG %v: %v", *asg.AutoScalingGroupARN, *asg.Status)
+				continue
 			}
-			err := c.Autoscaling().DescribeAutoScalingGroupsPages(request, func(p *autoscaling.DescribeAutoScalingGroupsOutput, lastPage bool) bool {
-				for _, asg := range p.AutoScalingGroups {
-					if !matchesAsgTags(tags, asg.Tags) {
-						// We used an inexact filter above
-						continue
-					}
-					// Check for "Delete in progress" (the only use of .Status)
-					if asg.Status != nil {
-						klog.Warningf("Skipping ASG %v (which matches tags): %v", *asg.AutoScalingGroupARN, *asg.Status)
-						continue
-					}
-					asgs = append(asgs, asg)
-				}
-				return true
-			})
-			if err != nil {
-				return nil, fmt.Errorf("error listing autoscaling groups: %v", err)
-			}
+			asgs = append(asgs, asg)
 		}
-
+		return true
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error listing autoscaling groups: %v", err)
 	}
 
 	return asgs, nil
-}
-
-// Returns the minimum of two ints
-func minInt(a int, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-// matchesAsgTags is used to filter an asg by tags
-func matchesAsgTags(tags map[string]string, actual []*autoscaling.TagDescription) bool {
-	for k, v := range tags {
-		found := false
-		for _, a := range actual {
-			if aws.StringValue(a.Key) == k {
-				if aws.StringValue(a.Value) == v {
-					found = true
-					break
-				}
-			}
-		}
-		if !found {
-			return false
-		}
-	}
-	return true
 }
 
 // findAutoscalingGroupLaunchConfiguration is responsible for finding the launch - which could be a launchconfiguration, a template or a mixed instance policy template
