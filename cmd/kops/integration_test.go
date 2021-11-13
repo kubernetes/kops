7f877b91e9b3e7d50f3657e4ed983deac98f93b9
John Gardiner Myers <jgmyers@proofpoint.com>
2021-11-12 18:28:09 -0800

    Upgrade external-dns to 0.10.1 for Kubernetes >= 1.19

--- cmd/kops/integration_test.go
+++ cmd/kops/integration_test.go
@@ -508,14 +508,14 @@ func TestCCM(t *testing.T) {
 
 func TestExternalDNS(t *testing.T) {
 	newIntegrationTest("minimal.example.com", "external_dns").
-		withAddons("external-dns.addons.k8s.io-k8s-1.12").
+		withAddons("external-dns.addons.k8s.io-k8s-1.19").
 		runTestTerraformAWS(t)
 }
 
 func TestExternalDNSIRSA(t *testing.T) {
 	newIntegrationTest("minimal.example.com", "external_dns_irsa").
 		withOIDCDiscovery().
-		withAddons("external-dns.addons.k8s.io-k8s-1.12").
+		withAddons("external-dns.addons.k8s.io-k8s-1.19").
 		withServiceAccountRole("external-dns.kube-system", true).
 		runTestTerraformAWS(t)
 }
