920c2da9a4143943a69bcf4c68c17b31493439fb
Bronson Mirafuentes <bronson.mirafuentes@doordash.com>
2021-12-03 12:53:22 -0800

    run hack/update-expected.sh

--- tests/integration/update_cluster/unmanaged/kubernetes.tf
+++ tests/integration/update_cluster/unmanaged/kubernetes.tf
@@ -307,7 +307,9 @@ resource "aws_ebs_volume" "us-test-1a-etcd-main-unmanaged-example-com" {
 }
 
 resource "aws_elb" "api-unmanaged-example-com" {
-  cross_zone_load_balancing = false
+  connection_draining         = true
+  connection_draining_timeout = 300
+  cross_zone_load_balancing   = false
   health_check {
     healthy_threshold   = 2
     interval            = 10
