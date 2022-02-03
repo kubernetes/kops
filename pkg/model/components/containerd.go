0766ca72f595358c2590775b9dbc194057637bfd
Ciprian Hacman <ciprian@hakman.dev>
2022-02-03 19:31:16 +0200

    Update containerd to v1.6.0-rc.2
    Signed-off-by: Ciprian Hacman <ciprian@hakman.dev>

--- pkg/model/components/containerd.go
+++ pkg/model/components/containerd.go
@@ -47,7 +47,7 @@ func (b *ContainerdOptionsBuilder) BuildOptions(o interface{}) error {
 		// Set version based on Kubernetes version
 		if fi.StringValue(containerd.Version) == "" {
 			if b.IsKubernetesGTE("1.23") {
-				containerd.Version = fi.String("1.6.0-rc.0")
+				containerd.Version = fi.String("1.6.0-rc.2")
 			} else if b.IsKubernetesGTE("1.19") {
 				containerd.Version = fi.String("1.4.12")
 			} else {
