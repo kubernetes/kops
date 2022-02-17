6d0e4973b50d607691e16d1b89950a16333fd2f0
Indeed <indeed@indeed.moe>
2022-02-17 07:06:33 -0800

    run test with pure kubetest2

--- tests/e2e/scenarios/upgrade-ha-leader-migration/run-test.sh
+++ tests/e2e/scenarios/upgrade-ha-leader-migration/run-test.sh
@@ -16,14 +16,32 @@
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 SCENARIO_ROOT="${REPO_ROOT}/tests/e2e/scenarios/upgrade-ha-leader-migration"
+KOPS_FEATURE_FLAGS=SpecOverrideFlag
+export KOPS_FEATURE_FLAGS
+(cd "$REPO_ROOT" && make test-e2e-install)
 
-source "${REPO_ROOT}"/tests/e2e/scenarios/lib/common.sh
+KUBETEST2="kubetest2 kops -v=2 --cloud-provider=${CLOUD_PROVIDER} --kops-root=${REPO_ROOT}"
+
+if [[ -n "${GCP_PROJECT-}" ]]; then
+	KUBETEST2="${KUBETEST2} --gcp-project=${GCP_PROJECT}"
+fi
+
+if [[ -n "${CLUSTER_NAME-}" ]]; then
+	KUBETEST2="${KUBETEST2} --cluster-name=${CLUSTER_NAME}"
+fi
+
+# for periodic job, let kubetest2 fetch latest kops. Otherwise let kubetest2 build.
+if [[ "${JOB_TYPE-}" == "periodic" ]]; then
+	KUBETEST2="${KUBETEST2} --kops-version-marker=https://storage.googleapis.com/kops-ci/bin/latest-ci-updown-green.txt"
+else
+	KUBETEST2="${KUBETEST2} --build"
+fi
 
 OVERRIDES=("--channel=alpha" "--node-count=1" "--master-count=3")
 
 case "${CLOUD_PROVIDER}" in
 gce)
-	export KOPS_FEATURE_FLAGS=AlphaAllowGCE,SpecOverrideFlag
+	export KOPS_FEATURE_FLAGS="${KOPS_FEATURE_FLAGS},AlphaAllowGCE"
 	OVERRIDES+=(
 		"--zones=us-central1-a,us-central1-b,us-central1-c"
 		"--master-zones=us-central1-a,us-central1-b,us-central1-c"
@@ -33,8 +51,6 @@ gce)
 *) ;;
 
 esac
-
-kops-acquire-latest
 
 # the migration in this test case is KCM to KCM+CCM, which should happen
 # during the upgrade from 1.23 to 1.24
@@ -52,7 +68,7 @@ fi
 ${KUBETEST2} \
 	--up \
 	--test=exec \
-	--kops-binary-path="${KOPS}" \
+	--down \
 	--kubernetes-version="${K8S_VERSION_A}" \
 	--create-args="${OVERRIDES[*]}" \
 	-- \
