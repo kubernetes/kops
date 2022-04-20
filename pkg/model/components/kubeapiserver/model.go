bd2ae0303021385fa38bd7c095c854f2912387ec
Ole Markus With <o.with@sportradar.com>
2022-04-20 09:11:41 +0200

    Release 1.22.5 (#13526)

--- pkg/model/components/kubeapiserver/model.go
+++ pkg/model/components/kubeapiserver/model.go
@@ -79,7 +79,7 @@ kind: Pod
 spec:
   containers:
   - name: healthcheck
-    image: k8s.gcr.io/kops/kube-apiserver-healthcheck:1.22.4
+    image: k8s.gcr.io/kops/kube-apiserver-healthcheck:1.22.5
     livenessProbe:
       httpGet:
         # The sidecar serves a healthcheck on the same port,
