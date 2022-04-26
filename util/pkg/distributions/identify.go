e35201ab8fcccf286a87b517ef93f741eb362681
Ciprian Hacman <ciprian@hakman.dev>
2022-04-26 08:22:50 +0300

    Add back support for Ubuntu 18.04

--- util/pkg/distributions/identify.go
+++ util/pkg/distributions/identify.go
@@ -54,6 +54,8 @@ func FindDistribution(rootfs string) (Distribution, error) {
 		return DistributionDebian10, nil
 	case "debian-11":
 		return DistributionDebian11, nil
+	case "ubuntu-18.04":
+		return DistributionUbuntu1804, nil
 	case "ubuntu-20.04":
 		return DistributionUbuntu2004, nil
 	case "ubuntu-20.10":
