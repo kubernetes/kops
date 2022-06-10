f35e32a404a479584666c6bd5a927844923c69d5
Ciprian Hacman <ciprian@hakman.dev>
2022-06-10 13:59:50 +0300

    Run hack/update-expected.sh

--- tests/integration/update_cluster/private-shared-subnet/kubernetes.tf
+++ tests/integration/update_cluster/private-shared-subnet/kubernetes.tf
@@ -764,14 +764,6 @@ resource "aws_s3_object" "private-shared-subnet-example-com-addons-bootstrap" {
   server_side_encryption = "AES256"
 }
 
-resource "aws_s3_object" "private-shared-subnet-example-com-addons-core-addons-k8s-io" {
-  bucket                 = "testingBucket"
-  content                = file("${path.module}/data/aws_s3_object_private-shared-subnet.example.com-addons-core.addons.k8s.io_content")
-  key                    = "clusters.example.com/private-shared-subnet.example.com/addons/core.addons.k8s.io/v1.4.0.yaml"
-  provider               = aws.files
-  server_side_encryption = "AES256"
-}
-
 resource "aws_s3_object" "private-shared-subnet-example-com-addons-coredns-addons-k8s-io-k8s-1-12" {
   bucket                 = "testingBucket"
   content                = file("${path.module}/data/aws_s3_object_private-shared-subnet.example.com-addons-coredns.addons.k8s.io-k8s-1.12_content")
