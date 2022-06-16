9c8cc8fe1e18e7d7cf11e4df0ac73662248cf4e9
Ole Markus With <o.with@sportradar.com>
2022-06-16 08:38:08 +0200

    Merge the cilium templates

--- tests/integration/update_cluster/privatecilium2/kubernetes.tf
+++ tests/integration/update_cluster/privatecilium2/kubernetes.tf
@@ -890,10 +890,10 @@ resource "aws_s3_object" "privatecilium-example-com-addons-limit-range-addons-k8
   server_side_encryption = "AES256"
 }
 
-resource "aws_s3_object" "privatecilium-example-com-addons-networking-cilium-io-k8s-1-23" {
+resource "aws_s3_object" "privatecilium-example-com-addons-networking-cilium-io-k8s-1-16" {
   bucket                 = "testingBucket"
-  content                = file("${path.module}/data/aws_s3_object_privatecilium.example.com-addons-networking.cilium.io-k8s-1.23_content")
-  key                    = "clusters.example.com/privatecilium.example.com/addons/networking.cilium.io/k8s-1.23-v1.11.yaml"
+  content                = file("${path.module}/data/aws_s3_object_privatecilium.example.com-addons-networking.cilium.io-k8s-1.16_content")
+  key                    = "clusters.example.com/privatecilium.example.com/addons/networking.cilium.io/k8s-1.16-v1.11.yaml"
   provider               = aws.files
   server_side_encryption = "AES256"
 }
