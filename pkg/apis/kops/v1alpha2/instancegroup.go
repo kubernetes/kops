a62c05d8a5d5033456bd354d9484879323ec9ea8
Ivan Volynkin <jonasasx@gmail.com>
2022-06-23 10:00:02 +0300

    Adding GuestAccelerators to InstanceTemplate

--- pkg/apis/kops/v1alpha2/instancegroup.go
+++ pkg/apis/kops/v1alpha2/instancegroup.go
@@ -160,6 +160,8 @@ type InstanceGroupSpec struct {
 	Containerd *ContainerdConfig `json:"containerd,omitempty"`
 	// Packages specifies additional packages to be installed.
 	Packages []string `json:"packages,omitempty"`
+	// GuestAccelerators configures additional accelerators
+	GuestAccelerators []AcceleratorConfig `json:"guestAccelerators,omitempty"`
 }
 
 // InstanceMetadataOptions defines the EC2 instance metadata service options (AWS Only)
@@ -267,4 +269,10 @@ type LoadBalancer struct {
 	LoadBalancerName *string `json:"loadBalancerName,omitempty"`
 	// TargetGroupARN to associate with this instance group (AWS ALB/NLB)
 	TargetGroupARN *string `json:"targetGroupArn,omitempty"`
+}
+
+// AcceleratorConfig defines an accelerator config
+type AcceleratorConfig struct {
+	AcceleratorCount int64  `json:"acceleratorCount,omitempty"`
+	AcceleratorType  string `json:"acceleratorType,omitempty"`
 }
