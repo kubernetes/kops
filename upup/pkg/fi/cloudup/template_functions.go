f6c08d4dd9c41b60d7e9eb09fbc079d068b49df1
Peter Rifel <pgrifel@gmail.com>
2022-07-11 19:10:59 -0600

    Use control-plane node role for AWS IAM Authenticator

--- upup/pkg/fi/cloudup/template_functions.go
+++ upup/pkg/fi/cloudup/template_functions.go
@@ -391,6 +391,8 @@ func (tf *TemplateFunctions) ControlPlaneControllerReplicas(deployOnWorkersIfExt
 func (tf *TemplateFunctions) APIServerNodeRole() string {
 	if featureflag.APIServerNodes.Enabled() {
 		return "node-role.kubernetes.io/api-server"
+	} else if tf.Cluster.IsKubernetesGTE("1.24") {
+		return "node-role.kubernetes.io/control-plane"
 	}
 	return "node-role.kubernetes.io/master"
 }
