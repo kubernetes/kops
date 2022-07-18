6b6599e2aea21a0748808896b1c9b7665e041877
Ciprian Hacman <ciprian@hakman.dev>
2022-07-18 06:34:40 +0300

    Update etcd-manager to v3.0.20220717

--- pkg/model/components/etcdmanager/model.go
+++ pkg/model/components/etcdmanager/model.go
@@ -183,7 +183,7 @@ metadata:
   namespace: kube-system
 spec:
   containers:
-  - image: k8s.gcr.io/etcdadm/etcd-manager:v3.0.20220417
+  - image: k8s.gcr.io/etcdadm/etcd-manager:v3.0.20220717
     name: etcd-manager
     resources:
       requests:
