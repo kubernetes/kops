d4ea445f245d71e29971e80a8a679aff180efc1b
Ole Markus With <o.with@sportradar.com>
2022-08-11 12:39:20 +0200

    Remove redundant buildKubeletConfig function

--- nodeup/pkg/model/kubelet.go
+++ nodeup/pkg/model/kubelet.go
@@ -69,7 +69,7 @@ func (b *KubeletBuilder) Build(c *fi.ModelBuilderContext) error {
 		return fmt.Errorf("error building kubelet server cert: %v", err)
 	}
 
-	kubeletConfig, err := b.buildKubeletConfig()
+	kubeletConfig, err := b.buildKubeletConfigSpec()
 	if err != nil {
 		return fmt.Errorf("error building kubelet config: %v", err)
 	}
@@ -407,17 +407,6 @@ func (b *KubeletBuilder) buildSystemdService() *nodetasks.Service {
 	}
 
 	return service
-}
-
-// buildKubeletConfig is responsible for creating the kubelet configuration
-func (b *KubeletBuilder) buildKubeletConfig() (*kops.KubeletConfigSpec, error) {
-	kubeletConfigSpec, err := b.buildKubeletConfigSpec()
-	if err != nil {
-		return nil, fmt.Errorf("error building kubelet config: %v", err)
-	}
-
-	// TODO: Memoize if we reuse this
-	return kubeletConfigSpec, nil
 }
 
 // usesContainerizedMounter returns true if we use the containerized mounter
