f4871583e410d68706f28ff14199226a6913fe4a
Ciprian Hacman <ciprian@hakman.dev>
2022-08-17 07:03:33 +0300

    Run make goimports

--- dnsprovider/pkg/dnsprovider/dns.go
+++ dnsprovider/pkg/dnsprovider/dns.go
@@ -99,14 +99,16 @@ type ResourceRecordSet interface {
 	Type() rrstype.RrsType
 }
 
-/* ResourceRecordSetsEquivalent compares two ResourceRecordSets for semantic equivalence.
-   Go's equality operator doesn't work the way we want it to in this case,
-   hence the need for this function.
-   More specifically (from the Go spec):
-   "Two struct values are equal if their corresponding non-blank fields are equal."
-   In our case, there may be some private internal member variables that may not be not equal,
-   but we want the two structs to be considered equivalent anyway, if the fields exposed
-   via their interfaces are equal.
+/*
+ResourceRecordSetsEquivalent compares two ResourceRecordSets for semantic equivalence.
+
+	Go's equality operator doesn't work the way we want it to in this case,
+	hence the need for this function.
+	More specifically (from the Go spec):
+	"Two struct values are equal if their corresponding non-blank fields are equal."
+	In our case, there may be some private internal member variables that may not be not equal,
+	but we want the two structs to be considered equivalent anyway, if the fields exposed
+	via their interfaces are equal.
 */
 func ResourceRecordSetsEquivalent(r1, r2 ResourceRecordSet) bool {
 	if r1.Name() == r2.Name() && reflect.DeepEqual(r1.Rrdatas(), r2.Rrdatas()) && r1.Ttl() == r2.Ttl() && r1.Type() == r2.Type() {
