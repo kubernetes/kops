f6db747c524fff9bc05cd3760a488b0890f2b9d5
Leïla MARABESE <lmarabese@scaleway.com>
2022-10-17 17:35:29 +0200

    cloud interface + utils

---
+++ upup/pkg/fi/cloudup/scaleway/utils.go
@@ -0,0 +1,27 @@
+package scaleway
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/scaleway/scaleway-sdk-go/scw"
+)
+
+// isHTTPCodeError returns true if err is an http error with code statusCode
+func isHTTPCodeError(err error, statusCode int) bool {
+	if err == nil {
+		return false
+	}
+
+	responseError := &scw.ResponseError{}
+	if errors.As(err, &responseError) && responseError.StatusCode == statusCode {
+		return true
+	}
+	return false
+}
+
+// is404Error returns true if err is an HTTP 404 error
+func is404Error(err error) bool {
+	notFoundError := &scw.ResourceNotFoundError{}
+	return isHTTPCodeError(err, http.StatusNotFound) || errors.As(err, &notFoundError)
+}
