0b28af6cf7963df1858940fa42e08581f31c5d1f
Leïla MARABESE <lmarabese@scaleway.com>
2022-10-19 09:55:58 +0200

    codegen + updated headers + refactoring after reviews

--- upup/pkg/fi/cloudup/scaleway/cloud.go
+++ upup/pkg/fi/cloudup/scaleway/cloud.go
@@ -25,6 +25,7 @@ import (
 	"github.com/scaleway/scaleway-sdk-go/api/instance/v1"
 	"github.com/scaleway/scaleway-sdk-go/scw"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/errors"
 	"k8s.io/klog/v2"
 	kopsv "k8s.io/kops"
 	"k8s.io/kops/dnsprovider/pkg/dnsprovider"
@@ -44,23 +45,23 @@ const (
 type ScwCloud interface {
 	fi.Cloud
 
+	ClusterName(tags []string) string
+	DNS() (dnsprovider.Interface, error)
+	ProviderID() kops.CloudProviderID
 	Region() string
 	Zone() string
-	ProviderID() kops.CloudProviderID
-	DNS() (dnsprovider.Interface, error)
-	ClusterName(tags []string) string
 
 	AccountService() *account.API
 	InstanceService() *instance.API
 
-	GetApiIngressStatus(cluster *kops.Cluster) ([]fi.ApiIngressStatus, error)
-	FindClusterStatus(cluster *kops.Cluster) (*kops.ClusterStatus, error)
-	GetCloudGroups(cluster *kops.Cluster, instancegroups []*kops.InstanceGroup, warnUnmatched bool, nodes []v1.Node) (map[string]*cloudinstances.CloudInstanceGroup, error)
 	DeleteGroup(group *cloudinstances.CloudInstanceGroup) error
-	FindVPCInfo(id string) (*fi.VPCInfo, error)
-	DetachInstance(instance *cloudinstances.CloudInstance) error
-	DeregisterInstance(instance *cloudinstances.CloudInstance) error
 	DeleteInstance(i *cloudinstances.CloudInstance) error
+	DeregisterInstance(instance *cloudinstances.CloudInstance) error
+	DetachInstance(instance *cloudinstances.CloudInstance) error
+	FindClusterStatus(cluster *kops.Cluster) (*kops.ClusterStatus, error)
+	FindVPCInfo(id string) (*fi.VPCInfo, error)
+	GetApiIngressStatus(cluster *kops.Cluster) ([]fi.ApiIngressStatus, error)
+	GetCloudGroups(cluster *kops.Cluster, instancegroups []*kops.InstanceGroup, warnUnmatched bool, nodes []v1.Node) (map[string]*cloudinstances.CloudInstanceGroup, error)
 
 	GetClusterServers(clusterName string, serverName *string) ([]*instance.Server, error)
 	GetClusterVolumes(clusterName string) ([]*instance.Volume, error)
@@ -86,31 +87,37 @@ type scwCloudImplementation struct {
 // NewScwCloud returns a Cloud with a Scaleway Client using the env vars SCW_ACCESS_KEY, SCW_SECRET_KEY,
 // SCW_DEFAULT_PROJECT_ID, SCW_DEFAULT_REGION and SCW_DEFAULT_ZONE
 func NewScwCloud(tags map[string]string) (ScwCloud, error) {
+	errList := []error(nil)
+
 	region, err := scw.ParseRegion(os.Getenv("SCW_DEFAULT_REGION"))
 	if err != nil {
-		return nil, fmt.Errorf("error parsing SCW_DEFAULT_REGION: %w", err)
+		errList = append(errList, fmt.Errorf("error parsing SCW_DEFAULT_REGION: %w", err))
 	}
 	zone, err := scw.ParseZone(os.Getenv("SCW_DEFAULT_ZONE"))
 	if err != nil {
-		return nil, fmt.Errorf("error parsing SCW_DEFAULT_ZONE: %w", err)
+		errList = append(errList, fmt.Errorf("error parsing SCW_DEFAULT_ZONE: %w", err))
 	}
 
 	// We make sure that the credentials env vars are defined
 	scwAccessKey := os.Getenv("SCW_ACCESS_KEY")
 	if scwAccessKey == "" {
-		return nil, fmt.Errorf("SCW_ACCESS_KEY has to be set as an environment variable")
+		errList = append(errList, fmt.Errorf("SCW_ACCESS_KEY has to be set as an environment variable"))
 	}
 	scwSecretKey := os.Getenv("SCW_SECRET_KEY")
 	if scwSecretKey == "" {
-		return nil, fmt.Errorf("SCW_SECRET_KEY has to be set as an environment variable")
+		errList = append(errList, fmt.Errorf("SCW_SECRET_KEY has to be set as an environment variable"))
 	}
 	scwProjectID := os.Getenv("SCW_DEFAULT_PROJECT_ID")
 	if scwProjectID == "" {
-		return nil, fmt.Errorf("SCW_DEFAULT_PROJECT_ID has to be set as an environment variable")
+		errList = append(errList, fmt.Errorf("SCW_DEFAULT_PROJECT_ID has to be set as an environment variable"))
+	}
+
+	if len(errList) != 0 {
+		return nil, errors.NewAggregate(errList)
 	}
 
 	scwClient, err := scw.NewClient(
-		scw.WithUserAgent("kubernetes-kops/"+kopsv.Version),
+		scw.WithUserAgent(KopsUserAgentPrefix+kopsv.Version),
 		scw.WithEnv(),
 	)
 	if err != nil {
@@ -127,14 +134,6 @@ func NewScwCloud(tags map[string]string) (ScwCloud, error) {
 	}, nil
 }
 
-func (s *scwCloudImplementation) Region() string {
-	return string(s.region)
-}
-
-func (s *scwCloudImplementation) Zone() string {
-	return string(s.zone)
-}
-
 func (s *scwCloudImplementation) ClusterName(tags []string) string {
 	for _, tag := range tags {
 		if strings.HasPrefix(tag, TagClusterName) {
@@ -144,13 +143,21 @@ func (s *scwCloudImplementation) ClusterName(tags []string) string {
 	return ""
 }
 
+func (s *scwCloudImplementation) DNS() (dnsprovider.Interface, error) {
+	klog.V(8).Infof("Scaleway DNS is not implemented yet")
+	return nil, fmt.Errorf("DNS is not implemented yet for Scaleway")
+}
+
 func (s *scwCloudImplementation) ProviderID() kops.CloudProviderID {
 	return kops.CloudProviderScaleway
 }
 
-func (s *scwCloudImplementation) DNS() (dnsprovider.Interface, error) {
-	//TODO(Mia-Cross) implement me
-	panic("Scaleway doesn't have a DNS yet")
+func (s *scwCloudImplementation) Region() string {
+	return string(s.region)
+}
+
+func (s *scwCloudImplementation) Zone() string {
+	return string(s.zone)
 }
 
 func (s *scwCloudImplementation) AccountService() *account.API {
@@ -161,10 +168,15 @@ func (s *scwCloudImplementation) InstanceService() *instance.API {
 	return s.instanceAPI
 }
 
-// FindVPCInfo is not implemented yet, it's only here to satisfy the fi.Cloud interface
-func (s *scwCloudImplementation) FindVPCInfo(id string) (*fi.VPCInfo, error) {
-	klog.V(8).Info("FindVPCInfo is not implemented yet for Scaleway")
-	return nil, fmt.Errorf("scaleway cloud provider does not support VPC at this time")
+func (s *scwCloudImplementation) DeleteGroup(group *cloudinstances.CloudInstanceGroup) error {
+	toDelete := append(group.NeedUpdate, group.Ready...)
+	for _, cloudInstance := range toDelete {
+		err := s.DeleteInstance(cloudInstance)
+		if err != nil {
+			return fmt.Errorf("error deleting group %q: %w", group.HumanName, err)
+		}
+	}
+	return nil
 }
 
 func (s *scwCloudImplementation) DeleteInstance(i *cloudinstances.CloudInstance) error {
@@ -189,24 +201,30 @@ func (s *scwCloudImplementation) DeleteInstance(i *cloudinstances.CloudInstance)
 }
 
 func (s *scwCloudImplementation) DeregisterInstance(i *cloudinstances.CloudInstance) error {
-	//TODO(Mia-Cross) implement me
-	panic("implement me")
-}
-
-func (s *scwCloudImplementation) DeleteGroup(group *cloudinstances.CloudInstanceGroup) error {
-	toDelete := append(group.NeedUpdate, group.Ready...)
-	for _, cloudInstance := range toDelete {
-		err := s.DeleteInstance(cloudInstance)
-		if err != nil {
-			return fmt.Errorf("error deleting group %q: %w", group.HumanName, err)
-		}
-	}
-	return nil
+	klog.V(8).Infof("Scaleway DeregisterInstance is not implemented yet")
+	return fmt.Errorf("DeregisterInstance is not implemented yet for Scaleway")
 }
 
 func (s *scwCloudImplementation) DetachInstance(i *cloudinstances.CloudInstance) error {
-	//TODO(Mia-Cross) implement me
-	panic("implement me")
+	klog.V(8).Infof("Scaleway DetachInstance is not implemented yet")
+	return fmt.Errorf("DetachInstance is not implemented yet for Scaleway")
+}
+
+// FindClusterStatus was used before etcd-manager to check the etcd cluster status and prevent unsupported changes.
+func (s *scwCloudImplementation) FindClusterStatus(cluster *kops.Cluster) (*kops.ClusterStatus, error) {
+	klog.V(8).Info("Scaleway FindClusterStatus is not implemented")
+	return nil, nil
+}
+
+// FindVPCInfo is not implemented yet, it's only here to satisfy the fi.Cloud interface
+func (s *scwCloudImplementation) FindVPCInfo(id string) (*fi.VPCInfo, error) {
+	klog.V(8).Info("Scaleway doesn't have a VPC yet so FindVPCInfo is not implemented")
+	return nil, fmt.Errorf("FindVPCInfo is not implemented yet for Scaleway")
+}
+
+func (s *scwCloudImplementation) GetApiIngressStatus(cluster *kops.Cluster) ([]fi.ApiIngressStatus, error) {
+	klog.V(8).Info("Scaleway doesn't have load-balancers yet so GetApiIngressStatus is not implemented")
+	return nil, fmt.Errorf("GetApiIngressStatus is not implemented yet for Scaleway")
 }
 
 func (s *scwCloudImplementation) GetCloudGroups(cluster *kops.Cluster, instancegroups []*kops.InstanceGroup, warnUnmatched bool, nodes []v1.Node) (map[string]*cloudinstances.CloudInstanceGroup, error) {
@@ -219,24 +237,18 @@ func (s *scwCloudImplementation) GetCloudGroups(cluster *kops.Cluster, instanceg
 		return nil, fmt.Errorf("failed to find server groups: %w", err)
 	}
 
-	for igName, serverGroup := range serverGroups {
-		var instanceGroup *kops.InstanceGroup
-		for _, ig := range instancegroups {
-			if igName == ig.Name {
-				instanceGroup = ig
-				break
-			}
-		}
-		if instanceGroup == nil {
+	for _, ig := range instancegroups {
+		serverGroup, ok := serverGroups[ig.Name]
+		if !ok {
 			if warnUnmatched {
-				klog.Warningf("Server group %q has no corresponding instance group", igName)
+				klog.Warningf("Server group %q has no corresponding instance group", ig.Name)
 			}
 			continue
 		}
 
-		groups[instanceGroup.Name], err = buildCloudGroup(instanceGroup, serverGroup, nodeMap)
+		groups[ig.Name], err = buildCloudGroup(ig, serverGroup, nodeMap)
 		if err != nil {
-			return nil, fmt.Errorf("failed to build cloud group for instance group %q: %w", instanceGroup.Name, err)
+			return nil, fmt.Errorf("failed to build cloud group for instance group %q: %w", ig.Name, err)
 		}
 	}
 
@@ -293,16 +305,6 @@ func buildCloudGroup(ig *kops.InstanceGroup, sg []*instance.Server, nodeMap map[
 	}
 
 	return cloudInstanceGroup, nil
-}
-
-func (s *scwCloudImplementation) FindClusterStatus(cluster *kops.Cluster) (*kops.ClusterStatus, error) {
-	klog.V(8).Info("FindClusterStatus is not implemented yet for Scaleway")
-	return nil, nil
-}
-
-func (s *scwCloudImplementation) GetApiIngressStatus(cluster *kops.Cluster) ([]fi.ApiIngressStatus, error) {
-	//TODO(Mia-Cross) implement me
-	panic("implement me")
 }
 
 func (s *scwCloudImplementation) GetClusterServers(clusterName string, serverName *string) ([]*instance.Server, error) {
@@ -372,14 +374,12 @@ func (s *scwCloudImplementation) DeleteServer(server *instance.Server) error {
 	if err != nil {
 		return fmt.Errorf("delete server %s: error deleting instance: %w", server.ID, err)
 	}
-	for {
-		_, err := s.instanceAPI.GetServer(&instance.GetServerRequest{
-			Zone:     s.zone,
-			ServerID: server.ID,
-		})
-		if is404Error(err) {
-			break
-		}
+	_, err = s.instanceAPI.WaitForServer(&instance.WaitForServerRequest{
+		ServerID: server.ID,
+		Zone:     s.zone,
+	})
+	if !is404Error(err) {
+		return fmt.Errorf("delete server %s: error waiting for instance after deletion: %w", server.ID, err)
 	}
 
 	// We delete the volumes that were attached to the server (including etcd volumes)
@@ -404,5 +404,14 @@ func (s *scwCloudImplementation) DeleteVolume(volume *instance.Volume) error {
 	if err != nil {
 		return fmt.Errorf("failed to delete volume %s: %w", volume.ID, err)
 	}
+
+	_, err = s.instanceAPI.WaitForVolume(&instance.WaitForVolumeRequest{
+		VolumeID: volume.ID,
+		Zone:     s.zone,
+	})
+	if !is404Error(err) {
+		return fmt.Errorf("delete server %s: error waiting for volume after deletion: %w", volume.ID, err)
+	}
+
 	return nil
 }
