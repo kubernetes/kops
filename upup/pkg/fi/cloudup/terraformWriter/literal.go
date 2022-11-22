9618bc2c140aa82e46b72962345033a82a874cd3
John Gardiner Myers <jgmyers@proofpoint.com>
2022-11-22 15:02:07 -0800

    Write Terraform vpc_cidr_block output for shared VPCs

--- upup/pkg/fi/cloudup/terraformWriter/literal.go
+++ upup/pkg/fi/cloudup/terraformWriter/literal.go
@@ -61,6 +61,15 @@ func LiteralSelfLink(resourceType, resourceName string) *Literal {
 	return LiteralProperty(resourceType, resourceName, "self_link")
 }
 
+func LiteralData(dataSourceType, dataSourceName, prop string) *Literal {
+	tfName := sanitizeName(dataSourceName)
+	expr := "${data." + dataSourceType + "." + tfName + "." + prop + "}"
+	return &Literal{
+		Value:  expr,
+		Tokens: []string{"data", dataSourceType, tfName, prop},
+	}
+}
+
 func LiteralProperty(resourceType, resourceName, prop string) *Literal {
 	tfName := sanitizeName(resourceName)
 	expr := "${" + resourceType + "." + tfName + "." + prop + "}"
