ca407eb6b379a89e748ff53984cb6263a7fd8756
Leïla MARABESE <lmarabese@scaleway.com>
2022-12-05 14:34:13 +0100

    rewording

--- pkg/model/master_volumes.go
+++ pkg/model/master_volumes.go
@@ -402,7 +402,6 @@ func (b *MasterVolumeBuilder) addScalewayVolume(c *fi.ModelBuilderContext, name 
 		fmt.Sprintf("%s=%s", scaleway.TagClusterName, b.Cluster.ObjectMeta.Name),
 		fmt.Sprintf("%s=%s", scaleway.TagNameEtcdClusterPrefix, etcd.Name),
 		fmt.Sprintf("%s=%s", scaleway.TagNameRolePrefix, scaleway.TagRoleMaster),
-		fmt.Sprintf("%s=%s", scaleway.TagRoleVolume, etcd.Name),
 		fmt.Sprintf("%s=%s", scaleway.TagInstanceGroup, fi.ValueOf(m.InstanceGroup)),
 	}
 
