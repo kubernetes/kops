37a0db42dd9fa06a8e95519913f2d528daff8303
Ciprian Hacman <ciprian@hakman.dev>
2022-12-09 07:52:27 +0200

    Update etcd-manager to v3.0.20221209

--- pkg/model/components/etcdmanager/model.go
+++ pkg/model/components/etcdmanager/model.go
@@ -180,8 +180,7 @@ metadata:
 spec:
   containers:
   - name: etcd-manager
-    #image: registry.k8s.io/etcdadm/etcd-manager:v3.0.20220831
-    image: gcr.io/k8s-staging-etcdadm/etcd-manager:v3.0.20220831-21-g12041b4c
+    image: registry.k8s.io/etcdadm/etcd-manager:v3.0.20221209
     resources:
       requests:
         cpu: 100m
