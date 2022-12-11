00631f0c2e1d8ca4fccef90f5118c8e5db1f8272
Ole Markus With <olemarkus@gmail.com>
2022-12-11 18:19:16 +0100

    Bump EBS CSI driver to 1.13.0

--- pkg/model/components/awsebscsidriver.go
+++ pkg/model/components/awsebscsidriver.go
@@ -48,7 +48,7 @@ func (b *AWSEBSCSIDriverOptionsBuilder) BuildOptions(o interface{}) error {
 	}
 
 	if c.Version == nil {
-		version := "v1.12.0"
+		version := "v1.13.0"
 		c.Version = fi.PtrTo(version)
 	}
 
