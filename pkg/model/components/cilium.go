a0d67fc475b97415fa41e8c7b13da3ca82f9448e
Ole Markus With <olemarkus@gmail.com>
2022-12-11 09:34:35 +0100

    Bump cilium to 1.13.5
    Bump to Cilium 1.14.0
    
    hack/update-expected.sh

--- pkg/model/components/cilium.go
+++ pkg/model/components/cilium.go
@@ -40,7 +40,7 @@ func (b *CiliumOptionsBuilder) BuildOptions(o interface{}) error {
 	}
 
 	if c.Version == "" {
-		c.Version = "v1.12.10"
+		c.Version = "v1.13.5"
 	}
 
 	if c.EnableEndpointHealthChecking == nil {
