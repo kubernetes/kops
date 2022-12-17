ecf1ea367d831086994221776c460de6d7fe833e
John Gardiner Myers <jgmyers@proofpoint.com>
2022-12-17 10:06:50 -0800

    hack/update-expected.sh

--- tests/integration/update_cluster/aws-lb-controller/kubernetes.tf
+++ tests/integration/update_cluster/aws-lb-controller/kubernetes.tf
@@ -595,7 +595,6 @@ resource "aws_s3_object" "cluster-completed-spec" {
 }
 
 resource "aws_s3_object" "discovery-json" {
-  acl                    = "public-read"
   bucket                 = "testingBucket"
   content                = file("${path.module}/data/aws_s3_object_discovery.json_content")
   key                    = "discovery.example.com/minimal.example.com/.well-known/openid-configuration"
@@ -620,7 +619,6 @@ resource "aws_s3_object" "etcd-cluster-spec-main" {
 }
 
 resource "aws_s3_object" "keys-json" {
-  acl                    = "public-read"
   bucket                 = "testingBucket"
   content                = file("${path.module}/data/aws_s3_object_keys.json_content")
   key                    = "discovery.example.com/minimal.example.com/openid/v1/jwks"
