707758f1e9820f24f0d30de676cab5c57ec7153b
justinsb <justinsb@google.com>
2023-01-01 10:56:45 -0500

    Support multiple terraform file providers
    By adding them on-demand, it is arguably a little simpler as well.
    
    Co-authored-by: John Gardiner Myers <jgmyers@proofpoint.com>

--- upup/pkg/fi/cloudup/apply_cluster.go
+++ upup/pkg/fi/cloudup/apply_cluster.go
@@ -727,15 +727,7 @@ func (c *ApplyClusterCmd) Run(ctx context.Context) error {
 
 	case TargetTerraform:
 		outDir := c.OutDir
-		var vfsProvider *vfs.TerraformProvider
-		if tfPath, ok := configBase.(vfs.TerraformPath); ok && featureflag.TerraformManagedFiles.Enabled() {
-			var err error
-			vfsProvider, err = tfPath.TerraformProvider()
-			if err != nil {
-				return err
-			}
-		}
-		tf := terraform.NewTerraformTarget(cloud, project, vfsProvider, outDir, cluster.Spec.Target)
+		tf := terraform.NewTerraformTarget(cloud, project, outDir, cluster.Spec.Target)
 
 		// We include a few "util" variables in the TF output
 		if err := tf.AddOutputVariable("region", terraformWriter.LiteralFromStringValue(cloud.Region())); err != nil {
