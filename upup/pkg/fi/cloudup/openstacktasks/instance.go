f739a1d2fd146a5659332462ea54980f8a66eeb8
ederst <stefan.prietl@bearingpoint.com>
2023-02-03 12:47:54 +0100

    Retry creating failed OpenStack servers
    This aims to improve the experience when creating openstack servers and
    they run into issues during scheduling, which is not covered by the
    create API request.
    
    So after creating the instance kops waits for it to become ACTIVE and if
    not, tries to reprovision the instance by deleting the failed instance
    and creating a new one.
    
    If the last attempt was still not successful, erroneous instances will
    persist to allow further investigation, and the task will fail, which
    will ultimately fail the update call.

--- upup/pkg/fi/cloudup/openstacktasks/instance.go
+++ upup/pkg/fi/cloudup/openstacktasks/instance.go
@@ -96,6 +96,7 @@ func (e *Instance) GetDependencies(tasks map[string]fi.CloudupTask) []fi.Cloudup
 
 var _ fi.CompareWithID = &Instance{}
 
+// TODO: delete this as this function is not used anywhere?
 func (e *Instance) WaitForStatusActive(t *openstack.OpenstackAPITarget) error {
 	return servers.WaitForStatus(t.Cloud.ComputeClient(), *e.ID, "ACTIVE", 120)
 }
