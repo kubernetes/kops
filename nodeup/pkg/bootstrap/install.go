bba6c9e8e0f6372bbbc0684ca772ab89af2329bc
Ciprian Hacman <ciprian@hakman.dev>
2023-02-12 12:29:06 +0200

    Revert "disable kops-configuration.service after successful execution"

--- nodeup/pkg/bootstrap/install.go
+++ nodeup/pkg/bootstrap/install.go
@@ -175,7 +175,6 @@ func (i *Installation) buildSystemdJob() *nodetasks.InstallService {
 	service := &nodetasks.InstallService{Service: nodetasks.Service{
 		Name:       serviceName,
 		Definition: fi.PtrTo(manifestString),
-		Enabled:    fi.PtrTo(false),
 	}}
 
 	service.InitDefaults()
