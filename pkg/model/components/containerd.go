4639bf477df2725fc7096b51a3c70a27649fdd25
Ciprian Hacman <ciprian@hakman.dev>
2023-02-16 04:40:24 +0200

    Update containerd to v1.6.18

--- pkg/model/components/containerd.go
+++ pkg/model/components/containerd.go
@@ -47,7 +47,7 @@ func (b *ContainerdOptionsBuilder) BuildOptions(o interface{}) error {
 		// Set version based on Kubernetes version
 		if fi.ValueOf(containerd.Version) == "" {
 			if b.IsKubernetesGTE("1.23") {
-				containerd.Version = fi.PtrTo("1.6.17")
+				containerd.Version = fi.PtrTo("1.6.18")
 				containerd.Runc = &kops.Runc{
 					Version: fi.PtrTo("1.1.4"),
 				}
