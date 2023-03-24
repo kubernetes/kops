0e79d1432eb21e6f37489ba60a13a84faf088984
ederst <stefan.prietl@bearingpoint.com>
2023-03-24 11:34:34 +0100

    Run make apimachinery and crds

--- pkg/apis/kops/zz_generated.deepcopy.go
+++ pkg/apis/kops/zz_generated.deepcopy.go
@@ -5083,6 +5083,11 @@ func (in *OpenstackNetwork) DeepCopyInto(out *OpenstackNetwork) {
 			}
 		}
 	}
+	if in.AddressSortOrder != nil {
+		in, out := &in.AddressSortOrder, &out.AddressSortOrder
+		*out = new(string)
+		**out = **in
+	}
 	return
 }
 
