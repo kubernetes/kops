9d9048413643c644f61ac37062ff507d54708e7e
Prateek Gogia <pgogia@amazon.com>
2023-06-24 23:22:45 -0500

    Include ssh pub key when creating cluster

--- tests/e2e/scenarios/scalability/run-test.sh
+++ tests/e2e/scenarios/scalability/run-test.sh
@@ -130,6 +130,10 @@ KUBECONFIG=$(mktemp -t kubeconfig.XXXXXXXXX)
 kops export kubecfg --admin --kubeconfig="${KUBECONFIG}"
 
 if [[ "${RUN_CL2_TEST:-}" == "true" ]]; then
+  # CL2 uses AWS_SSH_KEY path to ssh to instances for scraping metrics
+  export AWS_SSH_KEY=/root/.ssh/id_ecdsa
+  ssh-keygen -N '' -t ecdsa -b 521 -f "${AWS_SSH_KEY}" <<<y >/dev/null 2>&1
+  KUBETEST2_ARGS+=("--ssh-public-key=${SSH_KEY_PATH}.pub")
   kubetest2 kops "${KUBETEST2_ARGS[@]}" \
   --test=clusterloader2 \
   --kubernetes-version="${K8S_VERSION}" \
