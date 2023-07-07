739c3168e209a973a9b733c035868eb03f53aa34
Ciprian Hacman <ciprian@hakman.dev>
2023-07-07 20:38:51 +0300

    hack/update-expected.sh

--- tests/integration/update_cluster/minimal_gce_ilb/kubernetes.tf
+++ tests/integration/update_cluster/minimal_gce_ilb/kubernetes.tf
@@ -178,6 +178,13 @@ resource "aws_s3_object" "nodeupconfig-nodes" {
   server_side_encryption = "AES256"
 }
 
+resource "google_compute_address" "api-us-test1-minimal-gce-ilb-example-com" {
+  address_type = "INTERNAL"
+  name         = "api-us-test1-minimal-gce-ilb-example-com"
+  purpose      = "SHARED_LOADBALANCER_VIP"
+  subnetwork   = google_compute_subnetwork.us-test1-minimal-gce-ilb-example-com.name
+}
+
 resource "google_compute_backend_service" "api-minimal-gce-ilb-example-com" {
   backend {
     group = google_compute_instance_group_manager.a-master-us-test1-a-minimal-gce-ilb-example-com.instance_group
@@ -210,6 +217,17 @@ resource "google_compute_disk" "a-etcd-main-minimal-gce-ilb-example-com" {
   size = 20
   type = "pd-ssd"
   zone = "us-test1-a"
+}
+
+resource "google_compute_firewall" "lb-health-checks-minimal-gce-ilb-example-com" {
+  allow {
+    protocol = "tcp"
+  }
+  disabled      = false
+  name          = "lb-health-checks-minimal-gce-ilb-example-com"
+  network       = google_compute_network.minimal-gce-ilb-example-com.name
+  source_ranges = ["35.191.0.0/16", "130.211.0.0/22"]
+  target_tags   = ["minimal-gce-ilb-example-com-k8s-io-role-control-plane"]
 }
 
 resource "google_compute_firewall" "master-to-master-minimal-gce-ilb-example-com" {
@@ -390,14 +408,15 @@ resource "google_compute_firewall" "ssh-external-to-node-minimal-gce-ilb-example
   target_tags   = ["minimal-gce-ilb-example-com-k8s-io-role-node"]
 }
 
-resource "google_compute_forwarding_rule" "us-test-1-minimal-gce-ilb-example-com" {
+resource "google_compute_forwarding_rule" "api-us-test1-minimal-gce-ilb-example-com" {
   backend_service       = google_compute_backend_service.api-minimal-gce-ilb-example-com.id
+  ip_address            = google_compute_address.api-us-test1-minimal-gce-ilb-example-com.address
   ip_protocol           = "TCP"
   load_balancing_scheme = "INTERNAL"
-  name                  = "us-test-1-minimal-gce-ilb-example-com"
+  name                  = "api-us-test1-minimal-gce-ilb-example-com"
   network               = google_compute_network.minimal-gce-ilb-example-com.name
   ports                 = ["443"]
-  subnetwork            = "us-test-1"
+  subnetwork            = google_compute_subnetwork.us-test1-minimal-gce-ilb-example-com.name
 }
 
 resource "google_compute_health_check" "api-minimal-gce-ilb-example-com" {
