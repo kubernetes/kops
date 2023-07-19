1b481aa5b9247f66d5d58098377ba4d76f35edc4
Peter Rifel <pgrifel@gmail.com>
2023-07-19 07:15:50 -0500

    Continue skipping SCTP tests for cilium until we upgrade to 1.13

--- tests/e2e/pkg/tester/skip_regex.go
+++ tests/e2e/pkg/tester/skip_regex.go
@@ -67,8 +67,9 @@ func (t *Tester) setSkipRegexFlag() error {
 		skipRegex += "|same.hostPort.but.different.hostIP.and.protocol"
 		// https://github.com/cilium/cilium/issues/9207
 		skipRegex += "|serve.endpoints.on.same.port.and.different.protocols"
-		// This may be fixed in Cilium 1.13 but skipping for now
+		// These may be fixed in Cilium 1.13 but skipping for now
 		skipRegex += "|Service.with.multiple.ports.specified.in.multiple.EndpointSlices"
+		skipRegex += "|should.create.a.Pod.with.SCTP.HostPort"
 		// https://github.com/cilium/cilium/issues/18241
 		skipRegex += "|Services.should.create.endpoints.for.unready.pods"
 		skipRegex += "|Services.should.be.able.to.connect.to.terminating.and.unready.endpoints.if.PublishNotReadyAddresses.is.true"
