c68ccf91227731ac3268e2c89ed3f850886c1a83
Prateek Gogia <pgogia@amazon.com>
2023-08-26 10:40:46 -0500

    increase cidr count during cluster creation

--- tests/e2e/scenarios/scalability/run-test.sh
+++ tests/e2e/scenarios/scalability/run-test.sh
@@ -87,6 +87,10 @@ echo "ADMIN_ACCESS=${ADMIN_ACCESS}"
 #create_args="--networking cilium"
 create_args=()
 create_args=("--network-cidr=10.0.0.0/16")
+create_args=("--network-cidr=10.1.0.0/16")
+create_args=("--network-cidr=10.2.0.0/16")
+create_args=("--network-cidr=10.3.0.0/16")
+create_args=("--network-cidr=10.4.0.0/16")
 create_args+=("--networking=${CNI_PLUGIN:-calico}")
 if [[ "${CNI_PLUGIN}" == "amazonvpc" ]]; then
 create_args+=("--set spec.networking.amazonVPC.env=ENABLE_PREFIX_DELEGATION=true")
