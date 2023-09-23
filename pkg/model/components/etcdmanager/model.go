c07f51e06002260dba58ac81c9416c54ea398717
John Gardiner Myers <jgmyers@proofpoint.com>
2023-09-23 00:06:58 -0700

    Release 1.28.0 (#15954)

--- pkg/model/components/etcdmanager/model.go
+++ pkg/model/components/etcdmanager/model.go
@@ -208,7 +208,7 @@ spec:
     emptyDir: {}
 `
 
-const kopsUtilsImage = "registry.k8s.io/kops/kops-utils-cp:1.28.0-beta.2"
+const kopsUtilsImage = "registry.k8s.io/kops/kops-utils-cp:1.28.0"
 
 // buildPod creates the pod spec, based on the EtcdClusterSpec
 func (b *EtcdManagerBuilder) buildPod(etcdCluster kops.EtcdClusterSpec, instanceGroupName string) (*v1.Pod, error) {
