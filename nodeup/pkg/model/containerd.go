275c948cb692f8d5919741be3300338c48439090
upodroid <upodroid@users.noreply.github.com>
2023-12-03 21:57:46 +0000

    stop specifying LimitNoFILE

--- nodeup/pkg/model/containerd.go
+++ nodeup/pkg/model/containerd.go
@@ -203,7 +203,6 @@ func (b *ContainerdBuilder) buildSystemdService(sv semver.Version) *nodetasks.Se
 
 	manifest.Set("Service", "LimitNPROC", "infinity")
 	manifest.Set("Service", "LimitCORE", "infinity")
-	manifest.Set("Service", "LimitNOFILE", "infinity")
 	manifest.Set("Service", "TasksMax", "infinity")
 
 	// make killing of processes of this unit under memory pressure very unlikely
