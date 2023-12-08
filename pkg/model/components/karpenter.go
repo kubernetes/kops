5ce66a9d284769e0e4e9e08e00a65c4f723c44aa
Peter Rifel <pgrifel@gmail.com>
2023-12-07 18:56:47 -0600

    Upgrade Karpenter to v0.31.3

--- pkg/model/components/karpenter.go
+++ pkg/model/components/karpenter.go
@@ -36,7 +36,7 @@ func (b *KarpenterOptionsBuilder) BuildOptions(o interface{}) error {
 	}
 
 	if c.Image == "" {
-		c.Image = "public.ecr.aws/karpenter/controller:v0.30.0"
+		c.Image = "public.ecr.aws/karpenter/controller:v0.31.3"
 	}
 
 	if c.LogEncoding == "" {
