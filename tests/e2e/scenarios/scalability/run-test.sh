735092730968c258f3e61b9ec33b13787c249524
hakuna-matatah <hakuna@amazon.com>
2023-12-11 11:24:55 -0800

    Add warm ip and min ip for better prefix management

--- tests/e2e/scenarios/scalability/run-test.sh
+++ tests/e2e/scenarios/scalability/run-test.sh
@@ -90,6 +90,8 @@ create_args=("--network-cidr=10.0.0.0/16,10.1.0.0/16,10.2.0.0/16,10.3.0.0/16,10.
 create_args+=("--networking=${CNI_PLUGIN:-calico}")
 if [[ "${CNI_PLUGIN}" == "amazonvpc" ]]; then
 create_args+=("--set spec.networking.amazonVPC.env=ENABLE_PREFIX_DELEGATION=true")
+create_args+=("--set spec.networking.amazonVPC.env=WARM_IP_TARGET=2")
+create_args+=("--set spec.networking.amazonVPC.env=MIN_IP_TARGET=10")
 fi
 create_args+=("--image=${INSTANCE_IMAGE:-ssm:/aws/service/canonical/ubuntu/server/20.04/stable/current/amd64/hvm/ebs-gp2/ami-id}")
 create_args+=("--set spec.etcdClusters[0].manager.listenMetricsURLs=http://localhost:2382")
