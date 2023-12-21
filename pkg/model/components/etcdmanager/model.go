0300a3b858bf389f75d408b5ab9b12744139814c
Justin Santa Barbara <justinsb@google.com>
2023-12-21 08:24:27 -0500

    Release 1.29.0-alpha.3 (#16183)

--- pkg/model/components/etcdmanager/model.go
+++ pkg/model/components/etcdmanager/model.go
@@ -208,7 +208,7 @@ spec:
     emptyDir: {}
 `
 
-const kopsUtilsImage = "registry.k8s.io/kops/kops-utils-cp:1.29.0-alpha.2"
+const kopsUtilsImage = "registry.k8s.io/kops/kops-utils-cp:1.29.0-alpha.3"
 
 // buildPod creates the pod spec, based on the EtcdClusterSpec
 func (b *EtcdManagerBuilder) buildPod(etcdCluster kops.EtcdClusterSpec, instanceGroupName string) (*v1.Pod, error) {
