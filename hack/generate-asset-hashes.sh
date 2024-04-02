4cf7fe8647b7ffa8b69d3e5d9bcb909e45fc067c
Ciprian Hacman <ciprian@hakman.dev>
2024-04-02 06:52:11 +0300

    Update hack/generate-asset-hashes.sh

--- hack/generate-asset-hashes.sh
+++ hack/generate-asset-hashes.sh
@@ -14,6 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 cd "${REPO_ROOT}"
 
