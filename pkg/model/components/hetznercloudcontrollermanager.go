f6cf7385314bdc1f7b31559fe4079aa84fbc3868
Ciprian Hacman <ciprian@hakman.dev>
2024-05-05 11:07:35 +0300

    hetzner: Update CCM to v1.19.0

--- pkg/model/components/hetznercloudcontrollermanager.go
+++ pkg/model/components/hetznercloudcontrollermanager.go
@@ -55,7 +55,7 @@ func (b *HetznerCloudControllerManagerOptionsBuilder) BuildOptions(o interface{}
 	eccm.ConfigureCloudRoutes = fi.PtrTo(false)
 
 	if eccm.Image == "" {
-		eccm.Image = "hetznercloud/hcloud-cloud-controller-manager:v1.16.0"
+		eccm.Image = "hetznercloud/hcloud-cloud-controller-manager:v1.19.0"
 	}
 
 	return nil
