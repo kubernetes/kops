ba5519a54640e6b4b46fe2dc6a697db5179a0ce5
justinsb <justinsb@google.com>
2024-08-29 10:04:16 -0400

    tests: add simple s3 emulator for bare-metal testing
    We don't need a lot of S3 functionality, it's likely easier just to create our own.

---
+++ tools/metal/storage/README.md
@@ -0,0 +1,3 @@
+A very simple s3 mock to support our testing of bare metal / VMs.
+
+Just enough functionality to support our tests.
---
+++ tools/metal/storage/pkg/objectstore/interfaces.go
@@ -0,0 +1,59 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package objectstore
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+type ObjectStore interface {
+	ListBuckets(ctx context.Context) []BucketInfo
+
+	// GetBucket returns the bucket with the given name.
+	// If the bucket does not exist, it returns (nil, nil).
+	GetBucket(ctx context.Context, name string) (Bucket, error)
+
+	// CreateBucket creates the bucket with the given name.
+	// If the bucket already exist, it returns codes.AlreadyExists.
+	CreateBucket(ctx context.Context, name string) (*BucketInfo, error)
+}
+
+type BucketInfo struct {
+	Name         string
+	CreationDate time.Time
+}
+
+type Bucket interface {
+	// GetObject returns the object with the given key.
+	// If the object does not exist, it returns (nil, nil).
+	GetObject(ctx context.Context, key string) (Object, error)
+
+	// ListObjects returns the list of objects in the bucket.
+	ListObjects(ctx context.Context) ([]ObjectInfo, error)
+}
+
+type ObjectInfo struct {
+	Key          string
+	LastModified time.Time
+	Size         int64
+}
+
+type Object interface {
+	WriteTo(w http.ResponseWriter) error
+}
