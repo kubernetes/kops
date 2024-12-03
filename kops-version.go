8577fcac7fb85ce5cdf103bfa3a26bbb009fd8a5
Peter Rifel <rifelpet@users.noreply.github.com>
2024-12-03 06:13:01 -0600

    Release 1.30.3 (#16972)

--- kops-version.go
+++ kops-version.go
@@ -21,8 +21,8 @@ var Version = KOPS_RELEASE_VERSION
 
 // These constants are parsed by build tooling - be careful about changing the formats
 const (
-	KOPS_RELEASE_VERSION = "1.30.2"
-	KOPS_CI_VERSION      = "1.30.3"
+	KOPS_RELEASE_VERSION = "1.30.3"
+	KOPS_CI_VERSION      = "1.30.4"
 )
 
 // GitVersion should be replaced by the makefile
