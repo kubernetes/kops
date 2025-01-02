8be6008ac4d4809ede556dfe0c757b9a66387395
Ciprian Hacman <ciprian@hakman.dev>
2025-01-02 11:13:55 +0200

    Update etcd to v3.5.17

--- pkg/model/components/etcd.go
+++ pkg/model/components/etcd.go
@@ -29,7 +29,7 @@ type EtcdOptionsBuilder struct {
 var _ loader.OptionsBuilder = &EtcdOptionsBuilder{}
 
 const (
-	DefaultEtcd3Version_1_22 = "3.5.13"
+	DefaultEtcd3Version_1_22 = "3.5.17"
 )
 
 // BuildOptions is responsible for filling in the defaults for the etcd cluster model
