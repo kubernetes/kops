d112c6ec87cb0ae7ddf873ad1a5ed58cea806e6f
Ciprian Hacman <ciprian@hakman.dev>
2025-01-10 22:10:28 +0200

    Update containerd to v1.7.25

--- pkg/model/components/containerd.go
+++ pkg/model/components/containerd.go
@@ -48,7 +48,7 @@ func (b *ContainerdOptionsBuilder) BuildOptions(o *kops.Cluster) error {
 				Version: fi.PtrTo("1.1.5"),
 			}
 		default:
-			containerd.Version = fi.PtrTo("1.7.24")
+			containerd.Version = fi.PtrTo("1.7.25")
 			containerd.Runc = &kops.Runc{
 				Version: fi.PtrTo("1.2.4"),
 			}
