6311cab2facc317947959cb18d9ba7b6ab85bec6
justinsb <justinsb@google.com>
2025-01-14 08:03:55 -0500

    delete cluster: avoid logspam on GCE "resource in use" error
    Similar to what we do on AWS, we avoid printing confusing error
    messages when there is a dependency we aren't aware of.

--- pkg/resources/ops/delete.go
+++ pkg/resources/ops/delete.go
@@ -24,6 +24,7 @@ import (
 	"k8s.io/klog/v2"
 	"k8s.io/kops/pkg/resources"
 	awsresources "k8s.io/kops/pkg/resources/aws"
+	gceresources "k8s.io/kops/pkg/resources/gce"
 	"k8s.io/kops/upup/pkg/fi"
 )
 
@@ -128,7 +129,7 @@ func DeleteResources(cloud fi.Cloud, resourceMap map[string]*resources.Resource,
 					}
 					if err != nil {
 						mutex.Lock()
-						if awsresources.IsDependencyViolation(err) {
+						if awsresources.IsDependencyViolation(err) || gceresources.IsDependencyViolation(err) {
 							fmt.Printf("%s\tstill has dependencies, will retry\n", human)
 							klog.V(4).Infof("resource %q generated a dependency error: %v", human, err)
 						} else {
