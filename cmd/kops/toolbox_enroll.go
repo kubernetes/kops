a84f931c1318ddb8b58cf01f1f7692a5792b27e2
justinsb <justinsb@google.com>
2025-02-19 06:51:04 -0500

    metal: split host creation from enrollment
    This is needeed for bootstrapping the control plane,
    because it's a CRD so can't be registered until the control plane is running.
    
    It's also quite nice because we might want to review the contents of the
    host CRD, e.g. to verify the key out-of-band.

--- cmd/kops/toolbox_enroll.go
+++ cmd/kops/toolbox_enroll.go
@@ -51,6 +51,8 @@ func NewCmdToolboxEnroll(f commandutils.Factory, out io.Writer) *cobra.Command {
 	cmd.Flags().StringVar(&options.SSHUser, "ssh-user", options.SSHUser, "user for ssh")
 	cmd.Flags().IntVar(&options.SSHPort, "ssh-port", options.SSHPort, "port for ssh")
 
+	cmd.Flags().BoolVar(&options.BuildHost, "build-host", options.BuildHost, "only build the host resource, don't apply it or enroll the node")
+
 	options.CreateKubecfgOptions.AddCommonFlags(cmd.Flags())
 
 	return cmd
