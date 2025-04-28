8478ba3e5c62d2b594f5647ae3810cfef6766d1a
justinsb <justinsb@google.com>
2025-04-28 07:24:13 -0400

    autogen: update golden output for gce role labels

--- tests/integration/update_cluster/minimal_gce_private/kubernetes.tf
+++ tests/integration/update_cluster/minimal_gce_private/kubernetes.tf
@@ -436,8 +436,8 @@ resource "google_compute_instance_template" "master-us-test1-a-minimal-gce-priva
   labels = {
     "k8s-io-cluster-name"       = "minimal-gce-private-example-com"
     "k8s-io-instance-group"     = "master-us-test1-a"
-    "k8s-io-role-control-plane" = ""
-    "k8s-io-role-master"        = ""
+    "k8s-io-role-control-plane" = "control-plane"
+    "k8s-io-role-master"        = "master"
   }
   lifecycle {
     create_before_destroy = true
@@ -486,7 +486,7 @@ resource "google_compute_instance_template" "nodes-minimal-gce-private-example-c
   labels = {
     "k8s-io-cluster-name"   = "minimal-gce-private-example-com"
     "k8s-io-instance-group" = "nodes"
-    "k8s-io-role-node"      = ""
+    "k8s-io-role-node"      = "node"
   }
   lifecycle {
     create_before_destroy = true
