f2dfd54ce8d9703e5baca2a7ef8cfb7608b3dfca
Ciprian Hacman <ciprian@hakman.dev>
2025-07-28 23:35:31 +0300

    Revert "Fix invalid filters for describing security group rules"
    This reverts commit 0e172e9813b37ad517e1227fc3f1a23946c9b901.

--- upup/pkg/fi/cloudup/awstasks/securitygroup.go
+++ upup/pkg/fi/cloudup/awstasks/securitygroup.go
@@ -335,9 +335,14 @@ func (e *SecurityGroup) FindDeletions(c *fi.CloudupContext) ([]fi.CloudupDeletio
 	cloud := awsup.GetCloud(c)
 
 	filters := make([]ec2types.Filter, 0)
-	if e.ID != nil {
+	switch {
+	case e.ID != nil:
 		filters = append(filters, awsup.NewEC2Filter("group-id", *e.ID))
-	} else {
+	case e.Name != nil && e.VPC != nil:
+		filters = append(filters, awsup.NewEC2Filter("vpc-id", *e.VPC.ID))
+		filters = append(filters, awsup.NewEC2Filter("group-name", *e.Name))
+		filters = append(filters, awsup.NewEC2Filter("tag:kubernetes.io/cluster/"+c.T.Cluster.Name, "owned"))
+	default:
 		return nil, nil
 	}
 	request := &ec2.DescribeSecurityGroupRulesInput{
