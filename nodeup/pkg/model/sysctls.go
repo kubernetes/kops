e6aa1e746fda0d05fc8c9db8bfd73af8f7ffd19b
Aurélien Joga <aurelienjoga@gmail.com>
2025-08-14 11:09:11 +0200

    Remove explicit sysctl fs.inotify.max_user_watches setting
    Since Linux 5.11-rc1, fs.inotify.max_user_watches is dynamically computed up to 1048576 with regards to the addressable physical memory: https://github.com/torvalds/linux/commit/92890123749bafc317bbfacbe0a62ce08d78efb7 .
    
    Tablecloth math from the above-linked commit makes me understand that on a 64bits host with 64GB fs.inotify.max_user_watches would be set to the currently hard coded 524288.

--- nodeup/pkg/model/sysctls.go
+++ nodeup/pkg/model/sysctls.go
@@ -111,11 +111,10 @@ func (b *SysctlBuilder) Build(c *fi.NodeupModelBuilderContext) error {
 			"fs.file-max = 2097152",
 			"",
 
-			"# Max number of inotify instances and watches for a user",
+			"# Max number of inotify instances for a user",
 			"# Since dockerd runs as a single user, the default instances value of 128 per user is too low",
 			"# e.g. uses of inotify: nginx ingress controller, kubectl logs -f",
 			"fs.inotify.max_user_instances = 8192",
-			"fs.inotify.max_user_watches = 524288",
 
 			"# Additional sysctl flags that kubelet expects",
 			"vm.overcommit_memory = 1",
