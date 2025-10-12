2af647df8c0697f3a33fe160eb53f4fdb651a7c7
Ciprian Hacman <ciprian@hakman.dev>
2025-10-12 09:27:35 +0300

    Update Karpenter to v1.8.1
    Signed-off-by: Ciprian Hacman <ciprian@hakman.dev>

--- pkg/model/components/karpenter.go
+++ pkg/model/components/karpenter.go
@@ -36,7 +36,7 @@ func (b *KarpenterOptionsBuilder) BuildOptions(o *kops.Cluster) error {
 	}
 
 	if c.Image == "" {
-		c.Image = "public.ecr.aws/karpenter/controller:1.6.2"
+		c.Image = "public.ecr.aws/karpenter/controller:1.8.1"
 	}
 
 	if c.LogEncoding == "" {
