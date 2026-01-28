8746cceb4e85f5a574763da276e51816ab0382d3
camilabustos <camilabustos@google.com>
2026-01-28 01:03:29 +0000

    test: Update test license headers

--- pkg/instancegroups/delete_test.go
+++ pkg/instancegroups/delete_test.go
@@ -1,21 +1,5 @@
 /*
-Copyright The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-/*
-Copyright 2019 The Kubernetes Authors.
+Copyright 2026 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -207,8 +191,12 @@ func TestDeleteInstanceGroup(t *testing.T) {
 		Clientset: clientset,
 	}
 
-	assert.NoError(t, deleteIG.DeleteInstanceGroup(&ig, false /*force*/))
+	assert.ErrorContains(t, deleteIG.DeleteInstanceGroup(&ig, false /*force*/), "error getting Instance")
+	// Verify that the instance group was not deleted
+	_, err = clientset.InstanceGroupsFor(cluster).Get(ctx, ig.Name, metav1.GetOptions{})
+	assert.NoError(t, err)
 
+	assert.NoError(t, deleteIG.DeleteInstanceGroup(&ig, true /*force*/))
 	// Verify that the instance group was deleted from the clientset
 	_, err = clientset.InstanceGroupsFor(cluster).Get(ctx, ig.Name, metav1.GetOptions{})
 	assert.Error(t, err)
