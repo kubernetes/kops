960890f5713b598a7240e92ccbbbdd9d5bbcd933
Peter Rifel <peter.rifel@datadoghq.com>
2026-02-08 08:25:00 -0600

    Consolidate support for ENI-based CNIs

--- nodeup/pkg/model/networking/amazon-vpc-routed-eni.go
+++ nodeup/pkg/model/networking/amazon-vpc-routed-eni.go
@@ -19,8 +19,6 @@ package networking
 import (
 	"k8s.io/kops/nodeup/pkg/model"
 	"k8s.io/kops/upup/pkg/fi"
-	"k8s.io/kops/upup/pkg/fi/nodeup/nodetasks"
-	"k8s.io/kops/util/pkg/distributions"
 )
 
 // AmazonVPCRoutedENIBuilder writes the Amazon VPC CNI configuration
@@ -36,99 +34,11 @@ func (b *AmazonVPCRoutedENIBuilder) Build(c *fi.NodeupModelBuilderContext) error
 		return nil
 	}
 
-	if b.Distribution == distributions.DistributionAmazonLinux2023 {
-		// Mask udev triggers installed by amazon-ec2-net-utils package
-		// Create an empty file 99-vpc-policy-routes.rules
-		c.AddTask(&nodetasks.File{
-			Path:     "/etc/udev/rules.d/99-vpc-policy-routes.rules",
-			Contents: fi.NewStringResource(""),
-			Type:     nodetasks.FileType_File,
-			OnChangeExecute: [][]string{
-				{"udevadm", "control", "--reload-rules"},
-				{"udevadm", "trigger"},
-			},
-		})
-	}
-
-	if (b.Distribution.IsUbuntu() && b.Distribution.Version() >= 22.04) ||
-		b.Distribution == distributions.DistributionAmazonLinux2023 {
-		// Make systemd-networkd ignore foreign settings, else it may
-		// unexpectedly delete IP rules and routes added by CNI
-		contents := `
-# Do not clobber any routes or rules added by CNI.
-[Network]
-ManageForeignRoutes=no
-ManageForeignRoutingPolicyRules=no
-`
-		c.AddTask(&nodetasks.File{
-			Path:            "/usr/lib/systemd/networkd.conf.d/80-release.conf",
-			Contents:        fi.NewStringResource(contents),
-			Type:            nodetasks.FileType_File,
-			OnChangeExecute: [][]string{{"systemctl", "restart", "systemd-networkd"}},
-		})
-	}
-
-	// Running Amazon VPC CNI on Ubuntu 22.04+ and AL2023 requires
-	// setting MACAddressPolicy to `none` (ref: https://github.com/aws/amazon-vpc-cni-k8s/issues/2103
-	// & https://github.com/aws/amazon-vpc-cni-k8s/issues/2839
-	// & https://github.com/kubernetes/kops/issues/16255)
-	if b.Distribution.IsUbuntu() && b.Distribution.Version() >= 22.04 || b.Distribution == distributions.DistributionAmazonLinux2023 {
-		contents := `
-[Match]
-OriginalName=*
-[Link]
-NamePolicy=keep kernel database onboard slot path
-AlternativeNamesPolicy=database onboard slot path
-MACAddressPolicy=none
-`
-
-		// Copy all the relevant entries and replace the one that contains MACAddressPolicy= with MACAddressPolicy=none
-		c.AddTask(&nodetasks.File{
-			Path:            "/etc/systemd/network/99-default.link",
-			Contents:        fi.NewStringResource(contents),
-			Type:            nodetasks.FileType_File,
-			OnChangeExecute: [][]string{{"systemctl", "restart", "systemd-networkd"}},
-		})
-
-	}
-
-	// Running Amazon VPC CNI on al2023 requires setting Unmanaged to `yes`
-	// ref: https://github.com/aws/amazon-vpc-cni-k8s/issues/3524
-	if b.Distribution == distributions.DistributionAmazonLinux2023 {
-		contents := `
-[Match]
-Name=ens[6-9]* ens[1-9][0-9]*
-
-[Link]
-Unmanaged=yes
-`
-
-		c.AddTask(&nodetasks.File{
-			Path:            "/etc/systemd/network/10-vpc-cni-secondary.network",
-			Contents:        fi.NewStringResource(contents),
-			Type:            nodetasks.FileType_File,
-			OnChangeExecute: [][]string{{"systemctl", "restart", "systemd-networkd"}},
-		})
-
-	}
-
-	// On Ubuntu 24.04+, cloud-init network hotplug is enabled by default
-	// (https://github.com/canonical/cloud-init/pull/4799). This causes cloud-init to reconfigure netplan
-	// when Amazon VPC CNI attaches ENIs, breaking network functionality.
-	// See: https://github.com/kubernetes/kops/issues/17881
-	if b.Distribution.IsUbuntu() && b.Distribution.Version() >= 24.04 {
-		contents := `# Disable cloud-init network hotplug to prevent interference with Amazon VPC CNI ENI management.
-# See: https://github.com/kubernetes/kops/issues/17881
-updates:
-  network:
-    when: [boot-new-instance]
-`
-		c.AddTask(&nodetasks.File{
-			Path:     "/etc/cloud/cloud.cfg.d/99-disable-network-hotplug.cfg",
-			Contents: fi.NewStringResource(contents),
-			Type:     nodetasks.FileType_File,
-		})
-	}
+	maskEC2NetUtilsUdevRules(c, b.Distribution)
+	disableManageForeignRoutes(c, b.Distribution)
+	setMACAddressPolicyNone(c, b.Distribution)
+	markSecondaryENIsUnmanaged(c, b.Distribution)
+	disableCloudInitNetworkHotplug(c, b.Distribution)
 
 	return nil
 }
