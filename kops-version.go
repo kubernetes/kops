9d9bc4584ab54e4c180d5a7c9c0de8471d53394f
Justin Santa Barbara <justinsb@google.com>
2026-02-09 05:41:23 -0500

    Release 1.35.0-beta.1 (#17941)

--- kops-version.go
+++ kops-version.go
@@ -30,8 +30,8 @@ func KopsVersionImageTag() string {
 
 // These constants are parsed by build tooling - be careful about changing the formats
 const (
-	KOPS_RELEASE_VERSION = "1.35.0-alpha.1"
-	KOPS_CI_VERSION      = "1.35.0-alpha.2"
+	KOPS_RELEASE_VERSION = "1.35.0-beta.1"
+	KOPS_CI_VERSION      = "1.35.0-beta.2"
 )
 
 // GitVersion should be replaced by the makefile
