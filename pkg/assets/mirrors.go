91517ae42ffde015967b243ea4c5f544199be3fa
Ciprian Hacman <ciprian@hakman.dev>
2026-02-20 20:40:06 +0200

    drop storage.googleapis.com/k8s-artifacts-cni as a mirror
    Signed-off-by: Ciprian Hacman <ciprian@hakman.dev>

--- pkg/assets/mirrors.go
+++ pkg/assets/mirrors.go
@@ -34,15 +34,6 @@ var wellKnownMirrors = []mirrorConfig{
 			"https://github.com/kubernetes/kops/releases/download/v{kopsVersion}/",
 		},
 	},
-	{
-		Base: "https://storage.googleapis.com/k8s-artifacts-cni/release/",
-		Mirrors: []string{
-			"https://github.com/containernetworking/plugins/releases/download/",
-		},
-	},
-	{
-		Base: "https://dl.k8s.io/release/",
-	},
 }
 
 func (m *mirrorConfig) findMirrors(u string) ([]string, bool) {
