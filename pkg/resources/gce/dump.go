55039e4e819c08cec43ebf06340efa9eed10db80
Ciprian Hacman <ciprian@hakman.dev>
2026-03-09 18:37:12 +0200

    test: Skip dumping not found nodes for GCE

--- pkg/resources/gce/dump.go
+++ pkg/resources/gce/dump.go
@@ -62,18 +62,19 @@ func DumpManagedInstance(op *resources.DumpOperation, r *resources.Resource) err
 	instanceDetails := instanceMap[u.Name]
 	if instanceDetails == nil {
 		klog.Warningf("instance %q not found", instance.Instance)
-	} else {
-		for _, ni := range instanceDetails.NetworkInterfaces {
-			if ni.NetworkIP != "" {
-				i.PrivateAddresses = append(i.PrivateAddresses, ni.NetworkIP)
-			}
-			if ni.Ipv6Address != "" {
-				i.PrivateAddresses = append(i.PrivateAddresses, ni.Ipv6Address)
-			}
-			for _, ac := range ni.AccessConfigs {
-				if ac.NatIP != "" {
-					i.PublicAddresses = append(i.PublicAddresses, ac.NatIP)
-				}
+		return nil
+	}
+
+	for _, ni := range instanceDetails.NetworkInterfaces {
+		if ni.NetworkIP != "" {
+			i.PrivateAddresses = append(i.PrivateAddresses, ni.NetworkIP)
+		}
+		if ni.Ipv6Address != "" {
+			i.PrivateAddresses = append(i.PrivateAddresses, ni.Ipv6Address)
+		}
+		for _, ac := range ni.AccessConfigs {
+			if ac.NatIP != "" {
+				i.PublicAddresses = append(i.PublicAddresses, ac.NatIP)
 			}
 		}
 	}
