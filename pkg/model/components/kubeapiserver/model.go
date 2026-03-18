b54c05d9b9a1dab7e2cae2332c10957ea393af77
Ciprian Hacman <ciprian@hakman.dev>
2026-03-18 07:44:17 +0200

    Release 1.32.3

--- pkg/model/components/kubeapiserver/model.go
+++ pkg/model/components/kubeapiserver/model.go
@@ -80,7 +80,7 @@ kind: Pod
 spec:
   containers:
   - name: healthcheck
-    image: registry.k8s.io/kops/kube-apiserver-healthcheck:1.32.2
+    image: registry.k8s.io/kops/kube-apiserver-healthcheck:1.32.3
     livenessProbe:
       httpGet:
         # The sidecar serves a healthcheck on the same port,
