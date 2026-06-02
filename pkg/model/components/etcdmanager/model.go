aad5aa4ccc2a28edf5556e93364aff9925181e4c
Ciprian Hacman <ciprian@hakman.dev>
2026-06-02 14:43:38 +0300

    etcd-manager: upgrade to v3.0.20260531
    Signed-off-by: Ciprian Hacman <ciprian@hakman.dev>

--- pkg/model/components/etcdmanager/model.go
+++ pkg/model/components/etcdmanager/model.go
@@ -210,7 +210,7 @@ metadata:
 spec:
   containers:
   - name: etcd-manager
-    image: registry.k8s.io/etcd-manager/etcd-manager-slim:v3.0.20260512
+    image: registry.k8s.io/etcd-manager/etcd-manager-slim:v3.0.20260531
     resources:
       requests:
         cpu: 100m
