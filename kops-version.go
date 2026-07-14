19cde8222bd3fc00c6cd3ae8bd84b75e2375ab47
Ciprian Hacman <ciprian@hakman.dev>
2026-07-14 23:08:53 +0300

    Release 1.35.2

--- kops-version.go
+++ kops-version.go
@@ -30,8 +30,8 @@ func KopsVersionImageTag() string {
 
 // These constants are parsed by build tooling - be careful about changing the formats
 const (
-	KOPS_RELEASE_VERSION = "1.35.1"
-	KOPS_CI_VERSION      = "1.35.2"
+	KOPS_RELEASE_VERSION = "1.35.2"
+	KOPS_CI_VERSION      = "1.35.3"
 )
 
 // GitVersion should be replaced by the makefile
